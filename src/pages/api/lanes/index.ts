f0327e5de7cf5eb34d479731a8c4258972a7e7b3
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-10-13 14:29:30 -0400

    With sample data

--- src/pages/api/lanes/index.ts
+++ src/pages/api/lanes/index.ts
@@ -1,9 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import Data from "@/data/lane1.json"
+import lanes from "@/data/lanes.json"
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json(Data)
+  res.status(200).json(lanes)
 }
+
---
+++ src/pages/api/lanes/recently-digitized-and-updated-collections.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import data from "@/data/lanes.json"
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    res.status(200).json(data['lanes'][0]['recently-digitized-and-updated-collections'])
+}
