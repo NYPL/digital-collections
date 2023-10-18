d2c7e10cf4e8ff91123b8ff1d1ef816a48befe19
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-10-18 13:12:24 -0400

    testing linter

--- __tests__/lanes.test.tsx
+++ __tests__/lanes.test.tsx
@@ -1,40 +1,40 @@
-import { getLanesData } from '@/pages/api/lanes';
+import { getLanesData } from "@/pages/api/lanes";
 import lanesDataHandler from "@/pages/api/lanes";
-import { NextApiRequest } from 'next';
+import { NextApiRequest } from "next";
 
-describe('All lanes API endpoint handler', () => {
-    it('should respond with 200 status and lanes data for a GET request', () => {
-      const request: NextApiRequest = {
-        method: 'GET',
-      } as NextApiRequest;
-  
-      const mockResponse: any = {
-        status: jest.fn(function () {
-          return this; 
-        }),
-        json: jest.fn(),
-      };
-  
-      lanesDataHandler(request, mockResponse);
-  
-      expect(mockResponse.status).toHaveBeenCalledWith(200);
-      expect(mockResponse.json).toHaveBeenCalledWith(getLanesData());
-    });
-  
-    it('should respond with an error for non-GET requests', () => {
-      const request: NextApiRequest = {
-        method: 'POST',
-      } as NextApiRequest;
-  
-      const mockResponse: any = {
-        status: jest.fn(function () {
-          return this; 
-        }),
-      };
-  
-      lanesDataHandler(request, mockResponse);
-  
-      expect(mockResponse.status).not.toHaveBeenCalledWith(200);
-    });
+describe("All lanes API endpoint handler", () => {
+  it("should respond with 200 status and lanes data for a GET request", () => {
+    const request: NextApiRequest = {
+      method: "GET",
+    } as NextApiRequest;
+
+    const mockResponse: any = {
+      status: jest.fn(function () {
+        return this;
+      }),
+      json: jest.fn(),
+    };
+
+    lanesDataHandler(request, mockResponse);
+    //testing things
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.json).toHaveBeenCalledWith(getLanesData());
   });
-  
+
+  it("should respond with an error for non-GET requests", () => {
+    const request: NextApiRequest = {
+      method: "POST",
+    } as NextApiRequest;
+
+    const mockResponse: any = {
+      status: jest.fn(function () {
+        return this;
+      }),
+    };
+
+    lanesDataHandler(request, mockResponse);
+
+    expect(mockResponse.status).not.toHaveBeenCalledWith(200);
+  });
+});
