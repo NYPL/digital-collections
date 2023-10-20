5eebaa8c364f321095ff33f11bd12aaa95d2fd19
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-10-20 15:43:41 -0400

    A lot less folders of data

--- src/pages/api/lanes/[slug].ts
+++ src/pages/api/lanes/[slug].ts
@@ -1,32 +1,25 @@
-import path from "path";
-import { promises as fs } from "fs";
+import lanesData from "@/data/lanes.json";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
+export default async function slugHandler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const { slug } = request.query;
+  const { method, query } = request;
+  
+  const slug = query.slug as string;  
 
-  // Find the absolute path of the json directory for the lanes/[...slug].json file
-  const lanesJsonDirectory = path.join(
-    process.cwd(),
-    `/src/data/lanes/${slug}`
+  const slugData = lanesData.lanes.find(
+    (lane) => lane[slug] !== undefined
   );
 
-  // Read the json data file data.json
-  const fileContents = await fs.readFile(
-    lanesJsonDirectory + "/data.json",
-    "utf8"
-  );
-
-  const parsedData = JSON.parse(fileContents);
-
-  const { method } = request;
   if (method === "GET") {
-    response.status(200).json(parsedData);
+    if (slugData) {
+      return response.status(200).json(slugData);
+    } else {
+      return response.status(404).json({ error: 'Lane not found' });
+    }
   }
-}
+};
 
-
-// http://localhost:3000/api/lanes/:slug
+// http://localhost:3000/api/lanes/[slug]
