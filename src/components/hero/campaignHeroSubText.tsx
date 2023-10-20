a20bc29c64156e462e56d55ac9f9b074cd91c01f
Alessandra Vertrees <alessandravertrees@nypl.org>
2023-10-20 09:59:58 -0400

    update size of h1

--- src/components/hero/campaignHeroSubText.tsx
+++ src/components/hero/campaignHeroSubText.tsx
@@ -11,13 +11,13 @@ const CampaignHeroSubText = () => {
    return (  
      <>
      {/* To Do: make the link color blue: */}
-        <Text color="ui.typography.body">
+        <Text sx={{"lineHeight":"24px", }}color="ui.typography.body">
           This site is a living database with new materials added every day,
           featuring prints, photographs, maps, manuscripts, streaming video, and
           more.
         </Text>
         <Text color="ui.typography.body">
-        Our collections include some content that may be harmful or difficult to view. <DSLink color="var(--nypl-colors-ui-link-primary) !important" href="https://digitalcollections.nypl.org/about#nypl_harmful_content_statement" aria-label="Learn more about harmful content" >Learn more</DSLink>
+        Our collections include some content that may be harmful or difficult to <br /> view. <DSLink color="var(--nypl-colors-ui-link-primary) !important" href="https://digitalcollections.nypl.org/about#nypl_harmful_content_statement" aria-label="Learn more about harmful content" >Learn more</DSLink>
         </Text>
         <HorizontalRule/>
         <Text color="ui.typography.body" mb="0px">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,21 @@ import CampaignHero from "../components/hero/campaignHero"
 import React from "react"
 
 export default function Home() {
+
+  const [view, setView] = React.useState("form");
+  const apiEndpoint = "...";
+  const onSubmit = (values) => {
+    fetch(apiEndpoint, {
+      method: "POST",
+      body: JSON.stringify(values),
+    }).then((response) => {
+      if (response.ok) {
+        // Resolve the promise according to your application.
+        // And then call:
+        setView("confirmation");
+      }
+    });
+  };
   return (
     <CampaignHero />
   )
