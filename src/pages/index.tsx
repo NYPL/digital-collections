dfa11a4764c4eef4a404c78c2d9bcef0dd3d3dcd
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-10-25 14:21:12 -0400

    Template for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
 import CampaignHero from "../components/hero/campaignHero";
+import {
+  SkipNavigation,
+  Template,
+  TemplateAboveHeader,
+  TemplateBreakout,
+  TemplateContent,
+  TemplateFooter,
+  TemplateHeader,
+} from "@nypl/design-system-react-components";
 
 export default function Home() {
-  return <CampaignHero />;
+  return (
+    <>
+      <SkipNavigation />
+      <Template>
+        <TemplateBreakout>
+          <TemplateAboveHeader>
+            <p> Notification banner </p>
+          </TemplateAboveHeader>
+          <TemplateHeader>
+            <p> Header </p>
+          </TemplateHeader>
+          <CampaignHero />
+        </TemplateBreakout>
+        <TemplateContent>
+          <p>First swim lane</p>
+          <p>Featured Content</p>
+          <p>Rest of swim lanes</p>
+          <p>Explore further and links</p>
+        </TemplateContent>
+        <TemplateFooter>
+          <p> Footer </p>
+        </TemplateFooter>
+      </Template>
+    </>
+  );
 }
