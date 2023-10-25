926689e17b413e836eadc63cd574bac014188a08
Alessandra Vertrees <alessandravertrees@nypl.org>
2023-10-25 15:22:35 -0400

    change image on reload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import CampaignHero from "../components/hero/campaignHero";
+import featuredItemsData from "../data/featureditems.json";
 
-export default function Home() {
-  return <CampaignHero />;
+export default function Home(props: any) {
+  return <CampaignHero featuredItems={props.featuredItemsData.featuredItems} />;
+}
+
+export async function getServerSideProps() {
+  return { props: { featuredItemsData } };
 }
