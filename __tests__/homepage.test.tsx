b64eb6e28045c1f5e4c4b70c7c4102d0a626b6fe
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-10-25 14:37:59 -0400

    Tests and other fixes

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -17,4 +17,55 @@ describe("Home", () => {
     });
     expect(header).toBeInTheDocument();
   });
+
+  it("renders SkipNavigation", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("SkipNavigation")).toBeInTheDocument();
+  });
+
+  it("renders Template", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Template")).toBeInTheDocument();
+  });
+
+  it("renders TemplateBreakout", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("TemplateBreakout")).toBeInTheDocument();
+  });
+
+  it("renders TemplateAboveHeader", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("TemplateAboveHeader")).toBeInTheDocument();
+  });
+
+  it("renders TemplateHeader", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("TemplateHeader")).toBeInTheDocument();
+  });
+
+  it("renders CampaignHero", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("CampaignHero")).toBeInTheDocument();
+  });
+
+  it("renders TemplateContent", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("TemplateContent")).toBeInTheDocument();
+  });
+
+  it("renders TemplateFooter", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("TemplateFooter")).toBeInTheDocument();
+  });
+
+  it("renders the expected text content", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Notification banner")).toBeInTheDocument();
+    expect(getByText("Header")).toBeInTheDocument();
+    expect(getByText("First swim lane")).toBeInTheDocument();
+    expect(getByText("Featured Content")).toBeInTheDocument();
+    expect(getByText("Rest of swim lanes")).toBeInTheDocument();
+    expect(getByText("Explore further and links")).toBeInTheDocument();
+    expect(getByText("Footer")).toBeInTheDocument();
+  });
 });
