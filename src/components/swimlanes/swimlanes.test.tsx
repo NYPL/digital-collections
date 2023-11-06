e0a703d272c24229e7120fac3719e2846efab085
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-06 12:32:31 -0500

    Updating tests

--- src/components/swimlanes/swimlanes.test.tsx
+++ src/components/swimlanes/swimlanes.test.tsx
@@ -6,17 +6,8 @@ describe("Swim Lanes component renders with expected props", () => {
   it("renders the first row", () => {
     render(<SwimLanes {...props} />);
     const firstrow = screen.getByTestId("test-collections-1");
-    expect(within(firstrow).getAllByText(/See more/)[0]).toHaveAttribute(
-      "href",
-      "https://digitalcollections.nypl.org/search/index?"
-    );
-
     expect(within(firstrow).getByText("Posada Collection")).toBeInTheDocument();
     expect(within(firstrow).getByText("34 items")).toBeInTheDocument();
-    expect(within(firstrow).getAllByRole("img")[0]).toHaveAttribute(
-      "alt",
-      "Posada Collection"
-    );
     expect(within(firstrow).getAllByRole("img")[0]).toHaveAttribute(
       "src",
       "https://iiif.nypl.org/iiif/2/58270299/full/288,/0/default.jpg"
@@ -33,18 +24,10 @@ describe("Swim Lanes component renders with expected props", () => {
   it("renders the second row", () => {
     render(<SwimLanes {...props} />);
     const secondrow = screen.getByTestId("test-collections-2");
-    expect(within(secondrow).getAllByText(/See more/)[0]).toHaveAttribute(
-      "href",
-      "https://digitalcollections.nypl.org/search/index?"
-    );
     expect(
       within(secondrow).getByText("Friedman-Abeles photographs")
     ).toBeInTheDocument();
     expect(within(secondrow).getByText("35 items")).toBeInTheDocument();
-    expect(within(secondrow).getAllByRole("img")[0]).toHaveAttribute(
-      "alt",
-      "Friedman-Abeles photographs"
-    );
     expect(within(secondrow).getAllByRole("img")[0]).toHaveAttribute(
       "src",
       "https://iiif.nypl.org/iiif/2/58498722/full/288,/0/default.jpg"
