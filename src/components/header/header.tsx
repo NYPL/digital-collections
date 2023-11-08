e784e6e10309343975cbf47d368b32d2bbbbc1a5
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-08 15:29:55 -0500

    Midway

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,35 @@
-import { HStack } from "@nypl/design-system-react-components";
+import { Box, HStack, VStack } from "@nypl/design-system-react-components";
 import React from "react";
 import DCLogo from "../logo/logo";
 
 const Header = () => {
   return (
-    <>
-      <HStack ps="2" pe="2" justify="space-between">
-        <DCLogo />
+    <Box position="sticky" top={0} zIndex={10} bgColor="white">
+      <HStack
+        justify="center"
+        align="center"
+        sx={{
+          display: "flex",
+          alignSelf: "stretch",
+        }}
+      >
+        <HStack
+          justify="space-between"
+          align="center"
+          px="16px"
+          sx={{
+            display: "flex",
+            width: "1280px",
+          }}
+        >
+          <DCLogo />
+          <VStack py="16px">
+            <p> I am the nav links! </p>
+            <p> I am the search bar and filter! </p>
+          </VStack>
+        </HStack>
       </HStack>
-    </>
+    </Box>
   );
 };
 
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -17,11 +17,39 @@ const DCLogo = () => {
           size="xxsmall"
           aria-label="NYPL Lion Logo"
         />
-        <VStack align="left">
-          <Heading level="h3" mb="0" mt="0" size="heading6">
+        <VStack
+          align="left"
+          sx={{
+            display: { sm: "none", md: "none", lg: "block" },
+          }}
+        >
+          <Heading
+            level="h3"
+            mb="0"
+            mt="0"
+            size="heading6"
+            sx={{
+              color: "#504E49",
+              fontWeight: "700",
+              lineHeight: 0.8,
+              fontSize: "xl",
+              letterSpacing: "1.75px",
+            }}
+          >
             THE NEW YORK PUBLIC LIBRARY
           </Heading>
-          <Heading level="h4" mt="0" size="heading4">
+          <Heading
+            level="h4"
+            mt="0"
+            mb="0"
+            size="heading4"
+            sx={{
+              fontWeight: "700",
+              lineHeight: 0.8,
+              m: "0px",
+              fontSize: "4xl",
+            }}
+          >
             DIGITAL COLLECTIONS
           </Heading>
         </VStack>
