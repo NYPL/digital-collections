4dd4d94e5490508b0c9b1a851c637f0fa8d613c4
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-09 16:35:12 -0500

    Edwin fixes

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -10,54 +10,66 @@ import appConfig from "appConfig";
 
 const DCLogo = () => {
   return (
-    <>
-      <Link isUnderlined={false} href={`${appConfig.DC_URL}`}>
-        <HStack>
-          <Logo
-            pt="2"
-            pb="2"
-            name="nyplLionBlack"
-            size="xxsmall"
-            aria-label="NYPL Lion Logo"
-          />
-          <VStack
-            align="left"
+    <Link
+      isUnderlined={false}
+      aria-label={"Digital Collections Homepage"}
+      href={`${appConfig.DC_URL}`}
+    >
+      <HStack>
+        <Logo
+          pt="2"
+          pb="2"
+          name="nyplLionBlack"
+          size="xxsmall"
+          aria-label="NYPL Lion Logo"
+          sx={{
+            display: { sm: "block", md: "none", lg: "none" },
+          }}
+        />
+        <Logo
+          name="digitalCollectionsBlack"
+          aria-label="NYPL Digital Collections Logo"
+          sx={{
+            display: { sm: "none", md: "none", lg: "block" },
+          }}
+        />
+        {/* <VStack
+          align="left"
+          sx={{
+            display: { sm: "none", md: "none", lg: "block" },
+          }}
+        >
+          <Heading
+            level="h3"
+            mb="0"
+            mt="0"
+            size="heading6"
             sx={{
-              display: { sm: "none", md: "none", lg: "block" },
+              color: "var(--nypl-colors-ui-gray-dark)",
+              fontWeight: "700",
+              lineHeight: 0.8,
+              fontSize: "xl",
+              letterSpacing: "1.75px",
             }}
           >
-            <Heading
-              level="h3"
-              mb="0"
-              mt="0"
-              size="heading6"
-              sx={{
-                color: "var(--nypl-colors-ui-gray-dark)",
-                fontWeight: "700",
-                lineHeight: 0.8,
-                fontSize: "xl",
-                letterSpacing: "1.75px",
-              }}
-            >
-              THE NEW YORK PUBLIC LIBRARY
-            </Heading>
-            <Heading
-              level="h4"
-              mt="0"
-              mb="0"
-              size="heading4"
-              sx={{
-                fontWeight: "700",
-                lineHeight: 0.8,
-                fontSize: "4xl",
-              }}
-            >
-              DIGITAL COLLECTIONS
-            </Heading>
-          </VStack>
-        </HStack>
-      </Link>
-    </>
+            THE NEW YORK PUBLIC LIBRARY
+          </Heading>
+          <Heading
+            level="h4"
+            mt="0"
+            mb="0"
+            size="heading4"
+            sx={{
+              fontWeight: "700",
+              lineHeight: 0.8,
+              fontSize: "4xl",
+            }}
+          >
+            DIGITAL COLLECTIONS
+          </Heading>
+        </VStack> */}
+      </HStack>
+    </Link>
   );
 };
 
