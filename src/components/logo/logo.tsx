57279fa8c1cd74f069c827648286d1e3cfde3caa
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-14 11:29:47 -0500

    Updating to DS 2.1.2 and fixing logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -4,6 +4,7 @@ import {
   HStack,
   Logo,
   Link,
+  Box,
 } from "@nypl/design-system-react-components";
 import React from "react";
 import appConfig from "appConfig";
@@ -17,10 +18,9 @@ const DCLogo = () => {
     >
       <HStack>
         <Logo
-          pt="2"
-          pb="2"
           name="nyplLionBlack"
-          size="xxsmall"
+          sizeBasedOn="height"
+          height="40px"
           aria-label="NYPL Lion Logo"
           sx={{
             display: { sm: "block", md: "none", lg: "none" },
@@ -29,45 +29,12 @@ const DCLogo = () => {
         <Logo
           name="digitalCollectionsBlack"
           aria-label="NYPL Digital Collections Logo"
+          sizeBasedOn="height"
+          height="64px"
           sx={{
-            display: { sm: "none", md: "none", lg: "block" },
+            display: { sm: "none", md: "block", lg: "block" },
           }}
         />
-        {/* <VStack
-          align="left"
-          sx={{
-            display: { sm: "none", md: "none", lg: "block" },
-          }}
-        >
-          <Heading
-            level="h3"
-            mb="0"
-            mt="0"
-            size="heading6"
-            sx={{
-              color: "var(--nypl-colors-ui-gray-dark)",
-              fontWeight: "700",
-              lineHeight: 0.8,
-              fontSize: "xl",
-              letterSpacing: "1.75px",
-            }}
-          >
-            THE NEW YORK PUBLIC LIBRARY
-          </Heading>
-          <Heading
-            level="h4"
-            mt="0"
-            mb="0"
-            size="heading4"
-            sx={{
-              fontWeight: "700",
-              lineHeight: 0.8,
-              fontSize: "4xl",
-            }}
-          >
-            DIGITAL COLLECTIONS
-          </Heading>
-        </VStack> */}
       </HStack>
     </Link>
   );
