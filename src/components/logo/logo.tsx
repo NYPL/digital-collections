818d66c52785dc2abe35c9d5fc75ca30b0c29940
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-15 11:17:26 -0500

    What's going on

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,11 +1,4 @@
-import {
-  Heading,
-  VStack,
-  HStack,
-  Logo,
-  Link,
-  Box,
-} from "@nypl/design-system-react-components";
+import { HStack, Logo, Link } from "@nypl/design-system-react-components";
 import React from "react";
 import appConfig from "appConfig";
 
@@ -21,18 +14,16 @@ const DCLogo = () => {
           name="nyplLionBlack"
           sizeBasedOn="height"
           height="40px"
-          aria-label="NYPL Lion Logo"
           sx={{
-            display: { sm: "block", md: "none", lg: "none" },
+            display: { sm: "block", md: "none" },
           }}
         />
         <Logo
           name="digitalCollectionsBlack"
-          aria-label="NYPL Digital Collections Logo"
           sizeBasedOn="height"
           height="64px"
           sx={{
-            display: { sm: "none", md: "block", lg: "block" },
+            display: { sm: "none", md: "block" },
           }}
         />
       </HStack>
