b3b5183ea3877f4c8bff3d52928ee473715b8238
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-15 11:05:19 -0500

    Revert "DR-2590 and DR-2584 Header and logo"

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    DC_URL: "https://qa-digitalcollections.nypl.org",
-  },
 };
 
 module.exports = nextConfig;
