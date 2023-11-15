d1233b474cef28c7c3847c1349ae5a46e0497f11
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-15 11:14:55 -0500

    Revert "Refactored bc the hook doesn't really work"
    This reverts commit 4671a61861211b6cad815a72b5dac7d750508eca.

---
+++ src/utils/useScrolled.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+/* Returns if page is scrolled past given element. */
+export function useScrolled(elementId) {
+  const [isScrolled, setIsScrolled] = useState(true);
+
+  useEffect(() => {
+    function handleScroll() {
+      const element = document.getElementById(elementId);
+      if (element) {
+        const offset = element.offsetHeight;
+        if (window.scrollY > offset) {
+          setIsScrolled(false);
+        } else {
+          setIsScrolled(true);
+        }
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [elementId]);
+  return isScrolled;
+}
