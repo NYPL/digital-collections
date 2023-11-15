a29a1416d97b41637712b2d595d269b1de5a8f44
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-15 15:26:24 -0500

    letter spacing

--- src/components/swimlanes/swimLanes.tsx
+++ src/components/swimlanes/swimLanes.tsx
@@ -65,6 +65,7 @@ const SwimLanes = ({ lanesWithNumItems }) => {
                     fontWeight="400"
                     sx={{
                       display: { sm: "none", md: "inline" },
+                      letterSpacing: "-0.01em",
                     }}
                   >
                     <> {collection.numItems} items </>
