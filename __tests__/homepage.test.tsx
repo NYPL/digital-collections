2e7550af2d33af9b5306910c88b126f1b1681173
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-11-16 12:41:42 -0500

    Fixing resizeobserver in tests

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -2,9 +2,15 @@ import { render } from "@testing-library/react";
 import Home from "../src/pages/index";
 import { axe } from "jest-axe";
 import { props } from "./data/homepageProps";
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
 
 describe("Homepage Accessibility", () => {
   it("passes axe accessibility test", async () => {
+    window.ResizeObserver = ResizeObserver;
     const { container } = render(<Home {...props} />);
     expect(await axe(container)).toHaveNoViolations();
   });
