d196f32d6019aa6a5440a66e7beb7345ee3f72af
Alessandra Vertrees <alessandravertrees@nypl.org>
2023-12-06 15:12:36 -0500

    wrap tooltip around link text and not featured item link container

--- src/components/hero/campaignHeroSubText.tsx
+++ src/components/hero/campaignHeroSubText.tsx
@@ -37,11 +37,10 @@ const CampaignHeroSubText = ({ featuredItem }: any) => {
         </DSLink>
       </Text>
       <HorizontalRule />
-      <Tooltip content={featuredItem.title} shouldWrapChildren>
-        <Box>
-          <Text color="ui.typography.body" mb="0px" noOfLines={2}>
-            Featured Image:{" "}
-            {/* <Tooltip content={featuredItem.title} shouldWrapChildren> */}
+      <Box>
+        <Text color="ui.typography.body" mb="0px" noOfLines={2}>
+          Featured Image:{" "}
+          <Tooltip content={featuredItem.title}>
             <DSLink
               color="var(--nypl-colors-ui-link-primary) !important"
               __css={{
@@ -58,10 +57,9 @@ const CampaignHeroSubText = ({ featuredItem }: any) => {
               {" "}
               {featuredItem.title}{" "}
             </DSLink>
-            {/* </Tooltip> */}
-          </Text>
-        </Box>
-      </Tooltip>
+          </Tooltip>
+        </Text>
+      </Box>
     </>
   );
 };
