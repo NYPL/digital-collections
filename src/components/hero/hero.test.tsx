c3a06df35b81f07b2658e75f49e66c8844ab7b93
Alessandra Vertrees <alessandravertrees@nypl.org>
2023-12-07 10:12:50 -0500

    refactor hero.test.tsx to use homepageProps file

--- src/components/hero/hero.test.tsx
+++ src/components/hero/hero.test.tsx
@@ -1,18 +1,11 @@
 import CampaignHero from "@/components/hero/campaignHero";
 import { render, screen } from "@testing-library/react";
-import { imageURL } from "@/utils/utils";
+import { props } from "../../../__tests__/data/homepageProps";
 describe("Campaign Hero", () => {
-  const featuredItem = {
-    imageID: "5273165",
-    imageSrc: imageURL("5273165"),
-    uuid: "3a2cced0-12ca-0133-7a6d-58d385a7bbd0",
-    title: "Latona Giving Birth to Apollo and Diana on the Island of Delos",
-    href: `https://qa-digitalcollections.nypl.org/items/3a2cced0-12ca-0133-7a6d-58d385a7bbd0`,
-  };
   it("renders the Campaign Hero", () => {
     render(
       <CampaignHero
-        featuredItem={featuredItem}
+        featuredItem={props.featuredItem}
         numberOfDigitizedItems="863,848"
       />
     );
