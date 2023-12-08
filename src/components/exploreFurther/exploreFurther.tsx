5b8b21d81406b0e077740fcb46f13051a5b3b757
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-12-08 16:02:55 -0500

    Explore further all except test

--- src/components/exploreFurther/exploreFurther.tsx
+++ src/components/exploreFurther/exploreFurther.tsx
@@ -1,56 +1,87 @@
+import React from "react";
 import {
+  Box,
+  Heading,
   Card,
   CardHeading,
-  CardContent,
   Link,
-  Heading,
-  Button,
-  Box,
 } from "@nypl/design-system-react-components";
-import React from "react";
+import { ExploreFurtherData } from "@/types/ExploreFurtherData";
+import exploreFurtherData from "@/data/exploreFurtherData";
+import { headerBreakpoints } from "@/utils/breakpoints";
 
 const ExploreFurther = () => {
+  const data: ExploreFurtherData[] = exploreFurtherData;
+
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
+        marginTop: "xxl",
         backgroundColor: "var(--ui-bg-default, #F5F5F5)",
-        paddingTop: "xl",
-        paddingLeft: "xl",
       }}
     >
-      <Heading
-        size="heading3"
-        text="Explore further"
-        subtitle="Here are some other ways you can access and engage with digital content at NYPL and beyond"
-      />
-      <Card
-        id="card-id"
-        imageProps={{
-          alt: "Alt text",
-          aspectRatio: "square",
-          component: undefined,
-          isAtEnd: false,
-          isLazy: true,
-          size: "default",
-          src: "//placekitten.com/400/300",
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "1280px",
+          mx: "auto",
+          alignIitems: "center",
+          [`@media screen and (min-width: 0px)`]: {
+            paddingBottom: "l",
+            paddingTop: "xxl",
+            paddingX: "s",
+          },
+          [`@media screen and (min-width: ${headerBreakpoints.smTablet})`]: {
+            paddingBottom: "xxl",
+            paddingTop: "xxl",
+            paddingX: "s",
+          },
         }}
-        layout="row"
       >
-        <CardHeading
-          id="main-heading1"
-          level="h3"
-          subtitle="Sollicitudin Lorem Tortor Purus Ornare"
-        >
-          Optional Header
-        </CardHeading>
-        <CardContent>
-          Vestibulum id ligula porta felis euismod semper. Nulla vitae elit
-          libero, a pharetra augue. Nullam quis risus eget urna mollis ornare
-          vel eu leo. Nulla vitae elit libero, a pharetra augue.
-        </CardContent>
-      </Card>
+        <Box sx={{ paddingBottom: "s" }}>
+          <Heading
+            size="heading3"
+            text="Explore further"
+            subtitle="Here are some other ways you can access and engage with digital content at NYPL and beyond:"
+          />
+        </Box>
+        {data.map((item, index) => (
+          <Card
+            id={`card-id-${index}`}
+            key={`card-key-${index}`}
+            imageProps={{
+              alt: `${item.imgAlt}`,
+              aspectRatio: "sixteenByNine",
+              component: undefined,
+              isAtEnd: false,
+              isLazy: true,
+              size: "default",
+              src: `/${item.image}`,
+            }}
+            layout="row"
+            sx={{
+              [`@media screen and (min-width: 0px)`]: {
+                paddingBottom: "s",
+              },
+              [`@media screen and (min-width: ${headerBreakpoints.smTablet})`]:
+                {
+                  paddingBottom: "l",
+                },
+            }}
+          >
+            <CardHeading
+              id={`main-heading-${index}`}
+              level="h5"
+              subtitle={item.description}
+            >
+              <Link href={item.url} sx={{ textDecoration: "none !important" }}>
+                {item.title}
+              </Link>
+            </CardHeading>
+          </Card>
+        ))}
+      </Box>
     </Box>
   );
 };
