201d49b4afa651bea686da44d2ccdfd7bf1c5bc9
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-12-11 15:16:39 -0500

    Edwin fixes

--- src/data/dcNavLinks.ts
+++ src/data/dcNavLinks.ts
@@ -2,19 +2,19 @@ import appConfig from "appConfig";
 
 export const dcNavLinks = [
   {
-    href: appConfig.DC_URL + `/search/index?utf8=%E2%9C%93&keywords=`,
+    href: `${appConfig.DC_URL}/search/index?utf8=%E2%9C%93&keywords=`,
     text: "Items",
   },
   {
-    href: appConfig.DC_URL + `/collections`,
+    href: `${appConfig.DC_URL}/collections`,
     text: "Collections",
   },
   {
-    href: appConfig.DC_URL + `/divisions`,
+    href: `${appConfig.DC_URL}/divisions`,
     text: "Divisions",
   },
   {
-    href: appConfig.DC_URL + `/about`,
+    href: `${appConfig.DC_URL}/about`,
     text: "About",
   },
 ];
