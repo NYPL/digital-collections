1f969612d026e571410a9e166b42236f70d2566a
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-12-11 15:16:39 -0500

    Edwin fixes

--- src/components/navMenu/navMenu.tsx
+++ src/components/navMenu/navMenu.tsx
@@ -1,26 +1,50 @@
 import { dcNavLinks } from "@/data/dcNavLinks";
-import { NavMenuProps } from "@/types/NavMenuProps";
-import { HStack, Text, Link } from "@nypl/design-system-react-components";
+import { Link, List } from "@nypl/design-system-react-components";
 import React from "react";
 
 const NavMenu = () => {
   const listItems = dcNavLinks.map(({ href, text }) => (
-    <Link isUnderlined={false} href={href} key={text} aria-label={text}>
-      <Text mb="0px" size="subtitle2" color="black">
-        {text}
-      </Text>
+    <Link
+      isUnderlined={false}
+      href={href}
+      aria-label={text}
+      key={text}
+      sx={{
+        mb: "0px",
+        fontWeight: "510",
+        color: "ui.black",
+        paddingLeft: "m",
+        ":visited": {
+          color: "ui.black",
+        },
+        ":hover": {
+          textDecoration: "none !important",
+        },
+      }}
+    >
+      {text}
     </Link>
   ));
 
   return (
-    <HStack
-      sx={{
-        gap: "m",
+    <nav
+      style={{
         paddingBottom: "xs",
       }}
     >
-      {listItems}
-    </HStack>
+      <List
+        id="header-links"
+        listItems={listItems}
+        inline={true}
+        noStyling
+        type="ul"
+        sx={{
+          "& li": {
+            marginInlineEnd: "0px",
+          },
+        }}
+      />
+    </nav>
   );
 };
 
