7800b1a3766c4ac9be446a32edb140c01ca52bdc
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-12-12 13:32:43 -0500

    Needs Lily confirm on copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import Header from "@/components/header/header";
 import HomePageMainContent from "@/components/homePageMainContent/homePageMainContent";
 import appConfig from "appConfig";
 import { imageURL } from "@/utils/utils";
+import NotificationBanner from "@/components/notificationBanner/notificationBanner";
 
 export default function Home(props: any) {
   return (
@@ -20,7 +21,7 @@ export default function Home(props: any) {
       {/**
        * * @TODO: Header will need to be pulled into a reusable Layout component (DC Facelift phase 2)
        * * Let this be @7emansell 's problem if possible **/}
-      <p> Notification banner </p>
+      <NotificationBanner />
       <Header />
       <TemplateAppContainer
         breakout={
