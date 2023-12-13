341d34093c8461296ade233dd71de5cdb0eead84
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-12-13 14:11:42 -0500

    Fixes made but only with footer removed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,11 +7,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <div id="nypl-footer"></div>
+        {/* <div id="nypl-footer"></div>
         <script
           src="https://ds-header.nypl.org/footer.min.js?containerId=nypl-footer"
           async
-        ></script>
+        ></script> */}
       </body>
     </Html>
   );
