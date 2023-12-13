fb881dc6765048d8c690706174fa61f459c7c169
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2023-12-13 17:00:04 -0500

    VQA changes

--- src/components/exploreFurther/exploreFurther.tsx
+++ src/components/exploreFurther/exploreFurther.tsx
@@ -67,11 +67,10 @@ const ExploreFurther = () => {
             layout="row"
             sx={{
               alignItems: "center",
-              paddingBottom: "s",
-              paddingRight: "m",
+              paddingBottom: "l",
               [`@media screen and (min-width: ${headerBreakpoints.smTablet})`]:
                 {
-                  paddingBottom: "l",
+                  paddingRight: "m",
                 },
             }}
           >
