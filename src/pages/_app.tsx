d920eed3bdb1daa7c18ab39de6020f2ab377d38f
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-01-19 14:06:04 -0500

    No twitter tags but everything else

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,26 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>NYPL Digital Collections</title>
+        <meta
+          name="description"
+          content="NYPL's Digital Collections is a living database featuring prints, photographs, maps, manuscripts, video, and more unique research materials."
+        />
+        <meta
+          property="og:image"
+          content="https://digitalcollections.nypl.org/featured_items/ps_mss_831.jpg"
+        />
+        <meta property="og:title" content="NYPL Digital Collections" />
+        <meta
+          property="og:description"
+          content="Explore hundreds of thousands of digital items from The New York Public Library."
+        />
+        <meta
+          property="og:url"
+          content="https://digitalcollections.nypl.org/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="NYPL Digital Collections" />
       </Head>
       <DSProvider>
         <Component {...pageProps} />
