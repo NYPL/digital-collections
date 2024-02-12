7f85fd6718253536f97764b4403fdf8e7ac4f539
Edwin Guzman <edwin.gzmn@gmail.com>
2024-02-12 15:30:24 -0500

    Testing env var

--- appConfig.ts
+++ appConfig.ts
@@ -9,6 +9,7 @@ const appConfig = {
       process.env.ADOBE_EMBED_URL ||
       "https://assets.adobedtm.com/1a9376472d37/8519dfce636d/launch-672b7e7f98ee.min.js",
   },
+  TEST_ENV_VAR: process.env.TEST_ENV_VAR,
 };
 
 export default appConfig;
