b8949851ea09822de68539e27e0bfbda44bfc5d2
Edwin Guzman <edwin.gzmn@gmail.com>
2024-02-12 15:59:23 -0500

    Testing env var

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     DC_URL: process.env.DC_URL,
     ADOBE_EMBED_URL: process.env.ADOBE_EMBED_URL,
     APP_ENV: process.env.APP_ENV,
+    NEXT_PUBLIC_ANOTHER_TEST: process.env.NEXT_PUBLIC_ANOTHER_TEST,
   },
   images: {
     remotePatterns: [
