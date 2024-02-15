8dc55715df1c08d4793bfcbf7096229b9712791d
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-02-15 13:51:36 -0500

    fix broken tests

--- __tests__/data/appConfig.ts
+++ __tests__/data/appConfig.ts
@@ -1,5 +1,17 @@
 const appConfig = {
-  DC_URL: process.env.DC_URL,
+  environment: process.env.APP_ENV || "development",
+  DC_URL: {
+    development: "https://qa-digitalcollections.nypl.org",
+    qa: "https://qa-digitalcollections.nypl.org",
+    production: "https://digitalcollections.nypl.org",
+  },
+  adobeEmbedUrl: {
+    development:
+      "https://assets.adobedtm.com/1a9376472d37/8519dfce636d/launch-bf8436264b01-development.min.js",
+    qa: "https://assets.adobedtm.com/1a9376472d37/8519dfce636d/launch-bf8436264b01-development.min.js",
+    production:
+      "https://assets.adobedtm.com/1a9376472d37/8519dfce636d/launch-672b7e7f98ee.min.js",
+  },
 };
 
 export default appConfig;
