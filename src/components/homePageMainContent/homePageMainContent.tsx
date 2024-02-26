e36ab886c232fb0dbe401896eddcafd1be2f8646
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-02-26 12:26:10 -0500

    cleaned up code a bit more

--- src/components/homePageMainContent/homePageMainContent.tsx
+++ src/components/homePageMainContent/homePageMainContent.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import FeaturedContentComponent from "../featuredContent/featuredContent";
 import SwimLanes from "../swimlanes/swimLanes";
+import { useEffect, useState } from "react";
+import SwimLanesLoading from "../swimlanes/swimLanesLoading";
 
-const HomePageMainContent = ({ randomNumber, lanesWithNumItems }) => {
-  return (
+const HomePageMainContent = () => {
+  // const [isSwimLaneLoading, setIsSwimLaneLoading] = useState(false);
+  const [data, setData] = useState<any>({});
+
+  useEffect(() => {
+    // setIsSwimLaneLoading(true);
+    // console.log("isSwimLaneLoading before FETCH IS: ", isSwimLaneLoading);
+    const fetchData = async () => {
+      const response = await fetch("/api/homepagedata");
+      const responseData = await response.json();
+      console.log(responseData);
+      setData(responseData);
+      // setIsSwimLaneLoading(false);
+    };
+
+    console.log("fetching swim lane data");
+    fetchData();
+  }, []);
+  return data?.lanesWithNumItems ? (
     <>
-      <SwimLanes lanesWithNumItems={[lanesWithNumItems[0]]} />
-      <FeaturedContentComponent randomNumber={randomNumber} />
-      <SwimLanes lanesWithNumItems={lanesWithNumItems.slice(1)} />
+      <SwimLanes lanesWithNumItems={[data.lanesWithNumItems[0]]} />
+      <FeaturedContentComponent randomNumber={data.randomNumber} />
+      <SwimLanes lanesWithNumItems={data.lanesWithNumItems.slice(1)} />
+    </>
+  ) : (
+    <>
+      <SwimLanesLoading />
     </>
   );
 };
