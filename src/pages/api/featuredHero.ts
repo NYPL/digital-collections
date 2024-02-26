12cbd683e76a9e0511fbb9a5aa6f433eae977902
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-02-26 12:29:42 -0500

    more cleanup

--- src/pages/api/featuredHero.ts
+++ src/pages/api/featuredHero.ts
@@ -19,19 +19,12 @@ const itemsDataHandler = async (
     //pass query param to featuredImageID function to check if it is legit
     // logger.info("Generating featured image id");
 
-    // const imageID = (request.query.imageID as string)
-    //   ? featuredImageID(request.query.imageID as string)
-    //   : featuredImageID();
+    const imageID = (request.query.imageID as string)
+      ? featuredImageID(request.query.imageID as string)
+      : featuredImageID();
 
-    const imageID = request.query.imageID as string;
-
-    // console.log(
-    //   "Timer 1: Calls getItemDataFromImageID and getNumDigitizedItems"
-    // );
-    // console.time("timer1");
     const dataFromUri = await getItemDataFromImageID(imageID);
     const numDigitizedItems = await getNumDigitizedItems();
-    // console.timeEnd("timer1");
 
     const featuredItemObject = {
       imageID: imageID,
@@ -45,19 +38,7 @@ const itemsDataHandler = async (
       featuredItem: featuredItemObject,
       numberOfDigitizedItems: numDigitizedItems,
     });
-    // return response.status(200).json({
-    //   featuredItem: {
-    //     href: "https://qa-digitalcollections.nypl.org/items/3b2a61a0-c6cf-012f-b70d-58d385a7bc34",
-    //     imageID: "433221",
-    //     imageSrc:
-    //       "https://iiif.nypl.org/iiif/2/433221/full/!1600,1600/0/default.jpg",
-    //     title: "An oasis in the Badlands",
-    //     uuid: "3b2a61a0-c6cf-012f-b70d-58d385a7bc34",
-    //   },
-    //   numberOfDigitizedItems: "863,848",
-    // });
   }
 };
 export default itemsDataHandler;
-
 // http://localhost:3000/api/featuredItems
