71d71cd1b32b841680688707ecfbff778e9cb33c
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-02-26 12:46:36 -0500

    more cleanup

--- src/pages/api/featuredHero.ts
+++ src/pages/api/featuredHero.ts
@@ -1,10 +1,7 @@
-import itemsData from "@/data/featureditems.json";
-import data from "@/data/lanes";
 import {
   featuredImageID,
   getItemDataFromImageID,
   getNumDigitizedItems,
-  getNumItems,
   imageURL,
 } from "@/utils/utils";
 import appConfig from "appConfig";
