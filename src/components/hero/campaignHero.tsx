182bd2394df0ef85d76ec2f8c3f18f29d034cb6d
Kaylei Burke <kayleiburke@nypl.org>
2024-03-07 17:16:15 -0600

    [DR-2801] try to load a new hero image every time a failure occurs (up to 3 attempts)

--- src/components/hero/campaignHero.tsx
+++ src/components/hero/campaignHero.tsx
@@ -1,4 +1,4 @@
-import { Hero, Link as DSLink } from "@nypl/design-system-react-components";
+import { Hero } from "@nypl/design-system-react-components";
 import CampaignHeroSubText from "./campaignHeroSubText";
 import CampaignHeroHeading from "./campaignHeroHeading";
 import CampaignHeroLoading from "./campaignHeroLoading";
@@ -6,16 +6,32 @@ import { useEffect, useState } from "react";
 
 const CampaignHero = ({ imageID }) => {
   const [data, setData] = useState<any>({});
+  const [loadAttempt, setLoadAttempt] = useState(0);
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const preloadImage = (src) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = resolve;
+      img.onerror = reject;
+      img.src = src;
+    });
+  };
+
+  const fetchFeaturedItem = async () => {
+    try {
       const response = await fetch(`/api/featuredItem`);
       const responseData = await response.json();
       setData(responseData);
-    };
+      await preloadImage(responseData.featuredItem.imageSrc);
+    } catch (error) {
+      console.error("Error loading image:", error);
+      setLoadAttempt((prevAttempt) => prevAttempt + 1); // Trigger another attempt
+    }
+  };
 
-    fetchData();
-  }, [imageID]);
+  useEffect(() => {
+    fetchFeaturedItem();
+  }, [imageID, loadAttempt]); // Retry fetching when loadAttempt changes
 
   return data?.featuredItem ? (
     <Hero
