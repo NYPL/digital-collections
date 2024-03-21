9db934f80cbfdffb523248bf65daef0aad3ad461
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-03-21 12:58:15 -0400

    Adding <title> tag

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,6 +2,7 @@ import { Box } from "@nypl/design-system-react-components";
 import Header from "@/components/header/header";
 import NotificationBanner from "@/components/notificationBanner/notificationBanner";
 import aboutPageElements from "@/data/aboutPageElements";
+import Head from "next/head";
 
 export default function About() {
   function createSection(heading: React.JSX.Element, body: React.JSX.Element) {
@@ -17,6 +18,9 @@ export default function About() {
     <>
       <NotificationBanner />
       <Header />
+      <Head>
+        <title>About NYPL Digital Collections</title>
+      </Head>
       <Box
         sx={{
           maxWidth: "820px",
