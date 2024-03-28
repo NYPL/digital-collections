556d950e06116616d0166d25a186706a359217b8
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-03-28 12:45:33 -0700

    add height to iiif image

--- src/pages/iiif/image/index.tsx
+++ src/pages/iiif/image/index.tsx
@@ -1,8 +1,8 @@
 // import Image from "@samvera/clover-iiif/image";
 import dynamic from "next/dynamic";
 import Header from "@/components/header/header";
-
-const Image = dynamic(
+import { Box } from "@nypl/design-system-react-components";
+const Image: any = dynamic(
   () => import("@samvera/clover-iiif").then((Clover) => (Clover as any).Image),
   {
     ssr: false,
@@ -11,7 +11,9 @@ const Image = dynamic(
 
 const MyCustomImage = () => {
   return (
-    <Image src="https://ids.lib.harvard.edu/ids/iiif/18772291/full/full/0/default.jpg" />
+    <Box h="1000px">
+      <Image src="https://ids.lib.harvard.edu/ids/iiif/18772291/full/full/0/default.jpg" />
+    </Box>
   );
 };
 export default MyCustomImage;
