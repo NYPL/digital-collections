603f73b592fefb1329c3c9a31b0ddce5d3ec70ed
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-04-02 12:04:16 -0400

    Aria label fixed

--- src/components/featuredContent/featuredContent.tsx
+++ src/components/featuredContent/featuredContent.tsx
@@ -26,7 +26,7 @@ const FeaturedContentComponent = ({ randomNumber }) => {
             data-testid={data.buttonId}
             isUnderlined={false}
             target="_blank"
-            aria-label=""
+            aria-label={`${data.buttonText}, ${data.heading}`}
             type="buttonPrimary"
           >
             {data.buttonText}
