2a31c7b9a7989d12db5cea4c41abda3498f1b598
Jeff Croyle <jeffreycroyle@nypl.org>
2024-04-02 09:14:28 -0400

    clean up loading test script

--- test/homepageImageLoadingTest.js
+++ test/homepageImageLoadingTest.js
@@ -1,13 +1,29 @@
 const puppeteer = require("puppeteer");
 
+function parseArgs(args) {
+  const parsedArgs = {};
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (arg.startsWith("--")) {
+      const keyValue = arg.split("=");
+      const key = keyValue[0].slice(2);
+      const value = keyValue.length > 1 ? keyValue[1] : true;
+      parsedArgs[key] = value;
+    }
+  }
+  return parsedArgs;
+}
+
 (async () => {
+  const args = process.argv.slice(2);
+  const parsedArgs = parseArgs(args);
+  //console.log('Parsed command line arguments:', parsedArgs);
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Enable request interception
   await page.setRequestInterception(true);
 
-  // Array to store URLs of loaded images
   const loadedImageUrls = [];
 
   // Intercept network requests
@@ -20,10 +36,15 @@ const puppeteer = require("puppeteer");
     request.continue();
   });
 
-  // Navigate to the homepage
-  await page.goto("https://new-digitalcollections.nypl.org"); // Replace with your Next.js app URL
+  let homepage = "https://qa-new-digitalcollections.nypl.org";
 
-  // Initialize startTime before scrolling down the page
+  if (parsedArgs["prod"] === true) {
+    console.log("Condition is true");
+    homepage = "https://new-digitalcollections.nypl.org";
+  }
+
+  await page.goto(homepage);
+
   const startTime = new Date();
 
   // Scroll down the page to trigger image loading
@@ -82,7 +103,6 @@ const puppeteer = require("puppeteer");
     return desiredImageUrls.every((url) => loadedImageUrls.includes(url));
   });
 
-  // Measure the time it takes for all desired images to appear
   const loadTime = new Date() - startTime;
   console.log(
     "Time taken for all desired images to appear:",
@@ -90,7 +110,6 @@ const puppeteer = require("puppeteer");
     "milliseconds"
   );
 
-  // Output loaded image URLs
   console.log("Loaded image URLs:", loadedImageUrls);
 
   await browser.close();
