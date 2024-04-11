0f2cdba10ad360ba23c9ae37c269dad03448e5a7
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-04-11 09:17:58 -0700

    cleanup

--- appConfig.ts
+++ appConfig.ts
@@ -17,12 +17,6 @@ const appConfig = {
     production:
       "https://assets.adobedtm.com/1a9376472d37/8519dfce636d/launch-672b7e7f98ee.min.js",
   },
-  newRelicClientMonitoringPath: {
-    development: "/newrelic/development.js",
-    qa: "https://assets.adobedtm.com/1a9376472d37/8519dfce636d/launch-bf8436264b01-development.min.js",
-    production:
-      "https://assets.adobedtm.com/1a9376472d37/8519dfce636d/launch-672b7e7f98ee.min.js",
-  },
 };
 
 export default appConfig;
