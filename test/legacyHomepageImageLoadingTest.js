3276f5bb07a5aaf427ced0d585c79511cfef08fe
Jeff Croyle <jeffreycroyle@nypl.org>
2024-04-17 15:18:07 -0400

    remove commented lines

--- test/legacyHomepageImageLoadingTest.js
+++ test/legacyHomepageImageLoadingTest.js
@@ -47,24 +47,6 @@ function parseArgs(args) {
 
   const startTime = new Date();
 
-  // Scroll down the page to trigger image loading
-  //  await page.evaluate(async () => {
-  //    await new Promise((resolve, reject) => {
-  //      let totalHeight = 0;
-  //      const distance = 100;
-  //      const scrollInterval = setInterval(() => {
-  //        const scrollHeight = document.body.scrollHeight;
-  //        window.scrollBy(0, distance);
-  //        totalHeight += distance;
-  //
-  //        if (totalHeight >= scrollHeight) {
-  //          clearInterval(scrollInterval);
-  //          resolve();
-  //        }
-  //      }, 100);
-  //    });
-  //  });
-
   // Wait for all desired images to be present in the DOM
   await page.waitForFunction(() => {
     const desiredImageUrls = [
