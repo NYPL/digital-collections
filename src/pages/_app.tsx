dabfc18328c969beadfa39a3134512a713da1c36
Edwin Guzman <edwin.gzmn@gmail.com>
2024-05-17 15:54:54 -0400

    testing target hiding snippet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -63,6 +63,41 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       />
       {/* <!-- / OptinMonster --> */}
+      <Script id="target-snippet">
+        {`
+          //prehiding snippet for Adobe Target with asynchronous tags deployment
+          ;(function(win, doc, style, timeout) {
+          var STYLE_ID = 'at-body-style';
+          function getParent() {
+              return doc.getElementsByTagName('head')[0];
+          }
+          function addStyle(parent, id, def) {
+              if (!parent) {
+              return;
+              }
+              var style = doc.createElement('style');
+              style.id = id;
+              style.innerHTML = def;
+              parent.appendChild(style);
+          }
+          function removeStyle(parent, id) {
+              if (!parent) {
+              return;
+              }
+              var style = doc.getElementById(id);
+              if (!style) {
+              return;
+              }
+              parent.removeChild(style);
+          }
+          addStyle(getParent(), STYLE_ID, style);
+          setTimeout(function() {
+              removeStyle(getParent(), STYLE_ID);
+          }, timeout);
+          }(window, document, "body {opacity: 0 !important}", 3000));
+        `}
+      </Script>
+
       <Script async src={ADOBE_EMBED_URL} />
       <Script id="adobeDataLayerDefinition">
         {`
