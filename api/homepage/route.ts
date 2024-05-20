0f8e3a660b2ccbc7b14d646093d6f847ecab7477
Edwin Guzman <edwin.gzmn@gmail.com>
2024-05-20 16:20:30 -0400

    fixing another type issue

--- api/homepage/route.ts
+++ api/homepage/route.ts
@@ -11,9 +11,9 @@ const homepageHandler = async (
     const randomNumber = Math.floor(Math.random() * 2);
     const lanes = data.lanes;
     // Get all the UUIDs from the collections
-    const allCollectionUUIDs = lanes.reduce((acc, lane) => {
+    const allCollectionUUIDs: string[] = lanes.reduce((acc, lane) => {
       return acc.concat(lane.collections.map((collection) => collection.uuid));
-    }, []);
+    }, [] as string[]);
     const uuidtoItemCountMap = await getItemsCountFromUUIDs(allCollectionUUIDs);
     // Update the collections for each lane with the number of items
     const updatedLanes = lanes.map((lane) => {
