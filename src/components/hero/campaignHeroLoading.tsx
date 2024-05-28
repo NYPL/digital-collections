143771cad6d2fb7c42f87607a9c9a78107f70698
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-05-28 13:43:41 -0400

    Well i guess this works

--- src/components/hero/campaignHeroLoading.tsx
+++ src/components/hero/campaignHeroLoading.tsx
@@ -3,10 +3,12 @@ import { SkeletonLoader } from "@nypl/design-system-react-components";
 const CampaignHeroLoading = () => {
   return (
     <SkeletonLoader
+      sx={{ "> div": { height: "500px" } }}
       data-testid="hero-skeleton-loader"
       imageAspectRatio="landscape"
       showContent={false}
       showHeading={false}
+      showButton={false}
     />
   );
 };
