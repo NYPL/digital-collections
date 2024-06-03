ff215b877625223460e70a344f221e68f1026c4f
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-06-03 17:10:59 -0400

    And this

--- src/components/hero/campaignHeroLoading.tsx
+++ src/components/hero/campaignHeroLoading.tsx
@@ -1,9 +1,16 @@
+import { headerBreakpoints } from "@/utils/breakpoints";
 import { SkeletonLoader } from "@nypl/design-system-react-components";
 
 const CampaignHeroLoading = () => {
   return (
     <SkeletonLoader
-      sx={{ "> div": { maxHeight: "500px" } }}
+      sx={{
+        "> div": {
+          [`@media screen and (min-width: ${headerBreakpoints.lgMobile})`]: {
+            maxHeight: "500px",
+          },
+        },
+      }}
       data-testid="hero-skeleton-loader"
       imageAspectRatio="landscape"
       showContent={false}
