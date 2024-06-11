58e9fbae31c924a70bfec5ef68d8a4ab900cb394
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-06-11 11:52:35 -0400

    More

---
+++ pages/api/lanes/[slug].ts
@@ -0,0 +1,21 @@
+import lanesData from "../../../app/data/lanes";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function slugHandler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const { method, query } = request;
+  const slug = query.slug as string;
+  const slugData = lanesData.lanes.find((lane) => lane.slug === slug);
+
+  if (method === "GET") {
+    if (slugData) {
+      return response.status(200).json(slugData);
+    } else {
+      return response.status(404).json({ error: "Lane not found" });
+    }
+  }
+}
+
+// http://localhost:3000/api/lanes/:slug
