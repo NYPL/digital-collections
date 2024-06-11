838e29df2eac410ad2adc3f088ceaad8f889a3d5
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-06-11 11:52:35 -0400

    More

---
+++ pages/api/lanes/index.ts
@@ -0,0 +1,19 @@
+import lanesData from "../../../app/data/lanes";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export const getLanesData = () => {
+  return lanesData;
+};
+
+const lanesDataHandler = (
+  request: NextApiRequest,
+  response: NextApiResponse
+) => {
+  const { method } = request;
+  if (method === "GET") {
+    return response.status(200).json(getLanesData());
+  }
+};
+export default lanesDataHandler;
+
+// http://localhost:3000/api/lanes
