fe3a458105e533353008c454e3afced490e2693b
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-06-21 11:37:07 -0400

    Collections, divisions, lane pages added with middleware for redirects

---
+++ app/divisions/[slug]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function Division() {
+  return (
+    <>
+      <title>[Name of Division] - NYPL Digital Collections</title>
+      <h2> Division name </h2>
+    </>
+  );
+}
