62658ffe89f3d5ca755112da618d6a93cae5bc42
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-06-24 09:38:33 -0400

    fix divisions data

---
+++ src/data/divisions.ts
@@ -0,0 +1,125 @@
+// export const divisions = {
+//   divisions: [
+//     {
+//       title: string,
+//       slug: string,
+//       collections: [
+//         {
+//           title: string,
+//           uuid: string,
+//           url: string,
+//           imageID: string,
+//           numberOfDigitizedItems: string,
+//           containsAVMaterial: boolean,
+//           constainsOnSiteMaterials: boolean,
+//         },
+//         {
+//           title: string,
+//           uuid: string,
+//           url: string,
+//           imageID: string,
+//           numberOfDigitizedItems: string,
+//           containsAVMaterial: boolean,
+//           constainsOnSiteMaterials: boolean,
+//         },
+//         {
+//           title: string,
+//           uuid: string,
+//           url: string,
+//           imageID: string,
+//           numberOfDigitizedItems: string,
+//           containsAVMaterial: boolean,
+//           constainsOnSiteMaterials: boolean,
+//         },
+//         {
+//           title: string,
+//           uuid: string,
+//           url: string,
+//           imageID: string,
+//           numberOfDigitizedItems: string,
+//           containsAVMaterial: boolean,
+//           constainsOnSiteMaterials: boolean,
+//         }
+//       ]
+//     }
+//   ]
+// }
+
+// export const divisionsSlug = {
+//   division:
+//   {
+//     title: string,
+//     slug: string,
+//     summary: string,
+//     nyplLink: string,
+//     items: [
+//       {
+//         title: string,
+//         uuid: string,
+//         href: string,
+//         imageID: string,
+//         containsMultipleImages: boolean,
+//       },
+//       {
+//         title: string,
+//         uuid: string,
+//         href: string,
+//         imageID: string,
+//         containsMultipleImages: boolean,
+//       },
+//       {
+//         title: string,
+//         uuid: string,
+//         href: string,
+//         imageID: string,
+//         containsMultipleImages: boolean,
+//       },
+//       {
+//         title: string,
+//         uuid: string,
+//         href: string,
+//         imageID: string,
+//         containsMultipleImages: boolean,
+//       }
+//     ],
+//     collections: [
+//       {
+//         title: string,
+//         uuid: string,
+//         url: string,
+//         imageID: string,
+//         numberOfDigitizedItems: string,
+//         containsAVMaterial: boolean,
+//         constainsOnSiteMaterials: boolean,
+//       },
+//       {
+//         title: string,
+//         uuid: string,
+//         url: string,
+//         imageID: string,
+//         numberOfDigitizedItems: string,
+//         containsAVMaterial: boolean,
+//         constainsOnSiteMaterials: boolean,
+//       },
+//       {
+//         title: string,
+//         uuid: string,
+//         url: string,
+//         imageID: string,
+//         numberOfDigitizedItems: string,
+//         containsAVMaterial: boolean,
+//         constainsOnSiteMaterials: boolean,
+//       },
+//       {
+//         title: string,
+//         uuid: string,
+//         url: string,
+//         imageID: string,
+//         numberOfDigitizedItems: string,
+//         containsAVMaterial: boolean,
+//         constainsOnSiteMaterials: boolean,
+//       },
+//       ...
+//     ]
+//   }
+// }
