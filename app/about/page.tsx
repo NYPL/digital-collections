1f5678b8b3f7652bb15ae8ad80d64d16381de9a0
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-06-25 10:28:45 -0400

    Creating layout component and SC about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,35 +1,10 @@
-"use client";
-import { Box } from "@nypl/design-system-react-components";
-import aboutPageElements from "../data/aboutPageElements";
-import React from "react";
+import AboutPage from "app/components/aboutPage/aboutPage";
 
 export default function About() {
-  function createSection(heading: React.JSX.Element, body: React.JSX.Element) {
-    return (
-      <>
-        {heading}
-        {body}
-      </>
-    );
-  }
-
   return (
     <>
       <title>About NYPL Digital Collections</title>
-      <Box
-        id="mainContent"
-        sx={{
-          maxWidth: "820px",
-          margin: "s",
-          [`@media screen and (min-width: 820px)`]: {
-            margin: "auto",
-          },
-        }}
-      >
-        {aboutPageElements.map((section) =>
-          createSection(section.heading, section.body)
-        )}
-      </Box>
+      <AboutPage />
     </>
   );
 }
