4ad598f6f644a675711fc42abef292359f3ba347
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-06-27 08:08:57 -0400

    load nypl image in ImageViewer

--- app/components/items/clover/book/viewer.tsx
+++ app/components/items/clover/book/viewer.tsx
@@ -33,51 +33,13 @@ const options = {
   },
 };
 
-const customTheme = {
-  colors: {
-    /**
-     * Black and dark grays in a light theme.
-     * All must contrast to 4.5 or greater with `secondary`.
-     */
-    primary: "#37474F",
-    primaryMuted: "#546E7A",
-    primaryAlt: "#263238",
-
-    /**
-     * Key brand color(s).
-     * `accent` must contrast to 4.5 or greater with `secondary`.
-     */
-    accent: "#C62828",
-    accentMuted: "#E57373",
-    accentAlt: "#B71C1C",
-
-    /**
-     * White and light grays in a light theme.
-     * All must must contrast to 4.5 or greater with `primary` and  `accent`.
-     */
-    secondary: "#FFFFFF",
-    secondaryMuted: "#ECEFF1",
-    secondaryAlt: "#CFD8DC",
-  },
-  fonts: {
-    sans: "'Helvetica Neue', sans-serif",
-    display: "Optima, Georgia, Arial, sans-serif",
-  },
-};
-
 const BookViewer = () => {
   const iiifContent =
     "https://iiif.io/api/cookbook/recipe/0011-book-3-behavior/manifest-continuous.json";
   // "https://iiif.io/api/cookbook/recipe/0011-book-3-behavior/manifest-individuals.json";
   // "https://api.dc.library.northwestern.edu/api/v2/collections/c373ecd2-2c45-45f2-9f9e-52dc244870bd?as=iiif";
 
-  return (
-    <Viewer
-      iiifContent={iiifContent}
-      options={options}
-      customTheme={customTheme}
-    />
-  );
+  return <Viewer iiifContent={iiifContent} options={options} />;
 };
 
 export default BookViewer;
