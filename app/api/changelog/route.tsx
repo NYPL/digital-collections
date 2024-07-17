1934f4fb20eb5ad18dce56dd15c11675d5b2f4ca
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-07-17 17:19:25 -0400

    changelog endpoint

--- app/api/changelog/route.tsx
+++ app/api/changelog/route.tsx
@@ -1,16 +1,14 @@
-// import React, { useState, useEffect } from "react";
-// import markdown from "./assets/1.md";
-
-// const Post1 = () => {
-//   return <>{markdown}</>;
-// };
-
-// export default Post1;
 import { NextResponse } from "next/server";
-// import markdown from "../../../CHANGELOG.md";
+import fs from "fs";
+import path from "path";
 
 export const GET = async () => {
-  // return NextResponse.json(markdown, { status: 200 });
+  const filePath = path.join(process.cwd(), "public", "CHANGELOG.md");
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  // const mdToJSON = fileContent.toJSON();
+  const jsonMD = JSON.stringify(fileContent);
+  console.log("jsonMD is: ", jsonMD);
+  return NextResponse.json(JSON.parse(jsonMD), { status: 200 });
 };
 
-// http://localhost:3000/api/lanes
+// // http://localhost:3000/api/changelog
