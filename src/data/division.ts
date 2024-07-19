341c029051eeabca87abd586b45b02e5d0a879de
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-07-19 13:04:33 -0400

    dummy data

---
+++ src/data/division.ts
@@ -0,0 +1,63 @@
+const divisionData = {
+  division: [
+    {
+      title: "Recently digitized collections",
+      slug: "recently-digitized-collections",
+      rank: 0,
+      items: [
+        {
+          uuid: "60932400-20f2-0138-8583-05c43d448773",
+          title: "Posada Collection",
+          url: "https://digitalcollections.nypl.org/collections/posada-collection#/?tab=navigation",
+          image_id: "58270299",
+        },
+        {
+          uuid: "724303e0-c6bb-012f-afbd-58d385a7bc34",
+          title: "MAVO",
+          url: "https://digitalcollections.nypl.org/collections/mavo#/?tab=navigation",
+          image_id: "1408153",
+        },
+        {
+          uuid: "6b6532b0-5df7-013b-36f8-0242ac110002",
+          title: "Austin Hansen photograph collection",
+          url: "https://digitalcollections.nypl.org/collections/austin-hansen-photograph-collection#/?tab=navigation",
+          image_id: "58300996",
+        },
+        {
+          uuid: "5b996640-c31c-0139-0bac-0242ac110004",
+          title: "Arthur Alfonso Schomburg papers",
+          url: "https://digitalcollections.nypl.org/collections/arthur-alfonso-schomburg-papers#/?tab=navigation",
+          image_id: "58591658",
+        },
+      ],
+      collections: [
+        {
+          uuid: "60932400-20f2-0138-8583-05c43d448773",
+          title: "Posada Collection",
+          url: "https://digitalcollections.nypl.org/collections/posada-collection#/?tab=navigation",
+          image_id: "58270299",
+        },
+        {
+          uuid: "724303e0-c6bb-012f-afbd-58d385a7bc34",
+          title: "MAVO",
+          url: "https://digitalcollections.nypl.org/collections/mavo#/?tab=navigation",
+          image_id: "1408153",
+        },
+        {
+          uuid: "6b6532b0-5df7-013b-36f8-0242ac110002",
+          title: "Austin Hansen photograph collection",
+          url: "https://digitalcollections.nypl.org/collections/austin-hansen-photograph-collection#/?tab=navigation",
+          image_id: "58300996",
+        },
+        {
+          uuid: "5b996640-c31c-0139-0bac-0242ac110004",
+          title: "Arthur Alfonso Schomburg papers",
+          url: "https://digitalcollections.nypl.org/collections/arthur-alfonso-schomburg-papers#/?tab=navigation",
+          image_id: "58591658",
+        },
+      ],
+    },
+  ],
+};
+
+export default divisionData;
