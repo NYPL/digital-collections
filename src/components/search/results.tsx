e857a45b117cb89b27b6bd8e01ec2ad2438a33a5
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-08-01 09:59:17 -0400

    cleanup SearchResults component

--- src/components/search/results.tsx
+++ src/components/search/results.tsx
@@ -1,19 +1,12 @@
 "use client";
 import { Suspense } from "react";
 import SearchContent from "./content";
-import { useRouter } from "next/navigation";
 
 function SearchBarFallback() {
   return <>placeholder</>;
 }
 
 const SearchResults = ({}) => {
-  const router = useRouter();
-
-  if (process.env.APP_ENV === "qa" || process.env.APP_ENV === "production") {
-    router.replace("/", undefined);
-  }
-
   return (
     <>
       <Suspense fallback={<SearchBarFallback />}>
