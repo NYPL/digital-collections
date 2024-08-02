01999426b824819aa7c11ffe9d3fe44d82ceb2c2
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-02 12:26:29 -0400

    Ugh

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 function useScrollDirection() {
   const [scrollDirection, setScrollDirection] = useState<"up" | "down" | null>(
-    null
+    "up"
   );
 
   useEffect(() => {
