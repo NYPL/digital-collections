bb9c31e138f75d90e5c580ca1608eb0cb0644213
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-05 13:45:58 -0400

    Restoring item model

--- __tests__/__mocks__/data/mockItems.tsx
+++ __tests__/__mocks__/data/mockItems.tsx
@@ -1,15 +1,27 @@
-export const mockItems = [
+import { ItemModel } from "src/models/item";
+
+// TODO: Add data that makes more sense.
+
+export const mockItems: ItemModel[] = [
   {
     uuid: "50370c90-cabb-013c-da64-0242ac110002",
     title: "Sarah Myers Blackwell sitting in a tree",
     href: "https://digitalcollections.nypl.org/collections/posada-collection#/?tab=navigation",
     imageID: "58886955",
+    mods: "mods",
+    capture: "capture",
+    typeOfResource: "image",
+    isSingleCapture: false,
   },
   {
     uuid: "e56ef5f0-c456-013c-52b7-0242ac110002",
     title: "The Floating World: Lotus (125th)",
     href: "https://digitalcollections.nypl.org/items/e56ef5f0-c456-013c-52b7-0242ac110002",
     imageID: "58853491",
+    mods: "mods",
+    capture: "capture",
+    typeOfResource: "image",
+    isSingleCapture: false,
   },
   {
     uuid: "12563fb0-63a2-013b-bd44-0242ac110003",
@@ -17,6 +29,10 @@ export const mockItems = [
       "Reading room of the Schomburg Collection at the 135th Street Branch Library. Lawrence Reddick, curator, seated at right",
     href: "https://digitalcollections.nypl.org/items/12563fb0-63a2-013b-bd44-0242ac110003",
     imageID: "58613608",
+    mods: "mods",
+    capture: "capture",
+    typeOfResource: "image",
+    isSingleCapture: false,
   },
   {
     uuid: "510d47df-61f5-a3d9-e040-e00a18064a99",
@@ -24,5 +40,9 @@ export const mockItems = [
       "Van Heflin, Gloria Marlowe, and Richard Davalos in the stage production A View from the Bridge",
     href: "https://digitalcollections.nypl.org/items/510d47df-61f5-a3d9-e040-e00a18064a99",
     imageID: "TH-60505",
+    mods: "mods",
+    capture: "capture",
+    typeOfResource: "image",
+    isSingleCapture: false,
   },
 ];
