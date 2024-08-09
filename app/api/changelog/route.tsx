024421dff839fe2b7647d24ef482ee35a075256f
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-08-09 09:17:37 -0400

    remove comments

--- app/api/changelog/route.tsx
+++ app/api/changelog/route.tsx
@@ -5,9 +5,7 @@ import path from "path";
 export const GET = async () => {
   const filePath = path.join(process.cwd(), "public", "CHANGELOG.md");
   const fileContent = fs.readFileSync(filePath, "utf8");
-  // const mdToJSON = fileContent.toJSON();
   const jsonMD = JSON.stringify(fileContent);
-  console.log("jsonMD is: ", jsonMD);
   return NextResponse.json(JSON.parse(jsonMD), { status: 200 });
 };
 
