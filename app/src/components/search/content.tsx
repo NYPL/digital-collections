523f27c81f6d1d2c51e80382fc2cc2d6b51671d6
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-12 16:34:11 -0400

    Creating diff

--- app/src/components/search/content.tsx
+++ app/src/components/search/content.tsx
@@ -18,6 +18,7 @@ const SearchContent = () => {
   const query = queryParams.toString();
   const numColumns = useNumColumns();
   const { isLargerThanLargeTablet } = useBreakpoints();
+  //Git
 
   const router = useRouter();
 
