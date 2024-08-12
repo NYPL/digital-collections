3c3a4d97e642cf6ce5603a632c382650516aca5b
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-12 16:29:24 -0400

    Removing collections page-related changes

--- app/src/components/pages/collectionsPage/collectionsPage.tsx
+++ app/src/components/pages/collectionsPage/collectionsPage.tsx
@@ -2,13 +2,6 @@
 import PageLayout from "../../pageLayout/pageLayout";
 import React from "react";
 import SearchResults from "../../search/results";
-import {
-  Box,
-  Heading,
-  HorizontalRule,
-  SearchBar,
-} from "@nypl/design-system-react-components";
-import { headerBreakpoints } from "../../../utils/breakpoints";
 
 export const CollectionsPage = () => {
   return (
@@ -19,35 +12,6 @@ export const CollectionsPage = () => {
         { text: "Collections", url: "/collections" },
       ]}
     >
-      <Box
-        sx={{
-          [`@media screen and (min-width: ${headerBreakpoints.smTablet})`]: {
-            maxWidth: "715px",
-          },
-          "> hgroup > p": {
-            fontWeight: "400 !important",
-          },
-        }}
-      >
-        <Heading
-          level="h1"
-          text="Collections"
-          subtitle="Explore the New York Public Library's diverse collections, including digitized photographs, manuscripts, maps, and more. Start exploring by using the search bar below or browse through the collections."
-        />
-        <SearchBar
-          sx={{ maxWidth: "462px", marginTop: "l" }}
-          id={"search-collections"}
-          textInputProps={{
-            isClearable: true,
-            labelText: "Search by collection title",
-            name: "searchinput",
-            placeholder: "Search by collection title",
-          }}
-          onSubmit={function (event: React.FormEvent): void {}}
-          labelText={""}
-        />
-      </Box>
-      <HorizontalRule sx={{ marginTop: "xxl", marginBottom: "xxl" }} />
       <SearchResults />
     </PageLayout>
   );
