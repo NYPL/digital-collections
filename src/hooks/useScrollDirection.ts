43626981586df90df52972f93fd0a6b04d88a7a1
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-13 13:54:32 -0400

    Trying with throttle

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -1,40 +1,58 @@
+import { throttle } from "lodash";
 import { useState, useEffect } from "react";
 
+// function useScrollDirection() {
+//   const [scrollDirection, setScrollDirection] = useState<"up" | "down" | null>(
+//     "up"
+//   );
+
+//   useEffect(() => {
+//     let lastScrollY = window.scrollY;
+
+//     const updateScrollDirection = () => {
+//       const scrollY = window.scrollY;
+//       let direction: "up" | "down" | null =
+//         scrollY > lastScrollY ? "down" : "up";
+
+//       if (
+//         direction !== scrollDirection &&
+//         Math.abs(scrollY - lastScrollY) > 10
+//       ) {
+//         setScrollDirection(direction);
+//       }
+
+//       lastScrollY = scrollY > 0 ? scrollY : 0;
+//     };
+
+//     window.addEventListener("scroll", updateScrollDirection);
+
+//     return () => {
+//       window.removeEventListener("scroll", updateScrollDirection);
+//     };
+//   }, [scrollDirection]);
+
+//   return scrollDirection === "up";
+// }
+
 function useScrollDirection() {
-  const [scrollDirection, setScrollDirection] = useState<"up" | "down" | null>(
-    "up"
-  );
+  const [direction, setDirection] = useState("up");
+
+  let lastScrollY = 0;
 
   useEffect(() => {
-    let lastScrollY = window.scrollY;
-
-    const updateScrollDirection = () => {
-      const scrollY = window.scrollY;
-      let direction: "up" | "down" | null =
-        scrollY > lastScrollY ? "down" : "up";
-
-      if (scrollY <= 50) {
-        direction = "up";
-      }
-
-      if (
-        direction !== scrollDirection &&
-        Math.abs(scrollY - lastScrollY) > 20
-      ) {
-        setScrollDirection(direction);
-      }
-
-      lastScrollY = scrollY > 0 ? scrollY : 0;
-    };
-
-    window.addEventListener("scroll", updateScrollDirection);
+    window.onscroll = throttle(() => {
+      const { scrollY } = window;
+      const direction = lastScrollY < scrollY ? "down" : "up";
+      lastScrollY = scrollY;
+      setDirection(direction);
+    }, 300);
 
     return () => {
-      window.removeEventListener("scroll", updateScrollDirection);
+      window.onscroll = null;
     };
-  }, [scrollDirection]);
+  }, []);
 
-  return scrollDirection === "up";
+  return direction === "up";
 }
 
 export default useScrollDirection;
