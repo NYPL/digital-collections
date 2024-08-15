858bfca2f509ca661299511bcce213f099ce36d2
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-15 10:22:45 -0400

    Collection card test

---
+++ app/src/components/cards/collectionCard.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import { mockCollectionCards } from "__tests__/__mocks__/data/mockCollectionCards";
+import CollectionCard from "./collectionCard";
+
+describe("Collection card component", () => {
+  const mockProps = {
+    slug: "test-slug",
+    id: 1,
+    isLargerThanLargeTablet: true,
+    collection: mockCollectionCards[0],
+  };
+
+  const mockPropsNoOnSite = {
+    slug: "test-slug",
+    id: 1,
+    isLargerThanLargeTablet: true,
+    collection: mockCollectionCards[4],
+  };
+
+  it("renders the correct heading with the provided title", () => {
+    render(<CollectionCard {...mockProps} />);
+    const headingElement = screen.getByRole("heading", {
+      name: /Contains On-Site Materials Posada Collection/i,
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  it("renders the badge when containsOnSiteMaterials is true", () => {
+    render(<CollectionCard {...mockProps} />);
+    const badgeElement = screen.getByText(/Contains On-Site Materials/i);
+    expect(badgeElement).toBeInTheDocument();
+  });
+
+  it("does not render the badge when containsOnSiteMaterials is false", () => {
+    render(<CollectionCard {...mockPropsNoOnSite} />);
+    const badgeElement = screen.queryByText(/Contains On-Site Materials/i);
+    expect(badgeElement).not.toBeInTheDocument();
+  });
+
+  it("renders the correct number of items", () => {
+    render(<CollectionCard {...mockProps} />);
+    const itemCountElement = screen.getByText(/34 items/i);
+    expect(itemCountElement).toBeInTheDocument();
+  });
+});
