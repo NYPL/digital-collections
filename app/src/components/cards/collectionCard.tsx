0a9ec4ae17d53c9c6274ca120c3b0699864618c1
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-22 17:24:17 -0400

    Explroe further updates and reverting to no tooltip rc

--- app/src/components/cards/collectionCard.tsx
+++ app/src/components/cards/collectionCard.tsx
@@ -6,9 +6,7 @@ import {
   Text,
   CardContent,
   Tooltip,
-  Link,
   StatusBadge,
-  Heading,
 } from "@nypl/design-system-react-components";
 import styles from "./Card.module.css";
 import { headerBreakpoints } from "../../utils/breakpoints";
@@ -19,87 +17,73 @@ interface CollectionCardProps {
   isLargerThanLargeTablet: boolean;
   collection: CollectionCardDataType;
 }
-// TODO: Truncation logic, mobile breakpoints
+
 const CollectionCard = ({
   slug,
   id,
   isLargerThanLargeTablet,
   collection,
 }: CollectionCardProps) => {
-  return (
-    <Tooltip placement="bottom" elevation={-50} content={collection.title}>
-      <Card
-        id={`card-${slug}-${id}`}
-        mainActionLink={collection.url}
-        imageProps={
-          collection.imageID
-            ? {
-                alt: "",
-                id: `image-${slug}-${id}`,
-                isLazy: true,
-                aspectRatio: "twoByOne",
-                src: collection.imageURL,
-              }
-            : {
-                alt: "",
-                id: `no-image-${id}`,
-                isLazy: true,
-                aspectRatio: "twoByOne",
-                src: "/noImage.png",
-              }
-        }
+  const truncatedTitle = collection.title.length > 80; // Pretty much random
+  const card = (
+    <Card
+      id={`card-${slug}-${id}`}
+      mainActionLink={collection.url}
+      imageProps={
+        collection.imageID
+          ? {
+              alt: "",
+              id: `image-${slug}-${id}`,
+              isLazy: true,
+              aspectRatio: "twoByOne",
+              src: collection.imageURL,
+            }
+          : {
+              alt: "",
+              id: `no-image-${id}`,
+              isLazy: true,
+              aspectRatio: "twoByOne",
+              src: "/noImage.png",
+            }
+      }
+    >
+      <CardHeading
+        id={`row-card-heading-${slug}-${id}`}
+        level="h3"
+        size="heading5"
+        className={styles.cardTitle}
+        noOfLines={3}
+        sx={{ marginTop: "0px", marginBottom: "xs" }}
       >
-        <CardHeading sx={{ height: 0, padding: 0, margin: 0 }}></CardHeading>
-        <CardContent sx={{ alignContent: "top" }}>
-          {collection.containsOnSiteMaterials && (
-            <StatusBadge sx={{ marginBottom: "xs" }} type="informative">
-              Contains on-site materials
-            </StatusBadge>
-          )}
-          <Heading
-            id={`row-card-heading-${slug}-${id}`}
-            level="h3"
-            size="heading5"
-            className={styles.cardTitle}
-            noOfLines={3}
-            sx={{ marginTop: "0px", marginBottom: "xs" }}
-          >
-            <>
-              <Link
-                href={collection.url}
-                sx={{
-                  color: "ui.link.primary",
-                  ":hover": {
-                    color: "ui.link.secondary",
-                    textDecoration: "underline",
-                    textDecorationStyle: "dotted",
-                    textDecorationThickness: "1px",
-                    textUnderlineOffset: "2px",
-                  },
-                  marginBottom: "0",
-                }}
-              >
-                {collection.title}
-              </Link>
-            </>
-          </Heading>
-          <Text
-            id={`item-count-${slug}-${id}`}
-            size="subtitle2"
-            fontWeight="medium"
-            __css={{
-              display: "none",
-              [`@media screen and (min-width: ${headerBreakpoints.lgMobile})`]:
-                {
-                  display: "inline",
-                },
-            }}
-          >
-            {`${collection.numItems} items`}
-          </Text>
-        </CardContent>
-      </Card>
-    </Tooltip>
+        {collection.title}
+      </CardHeading>
+      <CardContent sx={{ alignContent: "top" }}>
+        {collection.containsOnSiteMaterials && (
+          <StatusBadge sx={{ marginBottom: "xs" }} type="informative">
+            Contains on-site materials
+          </StatusBadge>
+        )}
+        <Text
+          id={`item-count-${slug}-${id}`}
+          size="subtitle2"
+          fontWeight="medium"
+          __css={{
+            display: "none",
+            [`@media screen and (min-width: ${headerBreakpoints.lgMobile})`]: {
+              display: "inline",
+            },
+          }}
+        >
+          {`${collection.numItems} items`}
+        </Text>
+      </CardContent>
+    </Card>
+  );
+  return isLargerThanLargeTablet && truncatedTitle ? (
+    // Needs tooltip position updates
+    <Tooltip content={collection.title}>{card}</Tooltip>
+  ) : (
+    card
   );
 };
 
