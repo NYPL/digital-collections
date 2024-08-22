aaf96f3eb003fbdaf5b472d815189270f8681478
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-08-22 11:25:32 -0400

    fix accessibility test

--- __tests__/pages/homepage.test.tsx
+++ __tests__/pages/homepage.test.tsx
@@ -4,6 +4,11 @@ import React from "react";
 import { mockHomePageMainContent } from "__tests__/__mocks__/data/mockHomePageMainContent";
 import HomePage from "@/src/components/pages/homePage/homePage";
 
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+  usePathname: jest.fn(),
+}));
+
 describe("Homepage Accessibility", () => {
   it("passes axe accessibility test", async () => {
     const { container } = render(<HomePage data={mockHomePageMainContent} />);
