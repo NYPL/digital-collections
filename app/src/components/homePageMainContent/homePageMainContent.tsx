e93017250a66fc03d33d73fcdec795e1c432f9d6
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-08-23 09:23:18 -0400

    use breakpoints to test loading states

--- app/src/components/homePageMainContent/homePageMainContent.tsx
+++ app/src/components/homePageMainContent/homePageMainContent.tsx
@@ -3,10 +3,16 @@ import FeaturedContentComponent from "../featuredContent/featuredContent";
 import SwimLanes from "../swimlanes/swimLanes";
 import SwimLanesLoading from "../swimlanes/swimLanesLoading";
 import { useNumColumns } from "../../hooks/useNumColumns";
+import useBreakpoints from "@/src/hooks/useBreakpoints";
 
 const HomePageMainContent = ({ data }) => {
-  const numColumns = useNumColumns();
-
+  // const numColumns = useNumColumns();
+  const { isLargerThanLargeTablet, isLargerThanLargeMobile } = useBreakpoints();
+  const numColumns = isLargerThanLargeTablet
+    ? 4
+    : isLargerThanLargeMobile
+    ? 2
+    : 4;
   return data?.lanesWithNumItems ? (
     <>
       <SwimLanes
