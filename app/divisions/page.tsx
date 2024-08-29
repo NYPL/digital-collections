aa0c859845dcc088e2dcb150767a81d89c4d2e12
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-08-29 12:47:36 -0400

    Should work but returns undefined

--- app/divisions/page.tsx
+++ app/divisions/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Metadata } from "next";
 import DivisionsPage from "../src/components/pages/divisionsPage/divisionsPage";
+import { RepoAPICall } from "@/src/utils/utils";
 
 export const metadata: Metadata = {
   title: "Divisions - NYPL Digital Collections",
 };
 
-export default function Divisions() {
-  return <DivisionsPage />;
+export default async function Divisions() {
+  const apiUrl = `${process.env.API_URL}/api/v2/divisions`;
+  const data = await RepoAPICall(apiUrl);
+  console.log(data);
+  return <DivisionsPage data={data} />;
 }
