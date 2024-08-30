1ffcdd70fd439cd026c6d050f3b834d3e65aa4ae
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-08-30 14:55:21 -0400

    remove scalpel from patient

--- app/divisions/page.tsx
+++ app/divisions/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Metadata } from "next";
 import DivisionsPage from "../src/components/pages/divisionsPage/divisionsPage";
-import { getdivisionsData } from "@/src/utils/api";
 
 export const metadata: Metadata = {
   title: "Divisions - NYPL Digital Collections",
 };
 
-export default async function Divisions() {
+export default function Divisions() {
   return <DivisionsPage />;
 }
