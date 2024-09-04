9100c2a4048cd1f32f176364dd40841a8c4b9b2b
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-04 13:30:04 -0400

    Edwin updates

--- app/src/config/constants.ts
+++ app/src/config/constants.ts
@@ -17,3 +17,5 @@ export const ADOBE_ANALYTICS_PAGE_NAMES = {
 export const DC_URL = appConfig.DC_URL[appConfig.environment as ENV_KEY];
 export const ADOBE_EMBED_URL =
   appConfig.adobeEmbedUrl[appConfig.environment as ENV_KEY];
+
+export const TRUNCATED_LENGTH = 80;
