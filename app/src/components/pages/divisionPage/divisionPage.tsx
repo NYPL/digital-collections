886e53e5fea3593fd3ab28f1ce899e1d01802425
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-05 16:20:08 -0400

    Correcting old import

--- app/src/components/pages/divisionPage/divisionPage.tsx
+++ app/src/components/pages/divisionPage/divisionPage.tsx
@@ -21,8 +21,7 @@ import { mockCollections } from "../../../../../__tests__/__mocks__/data/mockCol
 import ItemCard from "../../../components/cards/itemCard";
 import { mockItems } from "../../../../../__tests__/__mocks__/data/mockItems";
 import { ItemCardModel } from "../../../models/itemCard";
-import React, { useEffect, useState } from "react";
-import SwimLanesLoading from "../../swimlanes/swimLanesLoading";
+import React from "react";
 
 export default function DivisionPage() {
   const params = useParams();
