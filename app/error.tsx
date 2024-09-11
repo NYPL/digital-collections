e37cd58b7ee0100eb89ea748f25bea923a20e7d4
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-11 16:52:26 -0400

    Styling

--- app/error.tsx
+++ app/error.tsx
@@ -1,23 +1,26 @@
 "use client";
 import { Box, Heading } from "@nypl/design-system-react-components";
-import appConfig from "appConfig";
 import Link from "next/link";
 import PageLayout from "./src/components/pageLayout/pageLayout";
-import { ENV_KEY } from "./src/types/EnvironmentType";
 
 export default function Error() {
   return (
     <PageLayout activePage="serverError">
-      <Box sx={{ margin: "xl" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "xl",
+          textAlign: "center",
+        }}
+      >
         <Heading level="h1">500 Error</Heading>
         <p>We&apos;re sorry...</p>
         <p>Something went wrong.</p>
         <p>
-          Return to{" "}
-          <Link href={appConfig.DC_URL[appConfig.environment as ENV_KEY]}>
-            Digital Collections
-          </Link>
-          .
+          Return to <Link href={"/"}>Digital Collections</Link>.
         </p>
       </Box>
     </PageLayout>
