7474d39c6bc6d5567c9c8f165d26096d740a089c
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-11 16:14:03 -0400

    VQA requests for 404

--- app/src/components/pages/notFoundPage/notFoundPage.tsx
+++ app/src/components/pages/notFoundPage/notFoundPage.tsx
@@ -8,16 +8,20 @@ import { ENV_KEY } from "@/src/types/EnvironmentType";
 export default function NotFoundPage() {
   return (
     <PageLayout activePage="notFound">
-      <Box sx={{ margin: "xl" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "xl",
+        }}
+      >
         <Heading level="h1">404 Not Found</Heading>
         <p>We&apos;re sorry...</p>
         <p>The page you were looking for doesn&apos;t exist.</p>
         <p>
-          Return to{" "}
-          <Link href={appConfig.DC_URL[appConfig.environment as ENV_KEY]}>
-            Digital Collections
-          </Link>
-          .
+          Return to <Link href={"/"}>Digital Collections</Link>.
         </p>
       </Box>
     </PageLayout>
