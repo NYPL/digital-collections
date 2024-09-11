1e3dc18451708fb643aa220fdabba02a87fcedee
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-11 14:39:46 -0400

    With error boundary

--- app/divisions/page.tsx
+++ app/divisions/page.tsx
@@ -1,20 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Metadata } from "next";
 import DivisionsPage from "../src/components/pages/divisionsPage/divisionsPage";
-import { RepoAPICall } from "@/src/utils/api";
-import logger from "logger";
+import { getDivisionsData } from "@/src/utils/api";
 
 export const metadata: Metadata = {
   title: "Divisions - NYPL Digital Collections",
 };
 
 export default async function Divisions() {
-  let data;
-  try {
-    data = await RepoAPICall(`${process.env.API_URL}/api/v2/divisions`);
-  } catch (error) {
-    logger.error("Failed to fetch divisions data", error);
-  }
-
-  return <DivisionsPage data={data?.nyplAPI?.response || null} />;
+  const data = await getDivisionsData();
+  return (
+    <Suspense>
+      <DivisionsPage data={data} />
+    </Suspense>
+  );
 }
