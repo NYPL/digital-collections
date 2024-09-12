f37ea0bb4c1715ef1ca323070830d278cf230599
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-12 16:03:28 -0400

    404 redirects

--- app/divisions/page.tsx
+++ app/divisions/page.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { Metadata } from "next";
 import DivisionsPage from "../src/components/pages/divisionsPage/divisionsPage";
 import { getDivisionData } from "@/src/utils/api";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Divisions - NYPL Digital Collections",
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 
 export default async function Divisions() {
   const data = await getDivisionData();
-  console.log(data);
+  if (data.headers.code === "404") {
+    redirect("/404");
+  }
   return (
     <Suspense>
       <DivisionsPage data={data} />
