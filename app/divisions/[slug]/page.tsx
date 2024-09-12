0d785f551f428d6d580f2f8725d24a6d398cba21
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-12 16:03:28 -0400

    404 redirects

--- app/divisions/[slug]/page.tsx
+++ app/divisions/[slug]/page.tsx
@@ -4,6 +4,7 @@ import DivisionPage from "../../src/components/pages/divisionPage/divisionPage";
 import { slugToString } from "../../src/utils/utils";
 import { getDivisionData } from "../../src/utils/api";
 import { Suspense } from "react";
+import { redirect } from "next/navigation";
 
 export type DivisionProps = {
   params: { slug: string };
@@ -21,6 +22,9 @@ export async function generateMetadata({
 
 export default async function Division({ params, searchParams }) {
   const data = await getDivisionData(params.slug, searchParams.page);
+  if (data.headers.code === "404") {
+    redirect("/404");
+  }
   const currentPage = Number(searchParams.page) || 1;
 
   return (
