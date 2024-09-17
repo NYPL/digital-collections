5a01858b7f64c177e1b4d85949df10e75a7848e9
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-17 15:27:55 -0400

    Trying to copy relevant parts of old commit

--- app/divisions/page.tsx
+++ app/divisions/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Metadata } from "next";
 import DivisionsPage from "../src/components/pages/divisionsPage/divisionsPage";
-import { RepoAPICall } from "../src/utils/utils";
 import logger from "logger";
+import { RepoAPICall } from "../src/utils/api";
 
 export const metadata: Metadata = {
   title: "Divisions - NYPL Digital Collections",
@@ -12,9 +12,12 @@ export default async function Divisions() {
   let data;
   try {
     data = await RepoAPICall(`${process.env.API_URL}/api/v2/divisions`);
+    console.log(
+      `api URL for divisions page is ${process.env.API_URL}/api/v2/divisions`
+    );
+    console.log("data in divisions page is: ", data);
   } catch (error) {
     logger.error("Failed to fetch divisions data", error);
   }
-
   return <DivisionsPage data={data?.nyplAPI?.response || null} />;
 }
