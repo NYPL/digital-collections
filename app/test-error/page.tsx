658d34e604db4dd415805891b16211074d19e2a7
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-27 10:57:42 -0400

    No metadata

--- app/test-error/page.tsx
+++ app/test-error/page.tsx
@@ -1,10 +1,5 @@
 "use client";
-import { Metadata } from "next";
 import { useEffect } from "react";
-
-export const metadata: Metadata = {
-  title: "Test Error",
-};
 
 export default function TestError() {
   useEffect(() => {
