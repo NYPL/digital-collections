681933fb9e02b72108952e7e2e68d5f7d08a9d88
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-27 10:51:54 -0400

    in use effect

--- app/test-error/page.tsx
+++ app/test-error/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { Metadata } from "next";
+import { useEffect } from "react";
 
 export const metadata: Metadata = {
   title: "Test Error",
 };
 
 export default function TestError() {
-  throw new Error("Test error");
+  useEffect(() => {
+    throw new Error("Test error");
+  }, []);
 }
