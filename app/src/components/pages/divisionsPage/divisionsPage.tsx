bf1a4b8e6e73edd89e7fa4746457fd1ad99a8b57
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-09-27 10:33:30 -0400

    Adding a test error route

--- app/src/components/pages/divisionsPage/divisionsPage.tsx
+++ app/src/components/pages/divisionsPage/divisionsPage.tsx
@@ -12,7 +12,6 @@ import { DC_URL } from "@/src/config/constants";
 import CollectionLanesLoading from "../../lanes/collectionLanes/collectionLanesLoading";
 
 export default function DivisionsPage({ data }) {
-  console.log(process.env.API_URL);
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
