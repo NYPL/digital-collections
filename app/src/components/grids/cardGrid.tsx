4c7551f60d4208cd6fa58955227f3912a13fe774
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-10-07 15:47:35 -0400

    create cardsGrid component

---
+++ app/src/components/grids/cardGrid.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import useBreakpoints from "../../hooks/useBreakpoints";
+import CollectionDataType from "../../types/CollectionDataType";
+import { CollectionCardModel } from "../../models/collectionCard";
+import ItemDataType from "@/src/types/ItemDataType";
+import ItemCardDataType from "@/src/types/ItemCardDataType";
+import { ItemCardModel } from "@/src/models/itemCard";
+import { SimpleGrid as DCSimpleGrid } from "../simpleGrid/simpleGrid";
+import { Card as DCCard } from "../card/card";
+
+interface CardGridProps {
+  records: CollectionDataType[] | ItemDataType[];
+}
+
+function isCollectionType(
+  records: CollectionDataType[] | ItemDataType[]
+): records is CollectionDataType[] {
+  return "numberOfDigitizedItems" in records[0];
+}
+
+export const CardsGrid = ({ records }: CardGridProps) => {
+  const { isLargerThanLargeTablet } = useBreakpoints();
+  const isCollections = isCollectionType(records);
+
+  return (
+    <DCSimpleGrid>
+      {records?.map((record, index) => {
+        if (isCollections) {
+          const collectionCardModel = new CollectionCardModel(record);
+          return (
+            <DCCard
+              key={index}
+              id={index}
+              slug={collectionCardModel.title}
+              record={collectionCardModel}
+              isLargerThanLargeTablet={isLargerThanLargeTablet}
+            />
+          );
+        } else {
+          const itemCardModel = new ItemCardModel(record);
+          return (
+            <DCCard
+              key={index}
+              id={index}
+              record={itemCardModel}
+              isLargerThanLargeTablet={isLargerThanLargeTablet}
+            />
+          );
+        }
+      })}
+    </DCSimpleGrid>
+  );
+};
