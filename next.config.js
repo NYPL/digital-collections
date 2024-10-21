50499d57da77f19e30495a229bec71fccf7fff5c
Edwin Guzman <edwin.gzmn@gmail.com>
2024-10-21 17:22:08 -0400

    Updating new relic and how env vars are read for it to work

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nrExternals = require("@newrelic/next/load-externals");
+// This is found in the codebase example but both seem to work and send data
+// correctly. Keep the above but can use the bottom for debugging.
+// const nrExternals = require('newrelic/load-externals');
 
 const nextConfig = {
   experimental: {
+    // Without this setting, the Next.js compilation step will routinely
+    // try to import files such as `LICENSE` from the `newrelic` module.
+    // See https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages.
     serverComponentsExternalPackages: ["newrelic"],
   },
   reactStrictMode: false,
@@ -28,6 +34,9 @@ const nextConfig = {
     ],
   },
   generateEtags: false,
+  // In order for newrelic to effectively instrument a Next.js application,
+  // the modules that newrelic supports should not be mangled by webpack. Thus,
+  // we need to "externalize" all of the modules that newrelic supports.
   webpack: (config) => {
     nrExternals(config);
     return config;
