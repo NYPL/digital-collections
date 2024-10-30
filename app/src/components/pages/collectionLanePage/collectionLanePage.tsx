eeb7ac00783fb3fedd9561be79afdbacb66b33f0
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-10-30 12:33:41 -0400

    Tweaking loading state for cardsGrid

--- app/src/components/pages/collectionLanePage/collectionLanePage.tsx
+++ app/src/components/pages/collectionLanePage/collectionLanePage.tsx
@@ -42,7 +42,7 @@ export default function CollectionLanePage({ data }: any) {
     const params = new URLSearchParams();
     params.set("page", pageNumber.toString());
     setCurrentPage(pageNumber);
-    const url = `${pathname}?${params.toString()}}`;
+    const url = `${pathname}?${params.toString()}`;
     replace(url);
     const firstChild = firstCardRef.current?.children[0] as HTMLElement;
     firstChild?.focus();
@@ -81,6 +81,15 @@ export default function CollectionLanePage({ data }: any) {
         </>
       ) : (
         <>
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
+          <LaneLoading withTitle={false} />,
           <LaneLoading withTitle={false} />,
           <LaneLoading withTitle={false} />,
           <LaneLoading withTitle={false} />,
