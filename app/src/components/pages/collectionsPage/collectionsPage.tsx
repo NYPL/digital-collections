e434c84a4a251aa11a932405387c46449d3d919c
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-10-30 15:59:50 -0700

    handleSearch and handleChange work

--- app/src/components/pages/collectionsPage/collectionsPage.tsx
+++ app/src/components/pages/collectionsPage/collectionsPage.tsx
@@ -21,40 +21,79 @@ import { headerBreakpoints } from "../../../utils/breakpoints";
 import { CardsGrid } from "../../grids/cardsGrid";
 import LaneLoading from "../../lane/laneLoading";
 import { slugToString, totalNumPages } from "../../../utils/utils";
+import type { SyntheticEvent } from "react";
 
 export const CollectionsPage = ({ data }) => {
   const router = useRouter();
   const { replace } = useRouter();
-  const queryParams = useSearchParams();
-  const query = queryParams.toString();
+
+  const searchParams = useSearchParams();
+  const query = searchParams.toString();
+
+  console.log("searchParams is: ", searchParams);
+  console.log("query is: ", query);
   const pathname = usePathname();
-  const keyword = queryParams.get("collection_keyword");
+  const keyword = searchParams.get("collection_keyword");
 
   const [isLoaded, setIsLoaded] = useState(false);
+
   const [currentPage, setCurrentPage] = useState(
-    Number(queryParams.get("page")) || 1
+    Number(searchParams.get("page")) || 1
   );
 
+  const [currentSort, setCurrentSort] = useState(
+    Number(searchParams.get("sort")) || "date-desc"
+  );
+
+  const [currentCollectionKeyword, setCurrentCollectionKeyword] = useState(
+    Number(searchParams.get("collection_keyword")) || ""
+  );
+
+  // pagination
   const numFound = data.numFound ? data.numFound : data.numResults;
   const totalPages = totalNumPages(numFound, data.perPage);
+
   // console.log("data: ", data);
 
   // search
-  // function handleSearch(term: string) {
+  // function handleSubmit(term: string) {
   //   const params = new URLSearchParams();
-  //   console.log(term);
+  //   console.log("term is", term);
   //   if (term) {
   //     params.set("collection_keyword", term);
   //   } else {
   //     params.delete("collection_keyword");
   //   }
-  //   replace(`${pathname}?${queryParams.toString()}`);
+  //   console.log("params in handleSearch are: ", params)
+  //   replace(`${pathname}?${searchParams.toString()}`);
   // }
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const searchParams = {
+      collection_keyword: currentCollectionKeyword,
+      sort: currentSort,
+      page: currentPage,
+    };
+
+    const queryString = createQueryStringFromObject(searchParams);
+    console.log("queryString is: ", queryString);
+    await router.push(`${pathname}?${queryString}`);
+  };
+
+  const handleChange = (
+    e: SyntheticEvent,
+    setValue: Dispatch<SetStateAction<string>>
+  ) => {
+    const target = e.target as HTMLInputElement;
+    setValue(target.value);
+  };
 
   // pagination
   const updatePageURL = async (pageNumber: number) => {
     const params = new URLSearchParams();
-    console.log("params are", params);
+    console.log("params in updatePageURL are", params);
 
     params.set("page", pageNumber.toString());
     setCurrentPage(pageNumber);
@@ -65,9 +104,23 @@ export const CollectionsPage = ({ data }) => {
   const createQueryString = (name, value) => {
     const params = new URLSearchParams();
     params.set(name, value);
+    console.log("params in createQueryString are: ", params);
+
     return params.toString();
   };
 
+  const createQueryStringFromObject = (paramObj) => {
+    const params = new URLSearchParams();
+    console.log("paramObj is: ", paramObj);
+    Object.keys(paramObj).map((name, value) => {
+      console.log("name: ", name);
+      console.log("value: ", paramObj[name]);
+      params.set(name.toString(), paramObj[name].toString());
+    });
+    console.log("params in createQueryStringFromObject are: ", params);
+    // params.set(name, value);
+    return params.toString();
+  };
   // sort
   function onMenuClick(id) {
     query
@@ -123,9 +176,10 @@ export const CollectionsPage = ({ data }) => {
             isClearable: true,
             labelText: "Search by collection title",
             name: "collection_keyword",
-            placeholder: "Search by collection title",
+            placeholder: "Search by collection title", // TODO: currntCollectionKeyword
+            onChange: (e) => handleChange(e, setCurrentCollectionKeyword),
           }}
-          onSubmit={function (event: React.FormEvent): void {}} // fix
+          onSubmit={handleSubmit} // TODO: fix
           // onChange={(e) => {
           //   handleSearch(e.target.value);
           // }}
@@ -142,7 +196,7 @@ export const CollectionsPage = ({ data }) => {
         <Menu
           showSelectionAsLabel
           showLabel
-          selectedItem="date-desc"
+          selectedItem="date-desc" // TODO: currentSort
           labelText={"Sort By"}
           listItemsData={[
             {
