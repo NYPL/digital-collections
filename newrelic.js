e5ba80dbe41a359cc31657c8886210bfdd40ecf5
Edwin Guzman <edwin.gzmn@gmail.com>
2024-11-08 16:47:41 -0500

    reverting commenting default nr config

--- newrelic.js
+++ newrelic.js
@@ -20,28 +20,27 @@ exports.config = {
    * will be captured for all traces, unless otherwise specified in a destination's
    * attributes include/exclude lists.
    */
-  // Commented to try the defaults from New Relic
-  // allow_all_headers: true,
-  // attributes: {
-  //   /**
-  //    * Prefix of attributes to exclude from all destinations. Allows * as wildcard
-  //    * at end.
-  //    *
-  //    * NOTE: If excluding headers, they must be in camelCase form to be filtered.
-  //    *
-  //    * @name NEW_RELIC_ATTRIBUTES_EXCLUDE
-  //    */
-  //   exclude: [
-  //     "request.headers.cookie",
-  //     "request.headers.authorization",
-  //     "request.headers.proxyAuthorization",
-  //     "request.headers.setCookie*",
-  //     "request.headers.x*",
-  //     "response.headers.cookie",
-  //     "response.headers.authorization",
-  //     "response.headers.proxyAuthorization",
-  //     "response.headers.setCookie*",
-  //     "response.headers.x*",
-  //   ],
-  // },
+  allow_all_headers: true,
+  attributes: {
+    /**
+     * Prefix of attributes to exclude from all destinations. Allows * as wildcard
+     * at end.
+     *
+     * NOTE: If excluding headers, they must be in camelCase form to be filtered.
+     *
+     * @name NEW_RELIC_ATTRIBUTES_EXCLUDE
+     */
+    exclude: [
+      "request.headers.cookie",
+      "request.headers.authorization",
+      "request.headers.proxyAuthorization",
+      "request.headers.setCookie*",
+      "request.headers.x*",
+      "response.headers.cookie",
+      "response.headers.authorization",
+      "response.headers.proxyAuthorization",
+      "response.headers.setCookie*",
+      "response.headers.x*",
+    ],
+  },
 };
