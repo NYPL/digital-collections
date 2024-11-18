ed4a4a795a208076b9b45a88469edad04c272100
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-11-18 16:56:25 -0500

    Updating copy for displaying pagination

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -113,5 +113,5 @@ export function displayResults(
 ) {
   const start = (page - 1) * perPage + 1;
   const end = Math.min(page * perPage, numFound);
-  return `Results: ${start}-${end} of ${numFound}`;
+  return `${start}-${end} of ${numFound}`;
 }
