0559b5d0095d67da8a5ce969de389176ad337152
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-11-19 11:27:01 -0500

    Just for QA

--- app/src/models/collectionCard.ts
+++ app/src/models/collectionCard.ts
@@ -15,7 +15,7 @@ export class CollectionCardModel {
   constructor(data: any) {
     this.uuid = data.uuid;
     this.title = data.title;
-    this.url = `/collections/${data.uuid}`;
+    this.url = `https://qa-digitalcollections.nypl.org/collections/${data.uuid}`;
     this.imageID = data.image_id || data.imageID;
     this.imageURL = imageURL(data.imageID, "full", "288,", "0");
     this.numberOfDigitizedItems =
