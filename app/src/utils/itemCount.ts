e2190468a9712e3d536e33b031b08dff7a4fcc74
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-11-20 14:52:20 -0500

    For readability

--- app/src/utils/itemCount.ts
+++ app/src/utils/itemCount.ts
@@ -7,17 +7,17 @@ import { apiResponse } from "./api";
 export const getItemsCountFromCollections = async (uuids: string[]) => {
   const results = {};
   const apiUrl = `${process.env.API_URL}/api/v2/collections/`;
-  await Promise.all(
-    uuids.map(async (uuid) => {
-      try {
-        const response = await apiResponse(`${apiUrl}${uuid}`);
-        const numItems = response.numItems || 0;
-        results[uuid] = numItems;
-      } catch (error) {
-        console.error(`Error fetching collection ${uuid}:`, error.message);
-        results[uuid] = 0;
-      }
-    })
-  );
+  const fetchPromises = uuids.map(async (uuid) => {
+    try {
+      const response = await apiResponse(`${apiUrl}${uuid}`);
+      const numItems = response.numItems || 0;
+      results[uuid] = numItems;
+    } catch (error) {
+      console.error(`Error fetching collection ${uuid}:`, error.message);
+      results[uuid] = 0;
+    }
+  });
+
+  await Promise.all(fetchPromises);
   return results;
 };
