e8e110c5f184e54ec7c0a2b8b935403ee9a20ed9
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-12-20 12:51:55 -0800

    noticeError is the only thing working

--- app/test-error/page.tsx
+++ app/test-error/page.tsx
@@ -3,12 +3,6 @@ import { useEffect } from "react";
 
 export default function TestError() {
   useEffect(() => {
-    // Send error to New Relic
-    if ((window as any).newrelic) {
-      (window as any).newrelic.log("Test error for New Relic", {
-        level: "warn",
-      });
-    }
-    throw new Error("Test error");
+    throw new Error("Test error for New Relic");
   }, []);
 }
