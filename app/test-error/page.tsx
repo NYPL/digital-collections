41eb8f195d1fe8e62a0bc181f155cc92b71c57d5
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-12-20 07:07:07 -0800

    Adding new relic log to the test error page

--- app/test-error/page.tsx
+++ app/test-error/page.tsx
@@ -3,6 +3,10 @@ import { useEffect } from "react";
 
 export default function TestError() {
   useEffect(() => {
+    // Send error to New Relic
+    if ((window as any).newrelic) {
+      (window as any).newrelic.log("Test error", { level: "warn" });
+    }
     throw new Error("Test error");
   }, []);
 }
