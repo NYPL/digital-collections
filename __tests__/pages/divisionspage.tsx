3437e633205dc80df68a4aecc229372edbaec6f8
Alessandra Vertrees <alessandravertrees@nypl.org>
2024-12-20 11:18:44 -0500

    create files for other tests

---
+++ __tests__/pages/divisionspage.tsx
@@ -0,0 +1,29 @@
+import { render } from "@testing-library/react";
+import { axe } from "jest-axe";
+import React from "react";
+import { mockDivisionsResponse } from "__tests__/__mocks__/data/api/mockDivisionsResponse";
+import DivisionsPage from "@/src/components/pages/divisionsPage/divisionsPage";
+import { useRouter } from "next/navigation";
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+  usePathname: jest.fn(),
+}));
+
+beforeEach(() => {
+  (useRouter as jest.Mock).mockImplementation(() => ({
+    pathname: "/divisions",
+  }));
+});
+
+describe.skip("Divisions page Accessibility", () => {
+  it("passes axe accessibility test", async () => {
+    const { container } = render(
+      <DivisionsPage
+        summary={mockDivisionsResponse.summary}
+        divisions={mockDivisionsResponse.divisions}
+      />
+    );
+    expect(await axe(container)).toHaveNoViolations();
+  }, 60000);
+});
