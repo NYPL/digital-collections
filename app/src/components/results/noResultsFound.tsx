b6668d4ca979859bf028d515c40dfd55e6fa5780
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-01-10 14:23:58 -0500

    An option for displaying

--- app/src/components/results/noResultsFound.tsx
+++ app/src/components/results/noResultsFound.tsx
@@ -6,8 +6,10 @@ import {
 } from "@nypl/design-system-react-components";
 import styles from "../../styles/Collections.module.css";
 
-export default function NoResultsFound({ searchTerm }) {
-  const headingText = `No Results for "${searchTerm}"`;
+export default function NoResultsFound({ searchTerm, page }) {
+  const headingText = `No Results for "${searchTerm}"${
+    page > 1 ? `, page ${page}` : ``
+  }`;
   return (
     <Flex
       flexDir="column"
