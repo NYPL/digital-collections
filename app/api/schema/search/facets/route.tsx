948ddbaa840564191c167ae738663c6015a73681
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-16 13:21:00 -0500

    move search schemas into /api/schema

--- app/api/schema/search/facets/route.tsx
+++ app/api/schema/search/facets/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import searchFacetsSchema from "../../../src/data/schemas/searchFacetsSchema";
+import searchFacetsSchema from "../../../../src/data/schemas/searchFacetsSchema";
 
 export const GET = async () => {
   return NextResponse.json(searchFacetsSchema, { status: 200 });
