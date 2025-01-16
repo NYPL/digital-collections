9f2c6e495d1d874d4f2156641b5bebbf1271de67
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-16 13:32:19 -0500

    update searchSchema and collectionsLandingPageSchema data files

---
+++ app/src/data/schemas/collectionSchema.ts
@@ -0,0 +1,12 @@
+const collectionObject = {
+  uuid: "string",
+  recordType: "collection | sub-collection",
+  title: "string",
+  url: "string",
+  imageID: "string | null",
+  imageURL: "string",
+  numberOfDigitizedItems: "number",
+  containsOnSiteMaterials: "boolean",
+};
+
+export default collectionObject;
---
+++ app/src/data/schemas/itemObject.ts
@@ -0,0 +1,12 @@
+const itemObject = {
+  uuid: "string",
+  recordType: "item",
+  title: "string",
+  url: "string",
+  imageID: "string | null",
+  imageURL: "string",
+  contentType: "image | multiple images | audio | video | pdf",
+  onSiteOnly: "boolean",
+};
+
+export default itemObject;
