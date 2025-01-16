a5f6731fc6e71b4d9357595b67bff5b9ecd23d3d
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-16 14:34:28 -0500

    collection endpoint schema

---
+++ app/api/schema/collections/route.tsx
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import collectionsSchema from "../../../../src/data/schemas/collectionsSchema";
+
+export const GET = async () => {
+  return NextResponse.json(collectionsSchema, { status: 200 });
+};
+
+// http://localhost:3000/api/schema/collections/:uuid
