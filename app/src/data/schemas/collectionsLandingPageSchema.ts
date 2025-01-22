4da1bbc997548a669b26bbc862a27aca6d520bc2
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-22 14:29:11 -0500

    update collection landing schema

--- app/src/data/schemas/collectionsLandingPageSchema.ts
+++ app/src/data/schemas/collectionsLandingPageSchema.ts
@@ -1,38 +1,60 @@
 const collectionsLandingPageSchema = {
   response: {
+    // Keyword included in search
+    keyword: "string",
+    // Pagination fields
     numResults: "integer",
     page: "integer",
     perPage: "integer",
+    // Sort fields
+    sort: "string ie: title DESC",
+    // Filter fields
+    filterField: "string",
     items: [
       {
         uuid: "string",
-        recordType: "item",
+        recordType: "sub-collection | item",
         title: "string",
         url: "string",
         imageID: "string | null",
         imageURL: "string",
-        contentType: "image | multiple images | audio | video | pdf",
-        onSiteOnly: "boolean",
+        numberOfDigitizedItems: "number",
+        containsOnSiteMaterials: "boolean | null if type is `item`",
+        contentType: "image | multiple images | audio | video | pdf | null",
+        onSiteOnly:
+          "boolean | null if type is `collection` or `sub-collection`",
+        highlights: "{ highlighted_field_name:[ string ] }",
+        firstIndexed_dt: "date",
       },
       {
         uuid: "string",
-        recordType: "item",
+        recordType: "collection | sub-collection | item",
         title: "string",
         url: "string",
         imageID: "string | null",
         imageURL: "string",
-        contentType: "image | multiple images | audio | video | pdf",
-        onSiteOnly: "boolean",
+        numberOfDigitizedItems: "number",
+        containsOnSiteMaterials: "boolean | null if type is `item`",
+        contentType: "image | multiple images | audio | video | pdf | null",
+        onSiteOnly:
+          "boolean | null if type is `collection` or `sub-collection`",
+        highlights: "{ highlighted_field_name:[ string ] }",
+        firstIndexed_dt: "date",
       },
       {
         uuid: "string",
-        recordType: "item",
+        recordType: "collection | sub-collection | item",
         title: "string",
         url: "string",
         imageID: "string | null",
         imageURL: "string",
-        contentType: "image | multiple images | audio | video | pdf",
-        onSiteOnly: "boolean",
+        numberOfDigitizedItems: "number",
+        containsOnSiteMaterials: "boolean | null if type is `item`",
+        contentType: "image | multiple images | audio | video | pdf | null",
+        onSiteOnly:
+          "boolean | null if type is `collection` or `sub-collection`",
+        highlights: "{ highlighted_field_name:[ string ] }",
+        firstIndexed_dt: "date",
       },
     ],
   },
