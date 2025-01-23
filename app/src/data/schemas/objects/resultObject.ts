1e86409a62d83fbc5f93253411e37a6e63658325
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-23 12:35:54 -0500

    change highlights field to object

--- app/src/data/schemas/objects/resultObject.ts
+++ app/src/data/schemas/objects/resultObject.ts
@@ -9,7 +9,7 @@ const resultObject = {
   containsAVMaterial: "boolean", // keeping bc the logic exists and it's already there
   containsMultipleCaptures: "boolean", // used to determine whether or not an item should display the "multiple images" tag
   contentType: "image | audio | video | pdf | null", // null
-  highlights: "{ highlighted_field_name:[ string ] }",
+  highlights: { highlighted_field_name: ["string"] },
   firstIndexed_dt: "date",
 };
 
