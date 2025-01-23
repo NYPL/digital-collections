b0d7d0f129524d41ed6929906d45374057aec5ad
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-23 12:35:54 -0500

    change highlights field to object

--- app/src/data/schemas/searchSchema.ts
+++ app/src/data/schemas/searchSchema.ts
@@ -16,42 +16,42 @@ const searchSchema = {
         uuid: "string",
         recordType: "collection | sub-collection | item",
         title: "string",
-        url: "string",
+        url: "string", // probably don't need?
         imageID: "string | null",
         numberOfDigitizedItems: "number",
         containsOnSiteMaterial: "boolean",
         containsAVMaterial: "boolean", // keeping bc the logic exists and it's already there
         containsMultipleCaptures: "boolean", // used to determine whether or not an item should display the "multiple images" tag
         contentType: "image | audio | video | pdf | null", // null
-        highlights: "{ highlighted_field_name:[ string ] }",
+        highlights: { highlighted_field_name: ["string"] },
         firstIndexed_dt: "date",
       },
       {
         uuid: "string",
         recordType: "collection | sub-collection | item",
         title: "string",
-        url: "string",
+        url: "string", // probably don't need?
         imageID: "string | null",
         numberOfDigitizedItems: "number",
         containsOnSiteMaterial: "boolean",
         containsAVMaterial: "boolean", // keeping bc the logic exists and it's already there
         containsMultipleCaptures: "boolean", // used to determine whether or not an item should display the "multiple images" tag
         contentType: "image | audio | video | pdf | null", // null
-        highlights: "{ highlighted_field_name:[ string ] }",
+        highlights: { highlighted_field_name: ["string"] },
         firstIndexed_dt: "date",
       },
       {
         uuid: "string",
         recordType: "collection | sub-collection | item",
         title: "string",
-        url: "string",
+        url: "string", // probably don't need?
         imageID: "string | null",
         numberOfDigitizedItems: "number",
         containsOnSiteMaterial: "boolean",
         containsAVMaterial: "boolean", // keeping bc the logic exists and it's already there
         containsMultipleCaptures: "boolean", // used to determine whether or not an item should display the "multiple images" tag
         contentType: "image | audio | video | pdf | null", // null
-        highlights: "{ highlighted_field_name:[ string ] }",
+        highlights: { highlighted_field_name: ["string"] },
         firstIndexed_dt: "date",
       },
     ],
