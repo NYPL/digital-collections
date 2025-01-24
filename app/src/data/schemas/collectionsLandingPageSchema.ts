1f542751829c686b79cc2d1f2c45d59644e0310a
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-24 13:50:32 -0500

    add checkbox fields and facets to search

--- app/src/data/schemas/collectionsLandingPageSchema.ts
+++ app/src/data/schemas/collectionsLandingPageSchema.ts
@@ -1,59 +1,6 @@
 const collectionsLandingPageSchema = {
   response: {
-    // Keyword included in search
-    keyword: "string",
-    // Pagination fields
-    numResults: "integer",
-    page: "integer",
-    perPage: "integer",
-    // Sort fields
-    sort: "string ie: title DESC",
-    // Filter fields
-    filterField: "string",
-    results: [
-      {
-        uuid: "string",
-        recordType: "collection | sub-collection | item",
-        title: "string",
-        url: "string", // probably don't need?
-        imageID: "string | null",
-        numberOfDigitizedItems: "number",
-        containsOnSiteMaterial: "boolean",
-        containsAVMaterial: "boolean", // keeping bc the logic exists and it's already there
-        containsMultipleCaptures: "boolean", // used to determine whether or not an item should display the "multiple images" tag
-        contentType: "image | audio | video | pdf | null", // null
-        highlights: { highlighted_field_name: ["string"] },
-        firstIndexed_dt: "date",
-      },
-      {
-        uuid: "string",
-        recordType: "collection | sub-collection | item",
-        title: "string",
-        url: "string", // probably don't need?
-        imageID: "string | null",
-        numberOfDigitizedItems: "number",
-        containsOnSiteMaterial: "boolean",
-        containsAVMaterial: "boolean", // keeping bc the logic exists and it's already there
-        containsMultipleCaptures: "boolean", // used to determine whether or not an item should display the "multiple images" tag
-        contentType: "image | audio | video | pdf | null", // null
-        highlights: { highlighted_field_name: ["string"] },
-        firstIndexed_dt: "date",
-      },
-      {
-        uuid: "string",
-        recordType: "collection | sub-collection | item",
-        title: "string",
-        url: "string", // probably don't need?
-        imageID: "string | null",
-        numberOfDigitizedItems: "number",
-        containsOnSiteMaterial: "boolean",
-        containsAVMaterial: "boolean", // keeping bc the logic exists and it's already there
-        containsMultipleCaptures: "boolean", // used to determine whether or not an item should display the "multiple images" tag
-        contentType: "image | audio | video | pdf | null", // null
-        highlights: { highlighted_field_name: ["string"] },
-        firstIndexed_dt: "date",
-      },
-    ],
+    // TODO: Awaiting product requirements
   },
 };
 
