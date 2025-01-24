49b74aa79dc04550df28bbcd7bcf6086e8c8f889
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-01-24 13:56:48 -0500

    remove facet objects

--- app/src/data/schemas/objects/factetObject.ts
+++
@@ -1,8 +0,0 @@
-const facetObject = {
-  value: "solr-field",
-  count: "integer",
-  label: "display field",
-};
-
-export default facetObject;
-//https://www.nypl.org/research/research-catalog/api/search?q=cats
