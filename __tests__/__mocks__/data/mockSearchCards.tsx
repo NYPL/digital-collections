109fc38fdb53ac5db8cc0cb526ea110743663eaf
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-01-29 10:28:34 -0500

    Halfway through card

--- __tests__/__mocks__/data/mockSearchCards.tsx
+++ __tests__/__mocks__/data/mockSearchCards.tsx
@@ -19,7 +19,7 @@ export const mockSearchCards: SearchCardType[] = [
   {
     uuid: "50370c90-cabb-013c-da64-0242ac110002",
     title: "Sarah Myers Blackwell sitting in a tree",
-    url: "https://digitalcollections.nypl.org/collections/posada-collection#/?tab=navigation",
+    url: "https://digitalcollections.nypl.org/items/50370c90-cabb-013c-da64-0242ac110002",
     imageID: "58886955",
     imageURL:
       "https://iiif.nypl.org/iiif/2/58886955/square/!288,288/0/default.jpg",
@@ -27,8 +27,41 @@ export const mockSearchCards: SearchCardType[] = [
     containsOnSiteMaterial: false,
     recordType: "item",
     containsAVMaterial: false,
+    containsMultipleCaptures: false,
     contentType: "image",
     highlights: [],
     firstIndexed: "1907-02-01T00:00:00Z",
+  },
+  {
+    uuid: "e5462600-c5d9-012f-a6a3-58d385a7bc34",
+    title: "Farm Security Administration Photographs",
+    url: "https://digitalcollections.nypl.org/collections/farm-security-administration-photographs#/?tab=navigation",
+    imageID: "1952272",
+    imageURL:
+      "https://iiif.nypl.org/iiif/2/1952272/square/!288,288/0/default.jpg",
+    numberOfDigitizedItems: 36,
+    containsOnSiteMaterial: false,
+    recordType: "collection",
+    containsAVMaterial: true,
+    contentType: "image",
+    highlights: [],
+    firstIndexed: "1907-02-01T00:00:00Z",
+  },
+  {
+    uuid: "12563fb0-63a2-013b-bd44-0242ac110003",
+    title:
+      "Reading room of the Schomburg Collection at the 135th Street Branch Library. Lawrence Reddick, curator, seated at right",
+    imageID: "58613608",
+    url: "https://digitalcollections.nypl.org/items/12563fb0-63a2-013b-bd44-0242ac110003",
+    imageURL:
+      "https://iiif.nypl.org/iiif/2/58613608/square/!288,288/0/default.jpg",
+    recordType: "item",
+    numberOfDigitizedItems: 1,
+    containsOnSiteMaterial: true,
+    containsAVMaterial: false,
+    containsMultipleCaptures: true,
+    contentType: "image",
+    highlights: [],
+    firstIndexed: "",
   },
 ];
--- app/src/types/SearchCardType.ts
+++ app/src/types/SearchCardType.ts
@@ -1,15 +1,17 @@
+export type SearchResultRecordType = "collection" | "sub-collection" | "item";
+export type SearchResultContentType = "image" | "audio" | "video" | "pdf";
 export interface SearchCardType {
   title: string;
   uuid: string;
   url: string;
   imageID: string;
   imageURL: string;
-  recordType: "collection" | "sub-collection" | "item";
+  recordType: SearchResultRecordType;
   numberOfDigitizedItems: number;
   containsOnSiteMaterial: boolean;
   containsAVMaterial: boolean;
   containsMultipleCaptures?: boolean;
-  contentType: "image" | "audio" | "video" | "pdf";
+  contentType: SearchResultContentType;
   highlights: string[];
   firstIndexed: string;
 }
