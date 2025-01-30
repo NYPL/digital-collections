d10d03c44f5f9a4873d43105fbf5db6f290947b6
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-01-30 18:48:48 -0500

    Start of tests

---
+++ app/src/components/card/searchCard.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { SearchCard } from "./searchCard";
+
+describe("Search card displaying collection result", () => {
+  it("renders the correct heading with the provided title", () => {});
+
+  it("renders the badge when containsOnSiteMaterial is true", () => {});
+
+  it("does not render the badge when containsOnSiteMaterial is false", () => {});
+
+  it("wraps card in the correct link", () => {});
+
+  it("renders the correct content type tag", () => {});
+});
+
+describe("Search card displaying item result", () => {
+  it("renders the correct heading with the provided title", () => {});
+
+  it("wraps card in the correct link", () => {});
+
+  it("renders the badge when containsOnSiteMaterial is true", () => {});
+
+  it("does not render the badge when containsOnSiteMaterial is false", () => {});
+
+  it("renders the correct content type tag", () => {});
+});
