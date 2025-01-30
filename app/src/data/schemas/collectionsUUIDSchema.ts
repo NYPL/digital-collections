c80dd7d8866670116a2fc268b1b5e53bde72c7d3
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-01-30 15:00:04 -0500

    more commenting

--- app/src/data/schemas/collectionsUUIDSchema.ts
+++ app/src/data/schemas/collectionsUUIDSchema.ts
@@ -6,6 +6,15 @@ const collectionsUUIDSchema = {
     // catalog link
     keyDate: "string", // date created? So not when it was indexed
     // parent division
+    // description
+    // topics: many
+    // genres: many
+    //type of resource: many
+    //identifiers
+    // collection history
+    // related resources
+    // COLLECTION STRUCTURE
+    // listing facets?
   },
 };
 
