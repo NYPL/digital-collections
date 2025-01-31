3eda88de85fa5b71159f0b993fdcfbe83f13f2d3
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-01-31 10:29:54 -0500

    Test

--- app/src/components/card/searchCard.test.tsx
+++ app/src/components/card/searchCard.test.tsx
@@ -1,26 +1,68 @@
 import { render, screen } from "@testing-library/react";
 import { SearchCard } from "./searchCard";
+import { mockSearchCards } from "__tests__/__mocks__/data/mockSearchCards";
+
+const collectionResultData = mockSearchCards[0];
+const itemResultData = mockSearchCards[3];
 
 describe("Search card displaying collection result", () => {
-  it("renders the correct heading with the provided title", () => {});
+  it("renders the correct heading with the provided title", () => {
+    render(<SearchCard result={collectionResultData} keywords={[]} />);
+    const headingElement = screen.getByRole("heading", {
+      name: collectionResultData.title,
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
 
-  it("renders the badge when containsOnSiteMaterial is true", () => {});
+  it("renders the badge when containsOnSiteMaterial is true", () => {
+    render(<SearchCard result={collectionResultData} keywords={[]} />);
+    const badgeElement = screen.getByText(/Contains on-site materials/i);
+    expect(badgeElement).toBeInTheDocument();
+  });
 
-  it("does not render the badge when containsOnSiteMaterial is false", () => {});
+  it("wraps card in the correct link", () => {
+    render(<SearchCard result={collectionResultData} keywords={[]} />);
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute(
+      "href",
+      expect.stringContaining("/collections/posada-collection#/?tab=navigation")
+    );
+  });
 
-  it("wraps card in the correct link", () => {});
-
-  it("renders the correct content type tag", () => {});
+  it("renders the correct content type tag", () => {
+    render(<SearchCard result={collectionResultData} keywords={[]} />);
+    const tagElement = screen.getByText("Collection");
+    expect(tagElement).toBeInTheDocument();
+  });
 });
 
 describe("Search card displaying item result", () => {
-  it("renders the correct heading with the provided title", () => {});
+  it("renders the correct heading with the provided title", () => {
+    render(<SearchCard result={itemResultData} keywords={[]} />);
+    const headingElement = screen.getByRole("heading", {
+      name: itemResultData.title,
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
 
-  it("wraps card in the correct link", () => {});
+  it("wraps card in the correct link", () => {
+    render(<SearchCard result={itemResultData} keywords={[]} />);
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute(
+      "href",
+      expect.stringContaining("items/12563fb0-63a2-013b-bd44-0242ac110003")
+    );
+  });
 
-  it("renders the badge when containsOnSiteMaterial is true", () => {});
+  it("renders the badge when containsOnSiteMaterial is true", () => {
+    render(<SearchCard result={itemResultData} keywords={[]} />);
+    const badgeElement = screen.getByText(/Available onsite only/i);
+    expect(badgeElement).toBeInTheDocument();
+  });
 
-  it("does not render the badge when containsOnSiteMaterial is false", () => {});
-
-  it("renders the correct content type tag", () => {});
+  it("renders the correct content type tag", () => {
+    render(<SearchCard result={itemResultData} keywords={[]} />);
+    const tagElement = screen.getByText("Multiple images");
+    expect(tagElement).toBeInTheDocument();
+  });
 });
