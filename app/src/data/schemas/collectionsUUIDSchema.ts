1a6091ee1f552dcd242251ba104503caccacaf13
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-03 12:27:22 -0500

    collections schema from feb 3

--- app/src/data/schemas/collectionsUUIDSchema.ts
+++ app/src/data/schemas/collectionsUUIDSchema.ts
@@ -1,25 +1,26 @@
 const collectionsUUIDSchema = {
-  response: {
-    title: "string",
-    uuid: "string",
-    // where is all the info/headers under "About" coming from– that should be combined into this endpoint
-    // finding aid link
-    // catalog link
-    // archives link (is it available in archives, this can be if archive id is returned)
-    keyDate: "string", // date created? So not when it was indexed
-    // library locations (parent division?)
+  title: "string",
+  uuid: "string",
+  // for the below fields: if not returned, assuming there isn't one
+  archivesCollectionID: "number", //to be used in a archives.nypl.org url
+  bNumber: "number", //to be used in a nypl.org/research/research-catalog/bib/ url
+  keyDate: "string",
+  yearBegin: "datetime",
+  divisionTitle: "string", //divisionFullName
+  divisionSlug: "string", //divisionShortName - this will be manually pulled from database, slug for division
+  shelfLocator: "string", //shelfLocator_mtxt_s (NYPL cleaned off)
+  genres: "string[]",
+  topics: "string[]", //topic_mtxt_s
+  typeOfResource: "string[]",
+  //identifiers: "string[]" (need to go through DC to see which ones we return beyond bNumber, MSS ID, and uuid)
+  contentNote: "string",
 
-    // topics: many
-    // genres: many
-    // type of resource = format: many
-    // identifiers (this would be enough for the catalog link and finding aid link?)
-    // collection history
-    // related resources
-
-    // COLLECTION STRUCTURE
-    // currently, it's at least the title on every subcollection?
-    // it's the number of results when you hit /subcollections
-  },
+  // related resources???
+  // COLLECTION STRUCTURE
+  // currently, it's at least the title on every subcollection?
+  // it's the number of results when you hit /subcollections
+  // **** slugs, short description, and long description only in DC database (like CMS), not in MMS
+  // maybe in MMS abstract in the mods?
 };
 
 export default collectionsUUIDSchema;
