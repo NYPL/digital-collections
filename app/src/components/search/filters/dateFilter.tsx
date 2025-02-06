6d35313f4966e5904515156e18929caa720df0f4
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-06 14:04:17 -0500

    Moving filters into components

---
+++ app/src/components/search/filters/dateFilter.tsx
@@ -0,0 +1,35 @@
+import {
+  Button,
+  Flex,
+  Heading,
+  TextInput,
+  Text,
+} from "@nypl/design-system-react-components";
+
+const DateFilter = () => {
+  return (
+    <>
+      <Heading size="heading6">Date range:</Heading>
+      <Flex gap="s" alignItems="center" marginBottom="m">
+        <TextInput
+          width="100px"
+          id="dateStart"
+          labelText="Start year"
+          placeholder="Start year"
+          showLabel={false}
+        />
+        <Text sx={{ marginBottom: "0" }}> to </Text>
+        <TextInput
+          width="100px"
+          id="dateStart"
+          labelText="End year"
+          showLabel={false}
+          placeholder="End year"
+        />
+        <Button id="date-filter-btn">Apply dates</Button>
+      </Flex>
+    </>
+  );
+};
+
+export default DateFilter;
--- app/src/components/search/filters/filters.tsx
+++ app/src/components/search/filters/filters.tsx
@@ -12,6 +12,8 @@ import {
 } from "@nypl/design-system-react-components";
 
 import SelectFilterGrid from "./selectFilterGrid";
+import RightsFilter from "./rightsFilter";
+import DateFilter from "./dateFilter";
 
 const Filters = ({ headingText }) => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -22,79 +24,26 @@ const Filters = ({ headingText }) => {
       <SelectFilterGrid
         filters={[
           {
-            facet: "Topic",
+            name: "Topic",
             options: [
               { name: "New York", count: 37 },
               { name: "New Jersey", count: 8 },
               { name: "Long Island", count: 12 },
             ],
           },
-          { facet: "Genre", options: [{ name: "New York", count: 37 }] },
-          { facet: "Format", options: [{ name: "New York", count: 8 }] },
-          { facet: "Collection", options: [{ name: "New York", count: 37 }] },
-          { facet: "Publishers", options: [{ name: "New York", count: 37 }] },
-          { facet: "Division", options: [{ name: "New York", count: 37 }] },
-          { facet: "Type", options: [{ name: "New York", count: 37 }] },
+          { name: "Genre", options: [{ name: "New York", count: 37 }] },
+          { name: "Format", options: [{ name: "New York", count: 8 }] },
+          { name: "Collection", options: [{ name: "New York", count: 37 }] },
+          { name: "Publishers", options: [{ name: "New York", count: 37 }] },
+          { name: "Division", options: [{ name: "New York", count: 37 }] },
+          { name: "Type", options: [{ name: "New York", count: 37 }] },
         ]}
         isExpanded={isExpanded}
       />
       {isExpanded && (
         <>
-          <Heading size="heading6">Date range:</Heading>
-          <Flex gap="s" alignItems="center" marginBottom="m">
-            <TextInput
-              width="100px"
-              id="dateStart"
-              labelText="Start year"
-              placeholder="Start year"
-              showLabel={false}
-            />
-            <Text sx={{ marginBottom: "0" }}> to </Text>
-            <TextInput
-              width="100px"
-              id="dateStart"
-              labelText="End year"
-              showLabel={false}
-              placeholder="End year"
-            />
-            <Button id="date-filter-btn">Apply dates</Button>
-          </Flex>
-          <Heading size="heading6">Show only:</Heading>
-          <RadioGroup
-            name="show-only-filters"
-            id="show-only-filters"
-            labelText="Show Only"
-            showLabel={false}
-            layout="row"
-            marginBottom="m"
-          >
-            <Radio
-              id="pd-checkbox"
-              labelText="Public domain"
-              value="pd-radio"
-            />
-            <Radio
-              id="online-radio"
-              labelText="Available online"
-              value="online-radio"
-            />
-            <Radio
-              id="onsite-radio"
-              labelText={
-                <Tooltip content="Somethin about on-site materials...missing copy">
-                  <span>
-                    Contains on-site materials{" "}
-                    <Icon
-                      size="medium"
-                      name="errorOutline"
-                      iconRotation="rotate180"
-                    />
-                  </span>
-                </Tooltip>
-              }
-              value="onsite-radio"
-            />
-          </RadioGroup>
+          <DateFilter />
+          <RightsFilter />
         </>
       )}
       <Button
