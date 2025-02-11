4b87cf631ca7ae4dfefeb8ce05fe2d92d6ee8b82
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-11 12:22:09 -0500

    Deeply messed up

--- app/src/components/search/filters/selectFilterGrid.tsx
+++ app/src/components/search/filters/selectFilterGrid.tsx
@@ -2,42 +2,38 @@ import { useState, useRef } from "react";
 import { Grid } from "@chakra-ui/react";
 import SelectFilter, { FilterCategory } from "./selectFilter";
 import useCloseDropDown from "@/src/hooks/useCloseDropDown";
+import SelectFilter2 from "./selectFilter2";
 
 type SelectFilterGridProps = {
   filters: FilterCategory[];
   isExpanded: boolean;
-  headingRef: React.RefObject<HTMLHeadingElement> | null;
 };
 
-const SelectFilterGrid = ({
-  filters,
-  isExpanded,
-  headingRef,
-}: SelectFilterGridProps) => {
-  const [openFilter, setOpenFilter] = useState<string | null>(null);
-  const gridRef = useRef<HTMLDivElement | null>(null);
+const SelectFilterGrid = ({ filters, isExpanded }: SelectFilterGridProps) => {
+  // const [openFilter, setOpenFilter] = useState<string | null>(null);
+  // const gridRef = useRef<HTMLDivElement | null>(null);
 
-  useCloseDropDown(() => setOpenFilter(null), gridRef);
+  // //useCloseDropDown(() => setOpenFilter(null), gridRef);
 
-  const handleToggle = (filterName: string) => {
-    setOpenFilter((prev) => (prev === filterName ? null : filterName));
-  };
+  // const handleToggle = (filterName: string) => {
+  //   setOpenFilter((prev) => (prev === filterName ? null : filterName));
+  // };
 
   return (
     <Grid
-      ref={gridRef}
+      //ref={gridRef}
       templateColumns="repeat(4, 1fr)"
       gap="m"
       marginBottom="m"
       width="full"
     >
       {(isExpanded ? filters : filters.slice(0, 4)).map((filter) => (
-        <SelectFilter
-          headingRef={headingRef}
+        <SelectFilter2
+          ref={headingRef}
           key={filter.name}
           filter={filter}
-          isOpen={openFilter === filter.name}
-          onToggle={() => handleToggle(filter.name)}
+          // isOpen={openFilter === filter.name}
+          // onToggle={() => handleToggle(filter.name)}
         />
       ))}
     </Grid>
