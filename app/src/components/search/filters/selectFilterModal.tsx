25eb999997dfa280b99f5dae722dd16fd22bce2f
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-12 11:16:34 -0500

    Focus management legend (me)

--- app/src/components/search/filters/selectFilterModal.tsx
+++ app/src/components/search/filters/selectFilterModal.tsx
@@ -1,19 +1,20 @@
-import { forwardRef, RefObject } from "react";
+import { forwardRef } from "react";
 import {
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
   ModalFooter,
-  ModalHeader,
   ModalOverlay,
   useDisclosure,
 } from "@chakra-ui/react";
 import type { FilterCategory } from "./selectFilter";
-import { Button } from "@nypl/design-system-react-components";
+import { Button, Box, Heading } from "@nypl/design-system-react-components";
+import DCSearchBar from "../dcSearchBar";
 
 type SelectFilterModalProps = {
   filter: FilterCategory;
+  onOpen: () => void;
+  onClose: () => void;
 };
 
 type FocusableElement = HTMLElement & { focus: () => void };
@@ -21,36 +22,85 @@ type FocusableElement = HTMLElement & { focus: () => void };
 const SelectFilterModal = forwardRef<
   HTMLHeadingElement,
   SelectFilterModalProps
->(({ filter }, headingRef?) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+>(({ filter, onOpen, onClose }, headingRef?) => {
+  const {
+    isOpen,
+    onOpen: chakraOnOpen,
+    onClose: chakraOnClose,
+  } = useDisclosure();
+
+  const handleOpen = () => {
+    chakraOnOpen();
+    onOpen();
+  };
+
+  const handleClose = () => {
+    chakraOnClose();
+    onClose();
+  };
+
   return (
     <>
       <Button
         className="modal"
         buttonType="link"
         id="modal-btn"
-        onClick={onOpen}
+        onClick={handleOpen}
       >{`View all ${filter.name.toLowerCase()}${
         filter.name === "Publishers" ? `` : `s`
       }`}</Button>
+
       <Modal
-        finalFocusRef={headingRef as RefObject<FocusableElement>}
+        finalFocusRef={headingRef as React.RefObject<FocusableElement>}
         isOpen={isOpen}
-        onClose={onClose}
+        onClose={handleClose}
       >
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{`${filter.name}${
-            filter.name === "Publishers" ? `` : `s`
-          }`}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody></ModalBody>
-
+        <ModalContent sx={{ borderRadius: "4px" }}>
+          <Box
+            sx={{
+              width: "100%",
+              bg: "ui.bg.default",
+              borderRadius: "4px",
+              borderBottom: "1px solid var(--ui-border-default, #BDBDBD)",
+              marginBottom: "s",
+            }}
+          >
+            <Heading
+              size="heading5"
+              paddingTop="s"
+              paddingLeft="s"
+              marginBottom="xs"
+            >
+              {`${filter.name}${filter.name === "Publishers" ? `` : `s`}`}
+            </Heading>
+          </Box>
+          <ModalBody>
+            <DCSearchBar
+              id={`search-${filter.name}-options`}
+              labelText={`Search ${filter.name}${
+                filter.name === "Publishers" ? `` : `s`
+              }`}
+              maxWrapperWidth="400px"
+              textInputProps={{
+                id: "filter-search-text",
+                isClearable: true,
+                labelText: `Search ${filter.name}${
+                  filter.name === "Publishers" ? `` : `s`
+                }`,
+                name: "filter_keywords",
+                placeholder: `Search ${filter.name.toLowerCase()}${
+                  filter.name === "Publishers" ? `` : `s`
+                }`,
+              }}
+              onSubmit={() => {}}
+            />
+          </ModalBody>
           <ModalFooter>
-            <Button mr={3} onClick={onClose} id={"close"}>
+            <Button mr={3} onClick={handleClose} id="close">
               Close
             </Button>
-            <Button id={"secondary"}>Secondary Action</Button>
+            <Button id="secondary">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
