47b0454aa974e8931971aea48ec1ee53ba94b62f
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-14 10:59:28 -0500

    Implementing on collections page, search manager, and tests

---
+++ __tests__/middleware/redirect.test.tsx
@@ -0,0 +1,43 @@
+import { middleware } from "middleware";
+import { NextRequest, NextResponse } from "next/server";
+
+jest.mock("next/server", () => {
+  return {
+    NextRequest: jest.fn(),
+    NextResponse: {
+      redirect: jest.fn(() => "redirect response"),
+      next: jest.fn(() => "next response"),
+    },
+  };
+});
+
+describe("redirects collection_keywords to q on collections search", () => {
+  let request: NextRequest;
+
+  beforeEach(() => {
+    request = {
+      nextUrl: new URL("http://localhost/collections?collection_keywords=test"),
+    } as NextRequest;
+  });
+
+  test("redirects collection_keywords to q", () => {
+    const response = middleware(request);
+
+    expect(NextResponse.redirect).toHaveBeenCalledWith(
+      "http://localhost/collections?q=test",
+      308
+    );
+    expect(response).toBe("redirect response");
+  });
+
+  test("goes through unchanged if no collection_keywords= param", () => {
+    request = {
+      nextUrl: new URL("http://localhost/collections?sort=title-asc"),
+    } as NextRequest;
+
+    const response = middleware(request);
+
+    expect(NextResponse.next).toHaveBeenCalled();
+    expect(response).toBe("next response");
+  });
+});
