b25234228d9b3871c949b26722d59fe3df76bf38
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-14 17:01:31 -0500

    More focus progress

--- app/src/components/collectionStructure/collectionStructure.tsx
+++ app/src/components/collectionStructure/collectionStructure.tsx
@@ -5,7 +5,7 @@ import {
   Text,
   Heading,
 } from "@nypl/design-system-react-components";
-import React, { useState } from "react";
+import React, { forwardRef, useState } from "react";
 
 export type CollectionChildProps = {
   title: string;
@@ -18,7 +18,8 @@ const AccordionItem = ({
   itemCount,
   children = [],
   level = 0,
-}: CollectionChildProps & { level?: number }) => {
+  headingRef,
+}: CollectionChildProps & { level?: number; headingRef }) => {
   const [isOpen, setIsOpen] = useState(false);
   const hasChildren = children.length > 0;
 
@@ -38,54 +39,66 @@ const AccordionItem = ({
         _hover={{ bg: "ui.hover.default" }}
         borderBottom="1px solid var(--ui-gray-medium, #BDBDBD)"
         paddingLeft={level > 0 ? level * 8 : "s"}
-        paddingTop="s"
+        paddingTop="m"
         paddingRight="s"
-        paddingBottom="s"
-        onClick={() => setIsOpen(!isOpen)}
+        paddingBottom="m"
+        onClick={() => {
+          setIsOpen(!isOpen);
+          // If the item is being opened, or it has no children
+          if (!isOpen || !hasChildren) {
+            setTimeout(() => {
+              headingRef?.current?.focus();
+            }, 100);
+          }
+        }}
       >
-        <Flex
-          width="100%"
-          justifyContent="space-between"
-          gap="s"
-          alignItems="center"
-        >
+        <Flex width="100%" alignItems="center">
           {hasChildren && (
-            <Icon size="small" name={isOpen ? "minus" : "plus"} />
+            <Icon
+              size="small"
+              name={isOpen ? "minus" : "plus"}
+              visibility={hasChildren ? "visible" : "hidden"}
+            />
           )}
           <Text
+            flex="1"
             marginBottom="0"
-            maxW="176px"
             whiteSpace="nowrap"
             overflow="hidden"
             textOverflow="ellipsis"
             fontWeight="500"
+            paddingLeft="s"
           >
             {title}
           </Text>
-          <span>{itemCount}</span>
+          <Box as="span" marginLeft="16px">
+            {itemCount}
+          </Box>
         </Flex>
       </Button>
       {hasChildren && (
-        <Collapse in={isOpen}>
-          <Box bg="white">
-            {children.map((child, index) => (
-              <AccordionItem
-                key={index}
-                itemCount={child.itemCount}
-                title={child.title}
-                level={level + 1}
-              >
-                {child.children}
-              </AccordionItem>
-            ))}
-          </Box>
+        <Collapse in={isOpen} style={{ overflow: "visible !important" }}>
+          {children.map((child, index) => (
+            <AccordionItem
+              key={index}
+              itemCount={child.itemCount}
+              title={child.title}
+              level={level + 1}
+              headingRef={headingRef}
+            >
+              {child.children}
+            </AccordionItem>
+          ))}
         </Collapse>
       )}
     </Box>
   );
 };
 
-const CollectionStructure = ({ data }: { data: CollectionChildProps[] }) => {
+const CollectionStructure = forwardRef<
+  HTMLHeadingElement,
+  { data: CollectionChildProps[] }
+>(({ data }, headingRef) => {
   return (
     <Flex flexDir="column">
       <Heading size="heading5"> Collection structure </Heading>
@@ -100,6 +113,7 @@ const CollectionStructure = ({ data }: { data: CollectionChildProps[] }) => {
             key={index}
             itemCount={item.itemCount}
             title={item.title}
+            headingRef={headingRef}
           >
             {item.children}
           </AccordionItem>
@@ -107,6 +121,8 @@ const CollectionStructure = ({ data }: { data: CollectionChildProps[] }) => {
       </Box>
     </Flex>
   );
-};
+});
+
+CollectionStructure.displayName = "CollectionStructure";
 
 export default CollectionStructure;
