be8e673cb3e33be87422cbd9258d5438fdace6f2
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-24 11:56:44 -0500

    Moving around imports and mock responses

--- app/search/index/page.tsx
+++ app/search/index/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import PageLayout from "../../src/components/pageLayout/pageLayout";
 import { createAdobeAnalyticsPageName } from "@/src/utils/utils";
-import { getSearchData } from "@/src/utils/apiHelpers";
+import { RepoApi } from "@/src/utils/apiClients";
 import { Metadata } from "next";
 import SearchPage from "@/src/components/pages/searchPage/searchPage";
-import { mockSearchResponse } from "__tests__/__mocks__/data/mockSearchResponse";
+import { mockSearchResponse } from "__tests__/__mocks__/data/collectionsApi/mockSearchResponse";
 import { Filter } from "@/src/types/FilterType";
 
 export interface SearchParams {
@@ -28,7 +28,7 @@ export type SearchProps = {
 export default async function Search({ searchParams }: SearchProps) {
   const pageName = searchParams.keywords ? "search-results" : "all-items";
 
-  const data = await getSearchData({
+  const data = await RepoApi.getSearchData({
     keyword: searchParams.keywords || "type_s:Item",
     sort: searchParams.sort,
     filters: searchParams.filters,
