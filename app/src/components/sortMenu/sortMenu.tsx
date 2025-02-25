0296f728c9cb9ddaa2334660a095026b43fcb6ab
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-25 14:07:23 -0500

    Sort menu component and constants updated

--- app/src/components/sortMenu/sortMenu.tsx
+++ app/src/components/sortMenu/sortMenu.tsx
@@ -1,50 +1,28 @@
-// import { Menu } from "@nypl/design-system-react-components";
+import { SearchManager } from "@/src/utils/searchManager";
+import { Menu } from "@nypl/design-system-react-components";
 
-// const SortMenu = (updateURL, searchManager, options) => {
-//   const menuItem (optionTitle, optionId) {
+type SortMenuProps = {
+  updateURL: (queryString: string) => Promise<void>;
+  searchManager: SearchManager;
+  options: Record<string, string>;
+};
 
-//   }
-//   return (
-//     <Menu
-//       showLabel
-//       selectedItem={collectionSearchManager.sort}
-//       labelText={`Sort by: ${
-//         COLLECTION_SORT_LABELS[collectionSearchManager.sort]
-//       }`}
-//       listItemsData={[
-//         {
-//           id: "date-desc",
-//           label: "Newest to oldest",
-//           onClick: () => {
-//             updateURL(collectionSearchManager.handleSortChange("date-desc"));
-//           },
-//           type: "action",
-//         },
-//         {
-//           id: "date-asc",
-//           label: "Oldest to newest",
-//           onClick: () => {
-//             updateURL(collectionSearchManager.handleSortChange("date-asc"));
-//           },
-//           type: "action",
-//         },
-//         {
-//           id: "title-asc",
-//           label: "Title A to Z",
-//           onClick: () => {
-//             updateURL(collectionSearchManager.handleSortChange("title-asc"));
-//           },
-//           type: "action",
-//         },
-//         {
-//           id: "title-desc",
-//           label: "Title Z to A",
-//           onClick: () => {
-//             updateURL(collectionSearchManager.handleSortChange("title-desc"));
-//           },
-//           type: "action",
-//         },
-//       ]}
-//     />
-//   );
-// };
+const SortMenu = ({ updateURL, searchManager, options }: SortMenuProps) => {
+  return (
+    <Menu
+      showLabel
+      selectedItem={searchManager.sort}
+      labelText={`Sort by: ${options[searchManager.sort]}`}
+      listItemsData={Object.entries(options).map(([id, label]) => ({
+        id,
+        label,
+        onClick: () => {
+          updateURL(searchManager.handleSortChange(id));
+        },
+        type: "action",
+      }))}
+    />
+  );
+};
+
+export default SortMenu;
