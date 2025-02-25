74ad831333aca740413e6d2fa20dbdda4833303a
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-25 17:17:21 -0500

    Updatign search manager and tests to account for different default sorts

--- __tests__/pages/collectionpage.test.tsx
+++ __tests__/pages/collectionpage.test.tsx
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react";
 import { mockItems } from "__tests__/__mocks__/data/mockItems";
 
 import { axe } from "jest-axe";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 jest.mock("next/navigation", () => ({
@@ -10,10 +11,25 @@ jest.mock("next/navigation", () => ({
   usePathname: jest.fn(),
 }));
 
+beforeEach(() => {
+  (useRouter as jest.Mock).mockImplementation(() => ({
+    pathname: "/collections/hello",
+  }));
+});
+
 describe("Collection page accessibility", () => {
+  const searchParams = {
+    keywords: "flower",
+    sort: "title-asc",
+    page: "2",
+  };
   it("passes axe accessibility test", async () => {
     const { container } = render(
-      <CollectionPage slug="Example collection" data={mockItems} />
+      <CollectionPage
+        searchParams={searchParams}
+        slug="Example collection"
+        data={mockItems}
+      />
     );
     expect(await axe(container)).toHaveNoViolations();
   }, 60000);
