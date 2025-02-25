f81cbacb6245237bde2181c85a23843282087fb4
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-02-25 12:52:28 -0500

    Start

---
+++ app/src/components/sortMenu/sortMenu.tsx
@@ -0,0 +1,50 @@
+// import { Menu } from "@nypl/design-system-react-components";
+
+// const SortMenu = (updateURL, searchManager, options) => {
+//   const menuItem (optionTitle, optionId) {
+
+//   }
+//   return (
+//     <Menu
+//       showLabel
+//       selectedItem={collectionSearchManager.sort}
+//       labelText={`Sort by: ${
+//         COLLECTION_SORT_LABELS[collectionSearchManager.sort]
+//       }`}
+//       listItemsData={[
+//         {
+//           id: "date-desc",
+//           label: "Newest to oldest",
+//           onClick: () => {
+//             updateURL(collectionSearchManager.handleSortChange("date-desc"));
+//           },
+//           type: "action",
+//         },
+//         {
+//           id: "date-asc",
+//           label: "Oldest to newest",
+//           onClick: () => {
+//             updateURL(collectionSearchManager.handleSortChange("date-asc"));
+//           },
+//           type: "action",
+//         },
+//         {
+//           id: "title-asc",
+//           label: "Title A to Z",
+//           onClick: () => {
+//             updateURL(collectionSearchManager.handleSortChange("title-asc"));
+//           },
+//           type: "action",
+//         },
+//         {
+//           id: "title-desc",
+//           label: "Title Z to A",
+//           onClick: () => {
+//             updateURL(collectionSearchManager.handleSortChange("title-desc"));
+//           },
+//           type: "action",
+//         },
+//       ]}
+//     />
+//   );
+// };
