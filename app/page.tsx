cd5749713127f17dc971ebf1dda09ba1c961a874
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-03-02 14:14:50 -0500

    Swapping out and writing tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import HomePage from "./src/components/pages/homePage/homePage";
-import { RepoApi } from "@/src/utils/apiClients";
+import { RepoApi, CollectionsApi } from "@/src/utils/apiClients";
 import { FeaturedItemDataType } from "../app/src/types/FeaturedItemDataType";
 import { revalidatePath } from "next/cache";
 
 export default async function Home() {
   revalidatePath("/");
-  const swimLaneData = await RepoApi.getHomePageData();
+  const swimLaneData = await CollectionsApi.getHomePageData();
   const featuredItemData: FeaturedItemDataType =
     await RepoApi.getFeaturedItemData();
   return (
