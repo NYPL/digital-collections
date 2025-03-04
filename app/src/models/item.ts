3f4430fb71093e7db7b6bc0cd6a6fefcbf9e2e17
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-03-04 15:37:19 -0500

    comment out the Maniiifest code bc it breaks things

--- app/src/models/item.ts
+++ app/src/models/item.ts
@@ -14,56 +14,56 @@ export class ItemModel {
   isSingleCapture: boolean;
   imageID: string;
   href: string;
-  metadata?: {
-    // Metadata fields
-    /* Title */
-    title: string;
-    // .mods.titleInfo.title
-    /* Names */
-    names?: string;
-    // .mods.name
-    /* Collection */
-    collection?: string;
-    // .mods.relatedItem?
-    /* Date/Origin */
-    origin: string;
-    // .mods.originInfo
-    /* Table of Contents */
-    tableOfContents?: string;
-    /* Library Locations */
-    locations: string;
-    // .mods.location
-    /* Subjects */
-    subjects?: string;
-    // .mods.subject
-    /* Genres */
-    genres: string;
-    // .mods.genre
-    /* Notes */
-    notes?: string;
-    /* Physical Description */
-    physicalDescription?: string;
-    // .mods.physicalDescription
-    /* Abstract */
-    abstract?: string;
-    /* Languages */
-    languages: string;
-    // Link
-    link?: string;
-    /* Identifiers */
-    identifiers: string;
-    // .mods.identifier
-    /* Access */
-    access: string;
-    /* Rights */
-    rights: string;
-    // .cappture.rightsStatement
-  };
+  // metadata?: {
+  //   // Metadata fields
+  //   /* Title */
+  //   title: string;
+  //   // .mods.titleInfo.title
+  //   /* Names */
+  //   names?: string;
+  //   // .mods.name
+  //   /* Collection */
+  //   collection?: string;
+  //   // .mods.relatedItem?
+  //   /* Date/Origin */
+  //   origin: string;
+  //   // .mods.originInfo
+  //   /* Table of Contents */
+  //   tableOfContents?: string;
+  //   /* Library Locations */
+  //   locations: string;
+  //   // .mods.location
+  //   /* Subjects */
+  //   subjects?: string;
+  //   // .mods.subject
+  //   /* Genres */
+  //   genres: string;
+  //   // .mods.genre
+  //   /* Notes */
+  //   notes?: string;
+  //   /* Physical Description */
+  //   physicalDescription?: string;
+  //   // .mods.physicalDescription
+  //   /* Abstract */
+  //   abstract?: string;
+  //   /* Languages */
+  //   languages: string;
+  //   // Link
+  //   link?: string;
+  //   /* Identifiers */
+  //   identifiers: string;
+  //   // .mods.identifier
+  //   /* Access */
+  //   access: string;
+  //   /* Rights */
+  //   rights: string;
+  //   // .cappture.rightsStatement
+  // }
 
   constructor(data: any, uuid: string, manifest: any) {
-    const parser = new Maniiifest(manifest);
-    const label = parser?.getManifestLabelByLanguage("en");
-    console.log(label?.["en"]);
+    // const parser = new Maniiifest(manifest);
+    // const label = parser?.getManifestLabelByLanguage("en");
+    // console.log(label?.["en"]);
 
     this.uuid = uuid; //data.uuid; //data.capture.uuid.$;
     this.mods = data.mods;
