53c6e08ad37dfc8cf8be0cd583a328b8091c44f1
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-03-05 09:57:15 -0500

    VQA- nothing on mobile for collection search

--- app/src/components/search/collectionSearch.tsx
+++ app/src/components/search/collectionSearch.tsx
@@ -1,16 +1,8 @@
 import { headerBreakpoints } from "@/src/utils/breakpoints";
-import {
-  Button,
-  Flex,
-  Heading,
-  Icon,
-} from "@nypl/design-system-react-components";
+import { Flex, Heading } from "@nypl/design-system-react-components";
 import DCSearchBar from "./dcSearchBar";
-import { useState } from "react";
 
 export const CollectionSearch = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-
   return (
     <Flex
       flexDir="column"
@@ -21,75 +13,31 @@ export const CollectionSearch = () => {
         paddingLeft: "m",
         paddingRight: "m",
         marginBottom: "l",
+        display: "none",
+        [`@media (min-width:  ${headerBreakpoints.lgMobile}px)`]: {
+          display: "flex",
+        },
       }}
     >
-      <Flex
-        alignItems="center"
-        justifyContent="space-between"
-        sx={{
-          marginBottom: isExpanded ? "xs" : "0",
-          [`@media (min-width:  ${headerBreakpoints.lgMobile}px)`]: {
-            marginBottom: "xs",
-          },
+      <Heading size="heading8" marginBottom="xs">
+        Search this collection:
+      </Heading>
+      <DCSearchBar
+        id="search-collection"
+        labelText="Search this collection by item title"
+        maxWrapperWidth="100%"
+        textInputProps={{
+          id: "collection-search-text",
+          isClearable: true,
+          isClearableCallback: () => {},
+          labelText: "Search this collection by item title",
+          name: "q",
+          placeholder: "Search this collection by item title",
+          defaultValue: "",
+          onChange: (e) => {},
         }}
-      >
-        <Heading size="heading8" marginBottom="0">
-          Search this collection
-        </Heading>
-        <Button
-          id={isExpanded ? "close-search" : "open-search"}
-          buttonType="text"
-          aria-label={isExpanded ? "Close search" : "Open search"}
-          sx={{
-            display: "flex",
-            [`@media (min-width:  ${headerBreakpoints.lgMobile}px)`]: {
-              display: "none",
-            },
-            padding: "0 !important",
-            minWidth: "unset",
-            _hover: { background: "unset", color: "unset" },
-          }}
-          onClick={() => setIsExpanded((prev) => !prev)}
-        >
-          {" "}
-          <Icon
-            name={isExpanded ? "close" : "search"}
-            size="medium"
-            color="ui.black"
-          />
-        </Button>
-      </Flex>
-      <Flex
-        sx={{
-          display: isExpanded ? "block" : "none",
-          [`@media (min-width:  ${headerBreakpoints.lgMobile}px)`]: {
-            display: "block",
-          },
-        }}
-      >
-        <DCSearchBar
-          id="search-collection"
-          labelText="Search this collection by item title"
-          maxWrapperWidth="100%"
-          textInputProps={{
-            id: "collection-search-text",
-            isClearable: true,
-            isClearableCallback: () => {},
-            labelText: "Search this collection by item title",
-            name: "q",
-            placeholder: "Search this collection by item title",
-            defaultValue: "",
-            onChange: (e) => {},
-          }}
-          onSubmit={() => {}}
-          sx={{
-            display: isExpanded ? "block" : "none",
-            [`@media (min-width:  ${headerBreakpoints.lgMobile}px)`]: {
-              display: "block",
-            },
-          }}
-        />
-      </Flex>
+        onSubmit={() => {}}
+      />
     </Flex>
   );
 };
