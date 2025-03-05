002c21408204814d055e0b6c3cf0ed521ce71779
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-03-04 20:40:51 -0500

    this is enough for now

--- app/src/components/pages/itemPage/itemPage.tsx
+++ app/src/components/pages/itemPage/itemPage.tsx
@@ -1,17 +1,10 @@
 "use client";
 import React from "react";
 import Item from "../../items/item";
-// import { getItemData } from "@/src/utils/apiClients";
-// import { RepoApi, CollectionsApi } from "../../src/utils/apiClients";
 
 import { ItemModel } from "@/src/models/item";
 
 export function ItemPage({ manifest, data, type, uuid }) {
-  console.log("data in ItemPage is: ", data);
   const item = new ItemModel(data, uuid, manifest);
-  return (
-    <>
-      <Item manifest={manifest} item={item} type={type} />
-    </>
-  );
+  return <Item manifest={manifest} item={item} type={type} />;
 }
--- app/src/models/item.ts
+++ app/src/models/item.ts
@@ -14,56 +14,66 @@ export class ItemModel {
   isSingleCapture: boolean;
   imageID: string;
   href: string;
-  // metadata?: {
-  //   // Metadata fields
-  //   /* Title */
-  //   title: string;
-  //   // .mods.titleInfo.title
-  //   /* Names */
-  //   names?: string;
-  //   // .mods.name
-  //   /* Collection */
-  //   collection?: string;
-  //   // .mods.relatedItem?
-  //   /* Date/Origin */
-  //   origin: string;
-  //   // .mods.originInfo
-  //   /* Table of Contents */
-  //   tableOfContents?: string;
-  //   /* Library Locations */
-  //   locations: string;
-  //   // .mods.location
-  //   /* Subjects */
-  //   subjects?: string;
-  //   // .mods.subject
-  //   /* Genres */
-  //   genres: string;
-  //   // .mods.genre
-  //   /* Notes */
-  //   notes?: string;
-  //   /* Physical Description */
-  //   physicalDescription?: string;
-  //   // .mods.physicalDescription
-  //   /* Abstract */
-  //   abstract?: string;
-  //   /* Languages */
-  //   languages: string;
-  //   // Link
-  //   link?: string;
-  //   /* Identifiers */
-  //   identifiers: string;
-  //   // .mods.identifier
-  //   /* Access */
-  //   access: string;
-  //   /* Rights */
-  //   rights: string;
-  //   // .cappture.rightsStatement
-  // }
+  metadata?: {
+    // Metadata fields
+    /* Title */
+    title: string;
+    // .mods.titleInfo.title
+    /* Names */
+    names?: string;
+    // .mods.name
+    /* Collection */
+    collection?: string;
+    // .mods.relatedItem?
+    /* Date/Origin */
+    origin: string;
+    // .mods.originInfo
+    /* Table of Contents */
+    tableOfContents?: string;
+    /* Library Locations */
+    locations: string;
+    // .mods.location
+    /* Subjects */
+    subjects?: string;
+    // .mods.subject
+    /* Genres */
+    genres: string;
+    // .mods.genre
+    /* Notes */
+    notes?: string;
+    /* Physical Description */
+    physicalDescription?: string;
+    // .mods.physicalDescription
+    /* Abstract */
+    abstract?: string;
+    /* Languages */
+    languages: string;
+    // Link
+    link?: string;
+    /* Identifiers */
+    identifiers: string;
+    // .mods.identifier
+    /* Access */
+    access: string;
+    /* Rights */
+    rights: string;
+    // .cappture.rightsStatement
+    typeOfResource: string;
+  };
 
   constructor(data: any, uuid: string, manifest: any) {
-    // const parser = new Maniiifest(manifest);
-    // const label = parser?.getManifestLabelByLanguage("en");
-    // console.log(label?.["en"]);
+    const parser = new Maniiifest(manifest);
+    const label = parser?.getManifestLabelByLanguage("en");
+    const metadata = parser.iterateManifestMetadata();
+    const manifestMetadataHash = {};
+
+    for (const field of metadata) {
+      // console.log("with just parser.iterateManifest")
+      const label = field.label["en"][0];
+      const value = field.value["en"];
+      manifestMetadataHash[label] = value;
+    }
+    console.log("manifestMetadataHash is: ", manifestMetadataHash);
 
     this.uuid = uuid; //data.uuid; //data.capture.uuid.$;
     this.mods = data.mods;
@@ -77,9 +87,98 @@ export class ItemModel {
     this.isSingleCapture = data.numResults.$ == 1;
     this.imageID = data.imageID;
     this.href = data.href;
-    // this.metadata = {
-    //   title: label?.['en'][0];
-    //   names: ;
-    // }
+
+    /*
+    field labels from manifest metadata section are: 
+    Title
+    Collection
+    Subjects
+    Genres
+    Rights
+    "Date Indexed"
+    "Library Location"
+    'Resource Type'
+    */
+
+    this.metadata = {
+      title: manifestMetadataHash["Title"]
+        ? manifestMetadataHash["Title"].toString()
+        : "",
+      collection: manifestMetadataHash["Collection"]
+        ? manifestMetadataHash["Collection"].toString()
+        : "",
+      names: manifestMetadataHash["names"]
+        ? manifestMetadataHash["names"].toString()
+        : "",
+      origin: manifestMetadataHash["Date Indexed"]
+        ? manifestMetadataHash["Date Indexed"].toString()
+        : "",
+      tableOfContents: manifestMetadataHash["Table Of Contents"]
+        ? manifestMetadataHash["Table Of Contents"].toString()
+        : "",
+      locations: manifestMetadataHash["Library Location"]
+        ? manifestMetadataHash["Library Location"].toString()
+        : "",
+      subjects: manifestMetadataHash["Subjects"]
+        ? manifestMetadataHash["Subjects"].toString()
+        : "",
+      genres: manifestMetadataHash["Genres"]
+        ? manifestMetadataHash["Genres"].toString()
+        : "",
+      notes: manifestMetadataHash["Notes"]
+        ? manifestMetadataHash["Notes"]?.toString().toString()
+        : "",
+      physicalDescription: manifestMetadataHash["Physical Description"]
+        ? manifestMetadataHash["Physical Description"].toString()
+        : "",
+      typeOfResource: manifestMetadataHash["Resource Type"]
+        ? manifestMetadataHash["Resource Type"].toString()
+        : "",
+      abstract: manifestMetadataHash["Abstract"]
+        ? manifestMetadataHash["Abstract"].toString()
+        : "",
+      languages: manifestMetadataHash["Languages"]
+        ? manifestMetadataHash["Lanugages"].toString()
+        : "",
+      link: manifestMetadataHash["Link"]
+        ? manifestMetadataHash["Link"].toString()
+        : "",
+      identifiers: manifestMetadataHash["Identifiers"]
+        ? manifestMetadataHash["Identifiers"].toString()
+        : "",
+      access: manifestMetadataHash["Access"]
+        ? manifestMetadataHash["Access"].toString()
+        : "",
+      rights: manifestMetadataHash["Rights"]
+        ? manifestMetadataHash["Rights"].toString()
+        : "",
+    };
   }
 }
+/*
+"Title": "mainTitle",
+"Names": "name_mtxt",
+"Collection": "rootCollection_s",
+"Date Indexed": "dateIndexed_dt",
+"Date Captured": "datecaptured_mtxt",
+"Copyright Date": "copyrightDate_mtxt",
+"Date (Other)": "dateOther_mtxt_s",
+"Place": "placeTerm_mtxt_s",
+"Edition": "edition_mtxt_s",
+"Table Of Contents": "tableOfContents_mtxt",
+"Library Location": "physicalLocation_mtxt",
+"Subjects": "topic_mtxt_s",
+"Genres": "genre_mtxt",
+"Notes": "note_mtxt",
+"Resource Type": "typeOfResource_mtxt",
+"Abstract": "abstract_mtxt",
+"Languages": "language_mtxt_s",
+"Access": "accessCondition_mtxt",
+"Rights": "rights_st",
+
+"Identifier - NYPL MSS ID": "identifier_local_mss",
+"Identifier - NYPL Catalog Record ID": "identifier_local_bnumber",
+"Identifier - Exhibition ID": "identifier_local_exhibition",
+"Identifier - NYP": "identifier_local_catnyp",
+"Identifier - Archives EAD ID": "identifier_local_archives_ead"
+*/
