c820b41f06183aef48d37971bbd15004de0c45e3
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-03-13 10:52:20 -0400

    put closing script tag in right place

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,21 +80,21 @@ export default async function RootLayout({
         <script>
           window.dataLayer = window.dataLayer || [];
           {/* <!-- Google Tag Manager --> */}
-          <Script
-            id="ga4-gtm"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
+        </script>
+        <Script
+          id="ga4-gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-RKWC');
               `,
-            }}
-          />
-          {/* <!-- End Google Tag Manager --> */}
-        </script>
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
       </head>
       <body>
