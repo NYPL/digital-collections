d9c263cb73938fea400e1647674a9d54f17b833f
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-03-31 10:52:10 -0400

    Hmm

--- app/src/components/header/header.tsx
+++ app/src/components/header/header.tsx
@@ -1,12 +1,10 @@
+import React, { useLayoutEffect, useRef, useState } from "react";
 import {
   Box,
   HStack,
   VStack,
   HorizontalRule,
 } from "@nypl/design-system-react-components";
-import React from "react";
-import { useScrolled } from "../../hooks/useScrolled";
-import { useStickyMargin } from "../../hooks/useStickyMargin";
 import Search from "../search/search";
 import DCLogo from "../logo/logo";
 import NavMenu from "../navMenu/navMenu";
@@ -15,123 +13,147 @@ import { headerBreakpoints } from "../../utils/breakpoints";
 import useScrollDirection from "../../hooks/useScrollDirection";
 
 const Header = () => {
-  useStickyMargin();
-  const isScrolled = useScrolled("header");
+  const headerRef = useRef<HTMLDivElement>(null);
+  const [headerHeight, setHeaderHeight] = useState<number>(150);
+  const [isFocused, setIsFocused] = useState(false);
   const isScrollingUp = useScrollDirection();
 
+  useLayoutEffect(() => {
+    const updateHeight = () => {
+      if (headerRef.current) {
+        const newHeight = headerRef.current.offsetHeight;
+        setHeaderHeight((prev) => (prev !== newHeight ? newHeight : prev));
+      }
+    };
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+    return () => {
+      window.removeEventListener("resize", updateHeight);
+    };
+  }, [isScrollingUp]);
+
   return (
-    <Box
-      data-sticky-header
-      data-sticky-offset="10"
-      position="sticky"
-      id="header"
-      top={0}
-      zIndex={99999}
-      bgColor="ui.white"
-      sx={{
-        [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]: {
-          pt: "xs",
-        },
-        alignItems: "center",
-      }}
-    >
+    <>
+      {/* Placeholder to avoid layout shift */}
+      {isFocused && <Box height={`${headerHeight}px`} />}
       <Box
-        maxWidth="1280px"
-        mx="auto"
-        padding="0 16px !important"
+        ref={headerRef}
+        data-sticky-header
+        data-sticky-offset="10"
+        position={isFocused ? "fixed" : "sticky"}
+        top="0px"
+        left="0"
+        right="0"
+        zIndex={99999}
+        bgColor="ui.white"
         sx={{
-          [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]: {
-            display: "flex",
-            justifyContent: "space-between",
+          [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]: {
+            pt: "xs",
           },
+          alignItems: "center",
         }}
+        onFocusCapture={() => setIsFocused(true)}
+        onBlurCapture={() => setIsFocused(false)}
       >
-        <HStack
-          justify="space-between"
+        <Box
+          maxWidth="1280px"
+          mx="auto"
+          padding="0 16px !important"
           sx={{
-            display: "none",
-            [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]:
+            [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]:
               {
                 display: "flex",
-                pt: "2px",
+                justifyContent: "space-between",
               },
-            pb: "xs",
           }}
         >
-          <DCLogo isMobile={false} />
-          <Box
+          <HStack
+            justify="space-between"
             sx={{
-              display: "block",
-              [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]:
+              display: "none",
+              [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]:
+                {
+                  display: "flex",
+                  pt: "2px",
+                },
+              pb: "xs",
+            }}
+          >
+            <DCLogo isMobile={false} />
+            <Box
+              sx={{
+                display: "block",
+                [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]:
+                  {
+                    display: "none",
+                  },
+              }}
+            >
+              <NavMenu render={0} />
+            </Box>
+          </HStack>
+          <HStack
+            sx={{
+              display: isScrollingUp ? "flex" : "none",
+              [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]:
+                {
+                  display: "none",
+                },
+              justifyContent: "space-between",
+              paddingTop: "xs",
+              paddingBottom: "xs",
+            }}
+          >
+            <DCLogo isMobile={true} />
+            <MobileNavMenu />
+          </HStack>
+          <HorizontalRule
+            height="1px"
+            width="auto"
+            sx={{
+              borderColor: "var(--nypl-colors-ui-border-default)",
+              margin: "0 -15px",
+              [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]:
                 {
                   display: "none",
                 },
             }}
+          />
+          <VStack
+            align="end"
+            sx={{
+              [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]:
+                {
+                  width: "36%",
+                },
+              paddingTop: "xs",
+            }}
           >
-            <NavMenu render={0} />
-          </Box>
-        </HStack>
-        <HStack
-          sx={{
-            display: isScrollingUp ? "flex" : "none",
-            [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]:
-              {
+            <Box
+              sx={{
                 display: "none",
-              },
-            justifyContent: "space-between",
-            paddingTop: "xs",
-            paddingBottom: "xs",
-          }}
-        >
-          <DCLogo isMobile={true} />
-          <MobileNavMenu />
-        </HStack>
+                [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]:
+                  {
+                    display: isScrollingUp ? "block" : "none",
+                  },
+              }}
+            >
+              <NavMenu render={1} />
+            </Box>
+            <Search />
+          </VStack>
+        </Box>
         <HorizontalRule
           height="1px"
           width="auto"
           sx={{
             borderColor: "var(--nypl-colors-ui-border-default)",
-            margin: "0 -15px",
-            [`@media screen and (min-width: ${headerBreakpoints.smTablet}px)`]:
-              {
-                display: "none",
-              },
+            margin: "0px",
+            display: "block",
           }}
         />
-        <VStack
-          align="end"
-          sx={{
-            [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]:
-              {
-                width: "36%",
-              },
-            paddingTop: "xs",
-          }}
-        >
-          <Box
-            sx={{
-              display: "none",
-              [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]:
-                {
-                  display: isScrollingUp ? "block" : "none",
-                },
-            }}
-          >
-            <NavMenu render={1} />
-          </Box>
-          <Search />
-        </VStack>
       </Box>
-      <HorizontalRule
-        height="1px"
-        width="auto"
-        sx={{
-          borderColor: "var(--nypl-colors-ui-border-default)",
-          margin: "0px",
-          display: "block",
-        }}
-      />
-    </Box>
+    </>
   );
 };
 
