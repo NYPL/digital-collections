a85bdeee75ed0d354fe413f11f887d5e9264c6b9
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-04-11 12:15:55 -0400

    Redirects, tests, and mapping

--- __tests__/middleware/collectionRedirects.test.tsx
+++ __tests__/middleware/collectionRedirects.test.tsx
@@ -52,4 +52,43 @@ it("goes through unchanged if no collection_keywords= param", () => {
   expect(response).toBe("next response");
 });
 
-it("redirects collection slug to uuid, or to a title search", () => {});
+it("redirects existing collection slug to uuid", () => {
+  request = {
+    nextUrl: new URL("http://localhost/collections/script-b"),
+  } as NextRequest;
+
+  const response = middleware(request);
+
+  expect(NextResponse.redirect).toHaveBeenCalledWith(
+    "http://localhost/collections/5af6c940-c45b-013c-85aa-0242ac110005",
+    301
+  );
+  expect(response).toBe("redirect response");
+});
+
+it("redirects non-existent collection slug to title search", () => {
+  request = {
+    nextUrl: new URL("http://localhost/collections/hello-im-not-a-real-slug"),
+  } as NextRequest;
+
+  const response = middleware(request);
+
+  expect(NextResponse.redirect).toHaveBeenCalledWith(
+    "http://localhost/search/index?q=Hello+Im+Not+A+Real+Slug",
+    301
+  );
+  expect(response).toBe("redirect response");
+});
+
+it("passes a collection uuid through", () => {
+  request = {
+    nextUrl: new URL(
+      "http://localhost/collections/bc920670-c5f9-012f-63ad-58d385a7bc34"
+    ),
+  } as NextRequest;
+
+  const response = middleware(request);
+
+  expect(NextResponse.next).toHaveBeenCalled();
+  expect(response).toBe("next response");
+});
