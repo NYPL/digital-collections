ac7218db1d8716dee2d62e59d6b51a680c033439
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-06-05 16:38:49 -0400

    return structured list of collections with arrows

--- app/src/components/items/overview/metadata/overview.tsx
+++ app/src/components/items/overview/metadata/overview.tsx
@@ -8,6 +8,7 @@ import {
   ButtonGroup,
   TagSet,
   HorizontalRule,
+  Icon,
 } from "@nypl/design-system-react-components";
 import parse from "html-react-parser";
 
@@ -33,9 +34,41 @@ const metadataFieldToDisplay = {
   dateIssued: "Date Issued",
 };
 
-const metadataFieldRender = (field, value) => {
-  if (field === "name") {
-  }
+// Helper function to parse anchor tag from HTML string
+// annoyingly redundant
+const parseHtmlString = (html) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const anchor = doc.querySelector("a");
+  return {
+    href: anchor?.getAttribute("href"),
+    text: anchor?.textContent,
+  };
+};
+
+// Designs use DS Link component but I wonder if we can get away with not using them for the
+// Items page since the manifests are returning the links
+const StructuredCollectionsList = (rawCollections) => {
+  return (
+    <>
+      {rawCollections.map((htmlString, index) => {
+        console.log("htmlString is: ", htmlString);
+        const { href, text } = parseHtmlString(htmlString);
+        return (
+          <span key={index} style={{ paddingLeft: `${(index - 1) * 1.5}rem` }}>
+            {index !== 0 && (
+              <Icon name="navigationSubdirectoryArrowRight" size="large" />
+            )}
+            {parse(htmlString)}
+            {/* <Link href={href}>
+              {text}
+            </Link> */}
+            <br></br>
+          </span>
+        );
+      })}
+    </>
+  );
 };
 
 const MetadataOverview = ({ metadata }) => {
@@ -50,16 +83,31 @@ const MetadataOverview = ({ metadata }) => {
         {Object.keys(metadata)?.map((field, index) => {
           const value = metadata[field];
           if (value !== "" && metadataFieldToDisplay[field] !== "") {
-            return (
-              <>
-                <Text key={index} size="overline1" marginBottom="xs">
-                  {metadataFieldToDisplay[field]}
-                </Text>
-                <Text key={index} marginBottom="m">
-                  {parse(value)}
-                </Text>
-              </>
-            );
+            if (field === "collection") {
+              const collections = value.split("<br>");
+              console.log("collections are: ", collections);
+              return (
+                <>
+                  <Text key={index} size="overline1" marginBottom="xs">
+                    {metadataFieldToDisplay[field]}
+                  </Text>
+                  <Text key={index} marginBottom="m">
+                    {StructuredCollectionsList(collections)}
+                  </Text>
+                </>
+              );
+            } else {
+              return (
+                <>
+                  <Text key={index} size="overline1" marginBottom="xs">
+                    {metadataFieldToDisplay[field]}
+                  </Text>
+                  <Text key={index} marginBottom="m">
+                    {parse(value)}
+                  </Text>
+                </>
+              );
+            }
           }
         })}
       </Box>
