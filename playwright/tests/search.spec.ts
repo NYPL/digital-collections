89ef4f489ff6a2804118c9cd5986081c8fae10a3
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-06-05 08:48:47 -0600

    Removed comments

--- playwright/tests/search.spec.ts
+++ playwright/tests/search.spec.ts
@@ -2,18 +2,17 @@ import { test, expect } from "@playwright/test";
 import SearchPage from "../pages/search.page";
 
 test("searches for a keyword from homepage", async ({ page }) => {
-  // from homepage
-  const searchPage = await SearchPage.loadPage("/", page); // rename when homepage class is created
+  const searchPage = await SearchPage.loadPage("/", page);
 
-  await expect(searchPage.searchBox).toBeVisible(); // create function to search
+  await expect(searchPage.searchBox).toBeVisible();
   await searchPage.searchBox.fill(searchPage.searchKeyword);
   await expect(searchPage.searchButton).toBeVisible();
   await searchPage.searchButton.click();
 
-  await page.waitForURL("**/search/**"); // gets flaky at this point waiting for search results
+  await page.waitForURL("**/search/**");
   await expect(page).toHaveTitle("Search results - NYPL Digital Collections");
 
-  await expect(searchPage.refineHeading).toBeVisible(); // split into new test - displays search results
+  await expect(searchPage.refineHeading).toBeVisible();
   await expect(searchPage.resultsHeading).toBeVisible();
   await expect(searchPage.firstResult).toBeVisible();
 });
