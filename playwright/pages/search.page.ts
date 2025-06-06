82d4fbd0d0a4d4c3d658943be11683c262988921
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2025-06-06 10:28:18 -0600

    DR-3659: Playwright tests to search for keyword (#343)
    * Wrote test to search for a keyword from homepage
    
    * Moved locators to search page
    
    * Set baseURL in Playwright config
    
    * Removed comments
    
    * Updated changelog
    
    * Increased timeout to 60s
    
    * Changed search term

---
+++ playwright/pages/search.page.ts
@@ -0,0 +1,34 @@
+import { Locator, Page } from "@playwright/test";
+
+export default class SearchPage {
+  readonly page: Page;
+  readonly searchKeyword: string;
+  readonly searchBox: Locator;
+  readonly searchButton: Locator;
+  readonly refineHeading: Locator;
+  readonly resultsHeading: Locator;
+  readonly firstResult: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.searchKeyword = "map of scandinavia";
+    this.searchBox = this.page.getByLabel("Search keyword(s)");
+    this.searchButton = this.page.getByRole("button", { name: "Search" });
+    this.refineHeading = this.page.getByRole("heading", {
+      name: "Refine your search",
+    });
+    this.resultsHeading = this.page.getByRole("heading", {
+      name: new RegExp(
+        `^Displaying \\d+-\\d+ of \\d+ results for "${this.searchKeyword}"$`
+      ),
+    });
+    this.firstResult = page
+      .getByRole("link", { name: this.searchKeyword })
+      .first();
+  }
+
+  static async loadPage(gotoPage: string, page: Page): Promise<SearchPage> {
+    await page.goto(gotoPage, { waitUntil: "load" });
+    return new SearchPage(page);
+  }
+}
