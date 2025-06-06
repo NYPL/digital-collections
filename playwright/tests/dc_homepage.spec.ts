12cb52d863686737f84fd6d97e7709751a82eaf5
Bridgette Rose <dz4888@wayne.edu>
2025-06-06 11:34:04 -0400

    DR-3658 Adding home page navigation menu test (#345)
    * Created the first test of the  dc home page for the navigation menu. Deleted the example.spec.ts
    
    * Added tests and assertions for navigation menu, search, and public domain
    
    * Added camel case, updated test to getByRole, added more test description
    
    * changed name  from search box to search bar

---
+++ playwright/tests/dc_homepage.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+import { DCHomepage } from "../pages/dc_homepage";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3000");
+});
+
+test("verify navigation menu is displayed (items, collections, divisions, about", async ({
+  page,
+}) => {
+  const dchomepage = new DCHomepage(page);
+  await expect(dchomepage.items).toBeVisible();
+  await expect(dchomepage.collections).toBeVisible();
+  await expect(dchomepage.divisions).toBeVisible();
+  await expect(dchomepage.about).toBeVisible();
+});
+
+test("verify search bar, search button are visible", async ({ page }) => {
+  const dchomepage = new DCHomepage(page);
+  await expect(dchomepage.searchBar).toBeVisible();
+  await expect(dchomepage.searchButton).toBeVisible();
+});
+
+test("verify public domain link is visible", async ({ page }) => {
+  const dchomepage = new DCHomepage(page);
+  await expect(dchomepage.whatIsPublicDomainLink).toBeVisible();
+});
