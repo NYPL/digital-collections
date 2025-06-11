d3a4f6fde344cbe6d9cb962a41907882b118b9b7
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-06-11 09:31:40 -0400

    update citations

--- app/src/components/items/overview/citations/overview.tsx
+++ app/src/components/items/overview/citations/overview.tsx
@@ -1,15 +1,28 @@
 import { Box, Flex, Heading, Text } from "@nypl/design-system-react-components";
 import parse from "html-react-parser";
 
+// Helper function to parse anchor tag from HTML string
+// annoyingly redundant
+const parseHtmlString = (html) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const anchor = doc.querySelector("a");
+  return {
+    href: anchor?.getAttribute("href"),
+    text: anchor?.textContent,
+  };
+};
+
 // TO DO: generate on the fly: https://github.com/NYPL/digitalreadingroom/blob/qa/app/views/partials/_item_metadata.html.erb
 // TO DO: add tests for citations, use https://digitalcollections.nypl.org/items/a3fff740-395b-0138-983a-7f56d1f9ecb7#/?uuid=9cf949b0-d298-0139-4999-0242ac110003 as example
-
 const CitationsOverview = ({ item }) => {
   const metadata = item.metadata;
   const shareUrl = item.link;
 
-  const location = metadata?.locations?.split("<br>")[0];
-  const resource = metadata?.typeOfResource?.split("<br>")[0];
+  const location = parseHtmlString(metadata?.locations?.split("<br>")[0]).text;
+  const resource = parseHtmlString(
+    metadata?.typeOfResource?.split("<br>")[0]
+  ).text;
 
   const today = new Date().toLocaleDateString("en-us", {
     day: "numeric",
@@ -19,7 +32,7 @@ const CitationsOverview = ({ item }) => {
 
   // TO DO: metadata.origin should be metadata.date_created
   const MLA = `<p>
-    ${location ? location : ""}
+    ${location ? location : ""},
     The New York Public Library. "${metadata.title}"
     <em>The New York Public Library Digital Collections</em>.
     ${metadata.origin ? metadata.origin + "." : ""}
@@ -28,7 +41,7 @@ const CitationsOverview = ({ item }) => {
   </p>`;
 
   const CHICAGO = `<p>
-  ${location ? location : ""}
+  ${location ? location : ""},
   The New York Public Library. "${metadata.title}"
     New York Public Library Digital Collections.
     Accessed ${today}.
@@ -38,7 +51,7 @@ const CitationsOverview = ({ item }) => {
   // TO DO: metadata.origin should be metadata.date_created
   const APA = `
       <p>
-      ${location ? location : ""}
+      ${location ? location : ""},
       The New York Public Library.
           ${metadata.origin ? metadata.origin + "." : ""}
           ${metadata.dateIssued ? metadata.dateIssued + "." : ""}
@@ -56,7 +69,7 @@ ${metadata.title}
 ${metadata.origin ? "(" + metadata.origin + ")" : ""}${
     metadata.dateIssued ? "(" + metadata.dateIssued + ")" : ""
   }
-|author=Digital Collections, The New York Public Library |accessdate=${today} |publisher=The New York Public Library, Astor, Lenox, and Tilden Foundations}}&lt;/ref&gt;</p>
+| author=Digital Collections, The New York Public Library | accessdate=${today} | publisher=The New York Public Library, Astor, Lenox, and Tilden Foundations}}&lt;/ref&gt;</p>
   `;
 
   const citationFormatDummyData = {
