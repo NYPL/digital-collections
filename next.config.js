67b60a4071770d10d416b68eb1c6f38bd333ff05
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-06-12 12:33:18 -0600

    Retry includng webServer

--- next.config.js
+++ next.config.js
@@ -35,10 +35,6 @@ const nextConfig = {
   // In order for newrelic to effectively instrument a Next.js application,
   // the modules that newrelic supports should not be mangled by webpack. Thus,
   // we need to "externalize" all of the modules that newrelic supports.
-  // webpack: (config) => {
-  //   nrExternals(config);
-  //   return config;
-  // },
 
   webpack: (config) => {
     if (process.env.NEW_RELIC_APP_NAME) {
