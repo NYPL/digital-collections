d8e04554da0184cb0e69863ee19035070ee68425
Crystal N <crystal.hk.ngai@gmail.com>
2025-06-13 16:41:55 -0400

    rm console

--- app/src/utils/ga4Utils.ts
+++ app/src/utils/ga4Utils.ts
@@ -11,3 +11,21 @@ export const trackGa4PageView = (
     subcollection: subCollection ? subCollection : "No Detail",
   });
 };
+
+export const sendDownloadEvent = (
+  fileName: string,
+  extension: string,
+  division?: string,
+  collection?: string,
+  subcollection?: string
+) => {
+  const dataLayer = window["dataLayer"] || [];
+  dataLayer.push({
+    event: "file_download",
+    file_name: fileName,
+    file_extension: extension,
+    division_center: division ? division : "No Detail",
+    collection: collection ? collection : "No Detail",
+    subcollection: subcollection ? subcollection : "No Detail",
+  });
+};
