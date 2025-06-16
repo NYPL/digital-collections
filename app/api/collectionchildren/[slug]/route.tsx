2420293d52037e1d3bc5378db401f2a7b7573fd0
Sarang Joshi <sarangjoshi@nypl.org>
2025-06-16 14:33:28 -0400

    Push collection children pagination into next api
    Instead of doing all the pagination client side when fetching a
    collection's children, fetch it all during the next API
    `collectionchildren` request.  This reduces the number of roundtrips
    between the client and server. I noticed that even when the server
    caches its `fetch` requests, the sheer number of requests from the
    client makes this whole thing slow.

--- app/api/collectionchildren/[slug]/route.tsx
+++ app/api/collectionchildren/[slug]/route.tsx
@@ -4,39 +4,71 @@ import { CARDS_PER_PAGE } from "@/src/config/constants";
 
 const oneMonth = 60 * 60 * 24 * 30;
 
+const PAGESIZE = 50;
+
+interface CollectionChildren {
+  numResults: number;
+  page: number;
+  perPage: number;
+  children: CollectionChild[];
+}
+
+interface CollectionChild {
+  title: string;
+  uuid: string;
+  hasSubContainers: boolean;
+  itemCount: number;
+}
+
 export async function GET(
   req: NextRequest,
   { params }: { params: { slug: string } }
 ) {
   const uuid = params.slug;
-  const page = Number(req.nextUrl.searchParams.get("page")) || 1;
 
   if (!uuid) {
     return new Response(JSON.stringify({ error: "Missing or invalid UUID" }), {
       status: 400,
     });
   }
-  try {
-    const apiUrl = `${process.env.COLLECTIONS_API_URL}/collections/${uuid}/children?page=${page}&perPage=${CARDS_PER_PAGE}`;
-    const response = await fetchApi({
-      apiUrl,
-      options: { isRepoApi: false, next: { revalidate: oneMonth } },
-    });
-
-    return new Response(JSON.stringify(response), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error: any) {
-    console.error(
-      "Error fetching collection children:",
-      error.message || error
-    );
-    return new Response(
-      JSON.stringify({ error: "Failed to fetch collection children" }),
-      { status: 500 }
-    );
+  let children: CollectionChild[] = [];
+  let page = 1;
+  while (true) {
+    try {
+      let pageResult = await fetchPage(uuid, page);
+      children.push(...pageResult.children);
+      if (pageResult.children.length < PAGESIZE) {
+        break;
+      }
+      page += 1;
+    } catch (error: any) {
+      console.error(
+        "Error fetching collection children:",
+        error.message || error
+      );
+      return new Response(
+        JSON.stringify({ error: "Failed to fetch collection children" }),
+        { status: 500 }
+      );
+    }
   }
+  const body = { children: children };
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+async function fetchPage(
+  uuid: string,
+  page: number
+): Promise<CollectionChildren> {
+  const apiUrl = `${process.env.COLLECTIONS_API_URL}/collections/${uuid}/children?page=${page}&perPage=${PAGESIZE}`;
+  const response = await fetchApi({
+    apiUrl,
+    options: { isRepoApi: false, next: { revalidate: oneMonth } },
+  });
+  return response;
 }
