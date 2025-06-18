7f16d23f8533afea7ca091349c117f6cf635832b
roseb89 <dz4888@wayne.edu>
2025-06-17 20:53:18 -0400

    Updated divisions test

--- playwright/pages/divisions.page.ts
+++ playwright/pages/divisions.page.ts
@@ -3,16 +3,37 @@ import { Locator, Page } from "@playwright/test";
 export class Divisions {
   private readonly page: Page;
 
-  readonly divisionsHeading: Locator;
-  readonly divisionsHeadingList: Locator;
-  readonly divisionSeeMore: Locator;
+  readonly expectedDivisionNames: string[];
 
   constructor(page: Page) {
     this.page = page;
-    this.divisionsHeading = this.page.getByRole("heading", {
-      name: "Divisions",
-    });
-    this.divisionsHeadingList = this.page.locator(".chakra-heading.css-8aiqvu");
-    this.divisionSeeMore = this.page.getByRole("link", { name: "See more" });
+
+    // Expected division names
+    this.expectedDivisionNames = [
+      "Billy Rose Theatre Division",
+      "Carl H. Pforzheimer Collection of Shelley and His Circle",
+      "Dorot Jewish Division",
+      "General Research Division",
+      "George Arents Collection",
+      "Henry W. and Albert A. Berg Collection of English and American Literature",
+      "Irma and Paul Milstein Division of United States History, Local History and Genealogy",
+      "Jerome Robbins Dance Division",
+      "Lionel Pincus and Princess Firyal Map Division",
+      "Manuscripts and Archives Division",
+      "Music Division",
+      "New York Public Library Archives",
+      "Rare Book Division",
+      "Rodgers and Hammerstein Archives of Recorded Sound",
+      "Schomburg Center for Research in Black Culture, Art and Artifacts Division",
+      "Schomburg Center for Research in Black Culture, Jean Blackwell Hutson Research and Reference Division",
+      "Schomburg Center for Research in Black Culture, Manuscripts, Archives and Rare Books Division",
+      "Schomburg Center for Research in Black Culture, Photographs and Prints Division",
+      "Schomburg Center for Research in Black Culture, Moving Image and Recorded Sound Division",
+      "Spencer Collection",
+      "The Miriam and Ira D. Wallach Division of Art, Prints and Photographs: Art & Architecture Collection",
+      "The Miriam and Ira D. Wallach Division of Art, Prints and Photographs: Photography Collection",
+      "The Miriam and Ira D. Wallach Division of Art, Prints and Photographs: Picture Collection",
+      "The Miriam and Ira D. Wallach Division of Art, Prints and Photographs: Print Collection",
+    ];
   }
 }
