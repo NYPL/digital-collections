40b67566ad2ed3b0c6c4c13321a6a1abe77b95c9
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-07-01 09:51:23 -0400

    code cleanup for Plyr component

--- app/src/components/items/plyr/player.tsx
+++ app/src/components/items/plyr/player.tsx
@@ -6,73 +6,43 @@ import "plyr-react/plyr.css";
 const Player = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 // example from https://www.npmjs.com/package/plyr-react
-export default function PlyrPlayer({ src, type }) {
+export default function PlyrPlayer({ title, src, type }) {
   // https://github.com/sampotts/plyr#the-source-setter
-  let playerHeight = "";
+  let playerHeight = type === "video" ? "650px" : "55px";
   let source;
   // this assumes that the only types returned to the component are 'audio' and 'video,' which should be the case
   if (type === "video") {
-    // video player
     source = {
       type: "video",
-      title: "Example title",
+      title: title,
       sources: [
         {
           src: src,
           type: "video/mp4",
-          // size: 720,
         },
-        // {
-        //   src: '/path/to/movie.webm',
-        //   type: 'video/webm',
-        //   size: 1080,
-        // },
       ],
-      // poster: '/path/to/poster.jpg',
-      // previewThumbnails: {
-      //   src: '/path/to/thumbnails.vtt',
-      // },
-      // tracks: [
-      //   {
-      //     kind: 'captions',
-      //     label: 'English',
-      //     srclang: 'en',
-      //     src: '/path/to/captions.en.vtt',
-      //     default: true,
-      //   },
-      //   {
-      //     kind: 'captions',
-      //     label: 'French',
-      //     srclang: 'fr',
-      //     src: '/path/to/captions.fr.vtt',
-      //   },
-      // ],
     };
   } else {
     source = {
       type: "audio",
-      title: "Example title",
+      title: title,
       sources: [
         {
           src: src,
           type: "audio/mp3",
         },
-        // {
-        //   src: '/path/to/audio.ogg',
-        //   type: 'audio/ogg',
-        // },
       ],
     };
   }
 
-  const options = "";
-  // https://github.com/sampotts/plyr#options
+  // docs: https://github.com/sampotts/plyr#options
+  const options = undefined;
   return (
     <div>
       <Plyr
         source={source}
-        options={undefined}
-        height={type === "video" ? "650px" : "55px"}
+        options={options}
+        height={playerHeight}
         width="100%"
       />
     </div>
