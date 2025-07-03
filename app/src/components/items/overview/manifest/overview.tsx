dd0f6813150c346fa66f8a4c3532102d73a8ea6e
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-07-03 12:47:54 -0400

    add button for manifest

---
+++ app/src/components/items/overview/manifest/overview.tsx
@@ -0,0 +1,39 @@
+import { SimpleGrid as ChakraSimpleGrid } from "@chakra-ui/react";
+import { Box, Heading, Link } from "@nypl/design-system-react-components";
+import { headerBreakpoints } from "@/src/utils/breakpoints";
+import React from "react";
+
+const ManifestOverview = ({ manifestURL }) => {
+  return (
+    <>
+      <ChakraSimpleGrid
+        sx={{
+          [`@media screen and (min-width: ${headerBreakpoints.lgMobile}px)`]: {
+            gridTemplateColumns: `repeat(1, minmax(0, 1fr))`,
+          },
+          [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]: {
+            gridTemplateColumns: `repeat(2, minmax(0, 1fr))`,
+          },
+        }}
+      >
+        <Box marginBottom="m">
+          <Heading size="heading6" marginBottom="xs">
+            View IIIF Manifest
+          </Heading>
+          <Link
+            href={manifestURL}
+            id={"print-btn"}
+            isUnderlined={false}
+            target="_blank"
+            aria-label={`order print`}
+            type="buttonSecondary"
+          >
+            Link to Item Manifest
+          </Link>
+        </Box>
+      </ChakraSimpleGrid>
+    </>
+  );
+};
+
+export default ManifestOverview;
--- app/src/components/items/overview/overview.tsx
+++ app/src/components/items/overview/overview.tsx
@@ -11,6 +11,7 @@ import { headerBreakpoints } from "@/src/utils/breakpoints";
 import ExternalLinksOverview from "./external/overview";
 import PrintOverview from "./print/overview";
 import CitationsOverview from "./citations/overview";
+import ManifestOverview from "./manifest/overview";
 
 const ItemOverview = ({ item }) => {
   return (
@@ -30,7 +31,11 @@ const ItemOverview = ({ item }) => {
           catalogLink={item.catalogLink}
           archivesLink={item.archivesLink}
         />
-        {item.isImage && <PrintOverview imageIDs={item.imageIDs} />}
+        {item.isImage ? (
+          <PrintOverview imageIDs={item.imageIDs} />
+        ) : (
+          <ManifestOverview manifestURL={item.manifestURL} />
+        )}
       </ChakraSimpleGrid>
       <HorizontalRule marginTop="xs" marginBottom="m" />
       <ChakraSimpleGrid
