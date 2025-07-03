0c59f25a392eeec293bbaa94c566c74a5ece6095
Sarang Joshi <sarangjoshi@nypl.org>
2025-07-03 15:32:19 -0400

    Move test constants to page object

--- playwright/pages/item.page.ts
+++ playwright/pages/item.page.ts
@@ -3,6 +3,10 @@ import { Locator, Page } from "@playwright/test";
 export default class ItemPage {
   readonly page: Page;
 
+  static captureIndex: number = 2;
+  static captureUuid: string = "e36b2b90-740d-0136-0517-476ebf952da4";
+  static itemUuid: string = "7cd3acc0-5d7f-0134-12ab-00505686a51c";
+
   constructor(page: Page) {
     this.page = page;
   }
--- playwright/tests/item-page.spec.ts
+++ playwright/tests/item-page.spec.ts
@@ -3,14 +3,10 @@ import ItemPage from "../pages/item.page";
 
 let itemPage: ItemPage;
 
-// Probably a nicer way to do this than hardcoding but :shrug?
-const CAPTURE_UUID = "e36b2b90-740d-0136-0517-476ebf952da4";
-const ITEM_UUID = "7cd3acc0-5d7f-0134-12ab-00505686a51c";
-
 test("Capture uuid redirects to correct canvas", async ({ page, baseURL }) => {
-  itemPage = await ItemPage.loadPage(`/items/${CAPTURE_UUID}`, page);
+  itemPage = await ItemPage.loadPage(`/items/${ItemPage.captureUuid}`, page);
   await expect(itemPage.page).toHaveURL(
-    `${baseURL}/items/${ITEM_UUID}?canvasIndex=2`
+    `${baseURL}/items/${ItemPage.itemUuid}?canvasIndex=${ItemPage.captureIndex}`
   );
 });
 
@@ -19,10 +15,10 @@ test("Hash url capture uuid redirects to correct canvas", async ({
   baseURL,
 }) => {
   itemPage = await ItemPage.loadPage(
-    `/items/${ITEM_UUID}#/?uuid=${CAPTURE_UUID}`,
+    `/items/${ItemPage.itemUuid}#/?uuid=${ItemPage.captureUuid}`,
     page
   );
   await expect(itemPage.page).toHaveURL(
-    `${baseURL}/items/${ITEM_UUID}?canvasIndex=2`
+    `${baseURL}/items/${ItemPage.itemUuid}?canvasIndex=${ItemPage.captureIndex}`
   );
 });
