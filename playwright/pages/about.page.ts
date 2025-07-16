8f283efe6b4aeb2e2a6a0e87eefd60eb9fbd1dd8
roseb89 <dz4888@wayne.edu>
2025-07-16 15:30:11 -0400

    update about page

--- playwright/pages/about.page.ts
+++ playwright/pages/about.page.ts
@@ -16,6 +16,36 @@ export class AboutPage {
   readonly publicDomaincheckbox: Locator;
   readonly whatIsPublicDomainLink: Locator;
 
+  // links
+  readonly searchLink: Locator;
+  readonly itemLink: Locator;
+  readonly collectionLink: Locator;
+  readonly divisionLink: Locator;
+  readonly nyplDigitalCollectionsPlatformLink: Locator;
+  readonly aboutLink: Locator;
+  readonly browseItemsLink: Locator;
+  readonly publicDomainItemsLink: Locator;
+  readonly freeForAllLink: Locator;
+  readonly permissionsAndReproductionsLink: Locator;
+  readonly investigateCopywrightLink: Locator;
+  readonly publicDomainDeterminationChartLink: Locator;
+  readonly wikipediaLink: Locator;
+  readonly fairUseBasicsLink: Locator;
+  readonly fairUseChecklistLink: Locator;
+  readonly audioAndMovingImageLink: Locator;
+  readonly nyplCatalogLink: Locator;
+  readonly nyplArchivesPortalLink: Locator;
+  readonly digitalPublicLibraryOfAmericaLink: Locator;
+  readonly creativeCommonsLink: Locator;
+  readonly nyplDigitalCollectionsAPILink: Locator;
+  readonly publicDomainDigitalCollectionsGithubLink: Locator;
+  readonly dplaLink: Locator;
+  readonly modsLink: Locator;
+  readonly internetArchiveBookReaderLink: Locator;
+  readonly iiifComplianceLink: Locator;
+  readonly webAndMobileAccessibilityPolicyLink: Locator;
+  readonly emailLink: Locator;
+
   // headings and text
   readonly aboutPageText: Locator;
   readonly aboutLinks: Locator;
@@ -45,8 +75,88 @@ export class AboutPage {
       name: "What is public domain?",
     });
 
-    // headings and text
-    this.aboutPageText = page.locator("body");
-    this.aboutLinks = page.locator("a");
+    // links
+    this.searchLink = page.getByRole("link", { name: "search", exact: true });
+    this.itemLink = page.getByRole("link", { name: "item", exact: true });
+    this.collectionLink = page.getByRole("link", {
+      name: "collection",
+      exact: true,
+    });
+    this.divisionLink = page.getByRole("link", {
+      name: "division",
+      exact: true,
+    });
+    this.aboutLink = page.getByRole("link", { name: "about", exact: true });
+
+    this.nyplDigitalCollectionsPlatformLink = page.getByRole("link", {
+      name: '"NYPL Digital Collections Platform: An Introduction"',
+    });
+    this.browseItemsLink = page.getByRole("link", {
+      name: "browse just the items that have no known U.S. copyright restrictions",
+    });
+    this.publicDomainItemsLink = page.getByRole("link", {
+      name: "296,687 public domain items",
+    });
+    this.freeForAllLink = page.getByRole("link", {
+      name: "Free for All: NYPL Enhances Public Domain Collections for Sharing and Reuse.",
+    });
+
+    this.permissionsAndReproductionsLink = page.getByRole("link", {
+      name: "Permissions and Reproductions",
+    });
+    this.investigateCopywrightLink = page.getByRole("link", {
+      name: "How to Investigate the Copyright Status of a Work [PDF]",
+    });
+    this.publicDomainDeterminationChartLink = page.getByRole("link", {
+      name: "public domain determination chart",
+    });
+    this.wikipediaLink = page.getByRole("link", {
+      name: "Wikipedia",
+    });
+    this.fairUseBasicsLink = page.getByRole("link", {
+      name: "fair use basics",
+    });
+    this.fairUseChecklistLink = page.getByRole("link", {
+      name: "fair use checklist",
+    });
+    this.audioAndMovingImageLink = page.getByRole("link", {
+      name: "Audio and Moving Image Preservation and Access Initiative.",
+    });
+    this.nyplCatalogLink = page.getByRole("link", {
+      name: "NYPL Catalog",
+    });
+    this.nyplArchivesPortalLink = page.getByRole("link", {
+      name: "NYPL Archives Portal",
+    });
+
+    this.digitalPublicLibraryOfAmericaLink = page.getByRole("link", {
+      name: "Digital Public Library of America",
+    });
+    this.creativeCommonsLink = page.getByRole("link", {
+      name: "Creative Commons CC0 1.0 Universal Public Domain Dedication",
+    });
+    this.nyplDigitalCollectionsAPILink = page.getByRole("link", {
+      name: "The New York Public Library Digital Collections API",
+    });
+
+    this.publicDomainDigitalCollectionsGithubLink = page.getByRole("link", {
+      name: "Public Domain Digital Collections GitHub",
+    });
+    this.dplaLink = page.getByRole("link", {
+      name: "Digital Public Library of America (DPLA)",
+    });
+    this.modsLink = page.getByRole("link", {
+      name: "Metadata Object Description Schema (MODS)",
+    });
+    this.internetArchiveBookReaderLink = page.getByRole("link", {
+      name: "Internet Archive BookReader",
+    });
+    this.iiifComplianceLink = page.getByRole("link", {
+      name: "IIIF Compliance",
+    });
+    this.webAndMobileAccessibilityPolicyLink = page.getByRole("link", {
+      name: "Web and Mobile Accessibility Policy",
+    });
+    this.emailLink = page.getByRole("link", { name: "Email" });
   }
 }
--- playwright/tests/about.spec.ts
+++ playwright/tests/about.spec.ts
@@ -22,92 +22,35 @@ test("verify search bar, search button, public domain checkbox on about page", a
   await expect(aboutPage.publicDomaincheckbox).toBeVisible();
 });
 
-test("verify text on about page", async ({ page }) => {
-  const aboutPage = new AboutPage(page);
-  const text = await aboutPage.aboutPageText.textContent();
-  await expect(aboutPage.aboutPageText).toBeVisible();
-  await expect(text).toContain("About NYPL Digital Collections");
-  await expect(text).toContain("Find items");
-  await expect(text).toContain("Download items");
-  await expect(text).toContain(
-    "Public Domain / no known U.S. copyright restrictions"
-  );
-  await expect(text).toContain(
-    "Is there a difference between 'Public Domain' & 'no known U.S. copyright restrictions'?"
-  );
-  await expect(text).toContain(
-    "Copyright held or managed by The New York Public Library"
-  );
-  await expect(text).toContain("Copyright status undefined");
-  await expect(text).toContain("Audio and Moving Image Materials");
-  await expect(text).toContain("Data");
-  await expect(text).toContain("Linked projects");
-  await expect(text).toContain("Available metadata");
-  await expect(text).toContain("Platform");
-  await expect(text).toContain("Accessibility");
-  await expect(text).toContain(
-    "NYPL's statement on potentially harmful content"
-  );
-  await expect(text).toContain("Frequently asked questions");
-  await expect(text).toContain(
-    "What harmful or difficult content may be found in NYPL's Digital Collections?"
-  );
-  await expect(text).toContain(
-    "Why does NYPL make potentially harmful content available?"
-  );
-  await expect(text).toContain(
-    "How is this material described, and why are some of the terms used in the descriptions harmful?"
-  );
-  await expect(text).toContain(
-    "How are librarians, cataloguers, and archivists working to address this problem and help users better understand such content?"
-  );
-  await expect(text).toContain("How can I report harmful content?");
-  await expect(text).toContain("Contact");
-  await expect(text).toContain("Acknowledgements");
-});
-
 test("verify links on about page", async ({ page }) => {
   const aboutPage = new AboutPage(page);
-  const links = await aboutPage.aboutLinks.allTextContents();
-
-  await expect(links).toContain("search");
-  await expect(links).toContain("item");
-  await expect(links).toContain("collection");
-  await expect(links).toContain("division");
-  await expect(links).toContain(
-    '"NYPL Digital Collections Platform: An Introduction"'
-  );
-  await expect(links).toContain(
-    "Free for All: NYPL Enhances Public Domain Collections for Sharing and Reuse."
-  );
-  await expect(links).toContain("Permissions and Reproductions");
-  await expect(links).toContain(
-    "How to Investigate the Copyright Status of a Work [PDF]"
-  );
-  await expect(links).toContain("public domain determination chart");
-  await expect(links).toContain("Wikipedia");
-  await expect(links).toContain("fair use basics");
-  await expect(links).toContain("fair use checklist");
-  await expect(links).toContain("Permissions and Reproductions");
-  await expect(links).toContain(
-    "Audio and Moving Image Preservation and Access Initiative."
-  );
-  await expect(links).toContain("NYPL Catalog");
-  await expect(links).toContain("NYPL Archives Portal");
-  await expect(links).toContain("Digital Public Library of America");
-  await expect(links).toContain(
-    "Creative Commons CC0 1.0 Universal Public Domain Dedication"
-  );
-  await expect(links).toContain(
-    "The New York Public Library Digital Collections API"
-  );
-  await expect(links).toContain(
-    "public domain portion of Digital Collections on GitHub"
-  );
-  await expect(links).toContain("Digital Public Library of America (DPLA)");
-  await expect(links).toContain("MODS");
-  await expect(links).toContain("Internet Archive's BookReader.");
-  await expect(links).toContain("IIIF compliance");
-  await expect(links).toContain("Web & Mobile Accessibility Policy.");
-  await expect(links).toContain("digitalcollections@nypl.org");
+  await expect(aboutPage.searchLink).toBeVisible();
+  await expect(aboutPage.itemLink).toBeVisible();
+  await expect(aboutPage.collectionLink).toBeVisible();
+  await expect(aboutPage.divisionLink).toBeVisible();
+  await expect(aboutPage.aboutLink).toBeVisible();
+  await expect(aboutPage.browseItemsLink).toBeVisible();
+  await expect(aboutPage.publicDomainItemsLink).toBeVisible();
+  await expect(aboutPage.freeForAllLink).toBeVisible();
+  await expect(aboutPage.permissionsAndReproductionsLink).toBeVisible();
+  await expect(aboutPage.investigateCopywrightLink).toBeVisible();
+  await expect(aboutPage.publicDomainDeterminationChartLink).toBeVisible();
+  await expect(aboutPage.wikipediaLink).toBeVisible();
+  await expect(aboutPage.fairUseBasicsLink).toBeVisible();
+  await expect(aboutPage.fairUseChecklistLink).toBeVisible();
+  await expect(aboutPage.audioAndMovingImageLink).toBeVisible();
+  await expect(aboutPage.nyplCatalogLink).toBeVisible();
+  await expect(aboutPage.nyplArchivesPortalLink).toBeVisible();
+  await expect(aboutPage.digitalPublicLibraryOfAmericaLink).toBeVisible();
+  await expect(aboutPage.creativeCommonsLink).toBeVisible();
+  await expect(aboutPage.nyplDigitalCollectionsAPILink).toBeVisible();
+  await expect(
+    aboutPage.publicDomainDigitalCollectionsGithubLink
+  ).toBeVisible();
+  await expect(aboutPage.dplaLink).toBeVisible();
+  await expect(aboutPage.modsLink).toBeVisible();
+  await expect(aboutPage.internetArchiveBookReaderLink).toBeVisible();
+  await expect(aboutPage.iiifComplianceLink).toBeVisible();
+  await expect(aboutPage.webAndMobileAccessibilityPolicyLink).toBeVisible();
+  await expect(aboutPage.emailLink).toBeVisible();
 });
