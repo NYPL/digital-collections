438010bb2131dc08b96de5d785fcbab493ed86d8
roseb89 <dz4888@wayne.edu>
2025-07-18 15:33:06 -0400

    added tests for one george arents divisions landing page and updated changelog, and rename test file to the specific division's name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+- Added Playwright test for George Arents Division's page (DR-3716)
 ### Added
 - Added Playwright tests to confirm featured section on the homepage (DR-3658)
 - Added Playwright tests to clear search results filters (DR-3659)
---
+++ playwright/tests/george-arent-divisions-landing.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+import { GeorgeArentDivisionsLandingPage } from "../pages/george-arent-divisions-landing-page";
+import { DivisionsPage } from "../pages/divisions.page";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(DivisionsPage.divisionsUrl);
+  const divisionsPage = new DivisionsPage(page);
+  await expect(divisionsPage.seeMoreLink.nth(4)).toBeVisible();
+  await divisionsPage.seeMoreLink.nth(4).click();
+});
+
+test("from divisions page click see more link and verify divisions landing page", async ({
+  page,
+}) => {
+  const divisionsPage = new DivisionsPage(page);
+  const divisionsLandingPage = new GeorgeArentDivisionsLandingPage(page);
+
+  await expect(divisionsLandingPage.itemsGeorgeDivisionHeading).toBeVisible();
+  await expect(divisionsLandingPage.itemsGeorgeArentsSeemoreLink).toBeVisible();
+
+  await expect(divisionsLandingPage.contactLink).toBeVisible();
+  await expect(
+    divisionsLandingPage.collectionsGeorgeArentsHeading
+  ).toBeVisible();
+  await expect(divisionsLandingPage.georgeArentsPagination).toBeVisible();
+});
