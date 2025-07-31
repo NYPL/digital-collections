2b455fcae78eac4ffb91c0a0497c8d5cb7be2392
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-07-31 14:42:02 -0400

    add IIIF Manifest button to Data Sources

--- app/src/components/items/overview/external/overview.tsx
+++ app/src/components/items/overview/external/overview.tsx
@@ -1,13 +1,13 @@
 import { Box, Heading, Link, Text } from "@nypl/design-system-react-components";
 
-const ExternalLinksOverview = ({ catalogLink, archivesLink }) => {
+const ExternalLinksOverview = ({ catalogLink, archivesLink, manifestURL }) => {
   return (
     <>
       <Box marginBottom="m">
         <Heading size="heading6" marginBottom="xs">
           {catalogLink && archivesLink ? "Data sources:" : "Data source:"}
         </Heading>
-        {!catalogLink && !archivesLink && (
+        {!catalogLink && !archivesLink && !manifestURL && (
           <Text>This item does not have any external references yet</Text>
         )}
         {archivesLink && (
@@ -31,8 +31,22 @@ const ExternalLinksOverview = ({ catalogLink, archivesLink }) => {
             target="_blank"
             aria-label={`view in catalog`}
             type="buttonSecondary"
+            marginRight="xs"
           >
             Research Catalog
+          </Link>
+        )}
+        {manifestURL && (
+          <Link
+            href={manifestURL}
+            id={"iiif-manifest-btn"}
+            isUnderlined={false}
+            target="_blank"
+            aria-label={`view IIIF manifest`}
+            type="buttonSecondary"
+            marginRight="xs"
+          >
+            IIIF Manifest
           </Link>
         )}
       </Box>
