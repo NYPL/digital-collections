1d9227cd227227bd7c2f6f4d8ad7d369f7b6b091
samanthaandrews <samantharaeandrews@gmail.com>
2025-09-16 21:51:51 -0400

    just do a prod build in ci and run npm run start in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,12 +73,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: !!process.env.CI
-    ? undefined
-    : {
-        // only run in local environment not CI
-        command: "npm run dev",
-        url: "http://localhost:3000",
-        reuseExistingServer: !process.env.CI,
-      },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
