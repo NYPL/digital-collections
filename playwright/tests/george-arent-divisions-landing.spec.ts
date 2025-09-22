58fa8e0ce32bba265242eadcdc8d585b1c87a825
jbdalton <josephdalton@nypl.org>
2025-09-22 15:45:28 -0400

    put 3rd-party domain route-filters in config to be called globally

--- playwright/tests/george-arent-divisions-landing.spec.ts
+++ playwright/tests/george-arent-divisions-landing.spec.ts
@@ -3,19 +3,6 @@ import { GeorgeArentDivisionsLandingPage } from "../pages/george-arent-divisions
 import { DivisionsPage } from "../pages/divisions.page";
 
 test.beforeEach(async ({ page }) => {
-  // Block analytics, tracking, and third-party domains
-  await page.route(/.*adobedc\.net.*/, (route) => route.abort());
-  await page.route(/.*adobedtm\.com.*/, (route) => route.abort());
-  await page.route(/.*demdex\.net.*/, (route) => route.abort());
-  await page.route(/.*everesttech\.net.*/, (route) => route.abort());
-  await page.route(/.*google-analytics\.com.*/, (route) => route.abort());
-  await page.route(/.*google\.com.*/, (route) => route.abort());
-  await page.route(/.*googletagmanager\.com.*/, (route) => route.abort());
-  await page.route(/.*ipify\.org.*/, (route) => route.abort());
-  await page.route(/.*newrelic\.com.*/, (route) => route.abort());
-  await page.route(/.*nr-data\.net.*/, (route) => route.abort());
-  await page.route(/.*omappapi\.com.*/, (route) => route.abort());
-
   // Navigate to the page after setting up the routing rules.
   await page.goto(DivisionsPage.divisionsUrl);
   const divisionsPage = new DivisionsPage(page);
