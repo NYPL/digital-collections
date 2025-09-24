08d018d743883b43e035f6dccd8fa2853fa883ea
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-09-24 15:00:30 -0400

    remove search link test on about page

--- playwright/tests/about.spec.ts
+++ playwright/tests/about.spec.ts
@@ -24,7 +24,6 @@ test("verify search bar, search button, public domain checkbox on about page", a
 
 test("verify links on about page", async ({ page }) => {
   const aboutPage = new AboutPage(page);
-  await expect(aboutPage.searchLink).toBeVisible();
   await expect(aboutPage.itemLink).toBeVisible();
   await expect(aboutPage.collectionLink).toBeVisible();
   await expect(aboutPage.divisionLink).toBeVisible();
