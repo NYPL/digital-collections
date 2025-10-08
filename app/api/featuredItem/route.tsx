1a1c32c00d4560663a04134f8dd5c58eaa9e5b42
Sarang Joshi <sarangjoshi@nypl.org>
2025-10-08 14:38:12 -0400

    fix: Fix featured item API typing
    There shouldn't be a second arg, just a return type.

--- app/api/featuredItem/route.tsx
+++ app/api/featuredItem/route.tsx
@@ -1,8 +1,11 @@
 import { NextResponse, NextRequest } from "next/server";
 import { CollectionsApi } from "@/src/utils/apiClients/apiClients";
+import { FeaturedItemDataType } from "@/src/types/FeaturedItemDataType";
 export const fetchCache = "force-no-store";
 
-export const GET = async (request: NextRequest, response: NextResponse) => {
+export const GET = async (
+  request: NextRequest
+): Promise<NextResponse<FeaturedItemDataType>> => {
   const data = await CollectionsApi.getFeaturedItemData();
 
   const newResponse = NextResponse.json(data, { status: 200 });
