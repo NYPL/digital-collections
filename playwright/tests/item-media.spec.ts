408d6d45e5ce12ab7705e337c09431c912cae16c
jbdalton <josephdalton@nypl.org>
2025-10-16 21:50:54 -0400

    get UUIDs from test-data util, and move UV-ready check to page object

--- playwright/tests/item-media.spec.ts
+++ playwright/tests/item-media.spec.ts
@@ -6,18 +6,15 @@ let itemMediaPage: ItemMediaPage;
 // configure  suite's timeout settings before the describe block.
 test.describe.configure({ timeout: 90000 });
 
-test.describe("Image Viewer Control Verification", () => {
+test.describe("Verify Image Viewer Controls", () => {
   test.beforeEach(async ({ page }) => {
     itemMediaPage = new ItemMediaPage(page);
 
     await itemMediaPage.loadPage(ItemMediaPage.itemMediaURL);
-
-    // wait for stable UV heading to confirm the module is fully loaded
-    await itemMediaPage.viewerHeading.waitFor({ state: "visible" });
   });
 
-  test("should verify all IMAGE-specific controls are visible", async () => {
-    await expect(itemMediaPage.viewerHeading).toBeVisible();
+  test("all image-specific controls are visible", async () => {
+    await itemMediaPage.verifyViewerIsReady();
 
     // check if IMAGE Controls are visible
     await expect(itemMediaPage.zoomInButton).toBeVisible();
@@ -25,7 +22,7 @@ test.describe("Image Viewer Control Verification", () => {
     await expect(itemMediaPage.fullScreenButton).toBeVisible();
   });
 
-  test("should verify VIDEO controls are NOT visible", async () => {
+  test("video controls are NOT visible", async () => {
     // check that no AUDIO/VIDEO controls are appearing (verifying correct UV mode)
     await expect(itemMediaPage.playButton).toBeHidden();
   });
