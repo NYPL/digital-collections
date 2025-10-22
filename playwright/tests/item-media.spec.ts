d69d345da880527ad15d46f69be13bf8f72f8d3c
jbdalton <josephdalton@nypl.org>
2025-10-22 15:12:28 -0400

    skip negative assertions - if the viewer is wrong then the primary assertions will fail, which is the functionality we are  testing

--- playwright/tests/item-media.spec.ts
+++ playwright/tests/item-media.spec.ts
@@ -13,7 +13,7 @@ test.describe("Verify Image Viewer Controls", () => {
     await itemMediaPage.loadPage(ItemMediaPage.itemImageURL);
   });
 
-  test("all image-specific controls are visible", async () => {
+  test("primary image-specific controls are visible", async () => {
     await itemMediaPage.verifyViewerIsReady();
 
     // check if IMAGE Controls are visible
@@ -22,7 +22,8 @@ test.describe("Verify Image Viewer Controls", () => {
     await expect(itemMediaPage.fullScreenButton).toBeVisible();
   });
 
-  test("video controls are NOT visible", async () => {
+  // flakey test because it checks before ANY controls are visible, so will always pass
+  test.skip("video controls are NOT visible", async () => {
     // check that no AUDIO/VIDEO controls are appearing (verifying correct UV mode)
     await expect(itemMediaPage.playButton).toBeHidden();
   });
@@ -35,7 +36,7 @@ test.describe("Verify Video Viewer Controls", () => {
     await itemMediaPage.loadPage(ItemMediaPage.itemVideoURL);
   });
 
-  test("all video-specific controls are visible", async () => {
+  test("primary video-specific controls are visible", async () => {
     await itemMediaPage.verifyViewerIsReady();
 
     // check if VIDEO Controls are visible
@@ -43,8 +44,18 @@ test.describe("Verify Video Viewer Controls", () => {
     await expect(itemMediaPage.playButton).toBeVisible();
   });
 
-  test("image controls are NOT visible", async () => {
-    // check that no IMAGE controls are appearing (verifying correct UV mode)
+  // unnecessary assertions, which also will get false-positives in async mode
+  test.skip("image controls are NOT visible", async () => {
+    // This will get a false positive in the DOM for the same reasons as
+    // the negative-assertion about video-controls will similarly fail in
+    // the main image-controls test.
+    // We could serially wait for the answer to the previous
+    // assertion (is media-play-button visible?) here, first.  But we
+    // can probably leave these negative assertions below out, because if
+    // you are on a video player, it's all or nothing:  you're not going to see
+    // ANY of the image controls we are checking
+    // for below on the video-player UV.  And the reverse holds true
+    // for the image-viewer UV.
     await expect(itemMediaPage.zoomInButton).toBeHidden();
     await expect(itemMediaPage.rotateRightButton).toBeHidden();
     await expect(itemMediaPage.fullScreenButton).toBeHidden();
