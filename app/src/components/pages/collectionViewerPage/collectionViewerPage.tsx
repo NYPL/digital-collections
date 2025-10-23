689880553ae35dd4e5f5840c4cb0a48d9285e393
Keith Bauer <bauer.keith@gmail.com>
2025-10-23 13:35:50 -0400

    Add UV CSS

--- app/src/components/pages/collectionViewerPage/collectionViewerPage.tsx
+++ app/src/components/pages/collectionViewerPage/collectionViewerPage.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { CanvasProvider } from "@/src/context/CanvasProvider";
 import { UniversalViewer } from "@/src/components/collections/uv/universalViewerLazy";
+import "universalviewer/dist/esm/index.css";
 import { Box } from "@nypl/design-system-react-components";
 
 export function CollectionViewerPage({ manifestId }) {
