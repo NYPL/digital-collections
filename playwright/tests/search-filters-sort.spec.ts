0ea4082c0e66e55969d400133a0cdfd6b09fcc9e
jbdalton <josephdalton@nypl.org>
2025-11-14 16:06:43 -0500

    moved sort-filters to their own test file

---
+++ playwright/tests/search-filters-sort.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from "../base";
+import SearchPage from "../pages/search.page";
+
+let searchPage: SearchPage;
+
+test.describe.serial("choose specific sort options", () => {
+  test.beforeEach(async ({ page }) => {
+    searchPage = new SearchPage(page);
+    await searchPage.loadPage(SearchPage.searchResultsUrl);
+
+    await searchPage.sortButton.click();
+
+    await expect(searchPage.refineHeading).toBeVisible();
+  });
+
+  test("sorts search results by relevance", async ({ page }) => {
+    // await searchPage.sortButton.click();
+    await expect(searchPage.sortByRelevance).toBeVisible();
+    await searchPage.sortByRelevance.click({ force: true });
+    await expect(searchPage.sortByRelevanceSelected).toBeVisible({
+      timeout: 20000,
+    });
+  });
+
+  test("sorts search results by age", async ({ page }) => {
+    // const searchPage = new SearchPage(page);
+    await expect(searchPage.sortByNewest).toBeVisible();
+    await expect(searchPage.sortByOldest).toBeVisible();
+    await searchPage.sortByNewest.click({ force: true });
+    await expect(searchPage.sortByNewestSelected).toBeVisible({
+      timeout: 20000,
+    });
+  });
+
+  test("sorts search results alphabetically", async ({ page }) => {
+    // const searchPage = new SearchPage(page);
+    await expect(searchPage.sortByAlpha).toBeVisible();
+    await expect(searchPage.sortByReverseAlpha).toBeVisible();
+    await searchPage.sortByAlpha.click({ force: true });
+    await expect(searchPage.sortByAlphaSelected).toBeVisible({
+      timeout: 20000,
+    });
+  });
+
+  test("sorts search results by type", async ({ page }) => {
+    // const searchPage = new SearchPage(page);
+    await expect(searchPage.sortByCollections).toBeVisible();
+    await expect(searchPage.sortByItems).toBeVisible();
+    await searchPage.sortByCollections.click({ force: true });
+    await expect(searchPage.sortByCollectionsSelected).toBeVisible({
+      timeout: 20000,
+    });
+  });
+});
