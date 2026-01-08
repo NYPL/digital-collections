87089dbf5dbb476dddd7755996150cd862696f30
Alessandra Vertrees <alessandravertrees@nypl.org>
2026-01-08 14:27:44 -0500

    bug fixes

--- app/src/components/card/searchCardImage.tsx
+++ app/src/components/card/searchCardImage.tsx
@@ -36,8 +36,8 @@ export const SearchCardImage = ({ record, viewMode }) => {
         fill
         decoding="sync"
         style={{
-          objectFit: viewMode == "grid" ? "contain" : "cover",
-          objectPosition: "center", //viewMode == "grid" ? "center" : "top",
+          objectFit: viewMode === "grid" ? "contain" : "cover",
+          objectPosition: "center",
         }}
         onError={(_event) => {
           console.warn(
