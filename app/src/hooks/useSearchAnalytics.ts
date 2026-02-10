769e77d02103e5132019327a57d6892c058cba3b
sarangj <sarangjoshi22@gmail.com>
2026-02-10 11:32:24 -0500

    fix: Don't send search results event for viewmode
    if the viewmode is the only thing that changed, we don't want to send
    this event.

--- app/src/hooks/useSearchAnalytics.ts
+++ app/src/hooks/useSearchAnalytics.ts
@@ -7,23 +7,26 @@ import { trackSearchResults } from "../utils/ga4Utils";
 
 const useSearchAnalytics = (searchManager: SearchManager) => {
   const filters = searchManager.filters;
-  const filterNames = filters.map((f) => f.filter);
-  const data = {
-    searchTerm: searchManager.keywords ?? undefined,
-    filterNames: searchManager.filters.map((f) => f.filter),
-    searchResultsLayout: searchManager.viewMode,
-    searchType:
-      searchManager.filters.find((f) => f.filter === "rights")?.value ??
-      undefined,
-  };
+  const filterNames = filters.map((f) => f.filter) ?? undefined;
+  const keywords = searchManager.keywords ?? undefined;
+  const searchType =
+    searchManager.filters.find((f) => f.filter === "rights")?.value ??
+    undefined;
+  // We don't send an event if the viewMode is the only thing that changed
+  const changeHash = JSON.stringify({
+    filterNames: filterNames,
+    searchType: searchType,
+    keywords: keywords,
+  });
+
   useEffect(() => {
     trackSearchResults(
-      data.searchResultsLayout,
-      data.filterNames,
-      data.searchType,
-      data.searchTerm
+      searchManager.viewMode,
+      filterNames,
+      searchType,
+      keywords
     );
-  }, [JSON.stringify(data)]);
+  }, [changeHash]);
 };
 
 export default useSearchAnalytics;
