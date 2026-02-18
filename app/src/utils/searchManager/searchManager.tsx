7c9a0a5301e2057504074f8cc1e699f00382f4dc
Keith Bauer <bauer.keith@gmail.com>
2026-02-18 14:16:48 -0500

    Use persisted viewMode when possible

--- app/src/utils/searchManager/searchManager.tsx
+++ app/src/utils/searchManager/searchManager.tsx
@@ -39,7 +39,7 @@ abstract class BaseSearchManager implements SearchManager {
   protected defaultSort: string;
   protected currentKeywords: string;
   protected currentFilters: Set<string>;
-  protected currentViewMode: "grid" | "list";
+  protected currentViewMode: "grid" | "list" | undefined;
   protected currentAvailableFilters: AvailableFilter[];
   public lastFilterRef: MutableRefObject<string | null>;
 
@@ -65,7 +65,9 @@ abstract class BaseSearchManager implements SearchManager {
       (config.initialFilters || []).map((filter) => JSON.stringify(filter))
     );
     this.currentKeywords = config.initialKeywords;
-    this.currentViewMode = config.initialViewMode || DEFAULT_VIEW_MODE;
+    this.currentViewMode = config.initialViewMode
+      ? config.initialViewMode
+      : undefined;
     this.currentAvailableFilters = transformToDisplayAvailableFilters(
       config.initialAvailableFilters ?? {}
     );
@@ -96,13 +98,14 @@ abstract class BaseSearchManager implements SearchManager {
 
   get viewMode() {
     if (typeof window !== "undefined") {
-      return (
-        (localStorage.getItem("viewMode") as "grid" | "list") ||
-        this.currentViewMode
-      );
-    } else {
-      return this.currentViewMode;
+      const cachedViewMode = localStorage.getItem("viewMode") as
+        | "grid"
+        | "list";
+      if (!this.currentViewMode && cachedViewMode) {
+        return cachedViewMode;
+      }
     }
+    return this.currentViewMode || DEFAULT_VIEW_MODE;
   }
 
   get availableFilters(): AvailableFilter[] {
@@ -241,7 +244,7 @@ export class GeneralSearchManager extends BaseSearchManager {
           isDefault = value === "";
           break;
         case "viewMode":
-          isDefault = value === DEFAULT_VIEW_MODE;
+          isDefault = false;
           break;
       }
 
@@ -317,7 +320,7 @@ export class CollectionSearchManager extends BaseSearchManager {
           isDefault = value === "";
           break;
         case "viewMode":
-          isDefault = value === DEFAULT_VIEW_MODE;
+          isDefault = false;
           break;
       }
 
