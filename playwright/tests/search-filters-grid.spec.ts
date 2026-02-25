c402e5d506a494821a6bd7655e21a460f6172b34
jbdalton <josephdalton@nypl.org>
2026-02-25 08:14:59 -0500

    add session/state tests

--- playwright/tests/search-filters-grid.spec.ts
+++ playwright/tests/search-filters-grid.spec.ts
@@ -4,50 +4,56 @@ import { applyRouteFilters } from "../utils/routeFilters";
 
 let searchPage: SearchPage;
 
-test.describe.serial("Verify Grid/List Layouts", () => {
+test.describe("Verify Grid and List Layouts", () => {
   test.beforeAll(async ({ browser }) => {
-    // Manually create context/page to force serialization to remain in block
     const browserContext = await browser.newContext();
     const page = await browserContext.newPage();
-
-    // Assign to global variable and load the page object
     searchPage = new SearchPage(page);
     await searchPage.loadPage(SearchPage.largeResultsUrl);
   });
 
-  // TEARDOWN: close the entire context.
   test.afterAll(async () => {
-    // Closes the BrowserContext, freeing up resources.
     await searchPage.page.context().close();
   });
 
-  test.describe.serial("Toggle default results to list view", async () => {
-    await test.step("When clicking the list view icon", async () => {
+  test.describe.serial("List view", () => {
+    test("list button is highlighted", async () => {
+      await searchPage.listViewButton.click({ force: true });
+      await expect(searchPage.listViewButton).toHaveAttribute(
+        "aria-pressed",
+        "true"
+      );
+      await expect(searchPage.gridViewButton).toHaveAttribute(
+        "aria-pressed",
+        "false"
+      );
+    });
+
+    test("results are displayed in a list view", async () => {
+      await searchPage.verifyLayout("list");
+    });
+
+    test("multi-row results retain their list view", async () => {
+      await searchPage.verifyMultiRowLayout("list");
+    });
+
+    test("remains a list after reload", async () => {
+      await searchPage.verifyLayoutReloadState("list");
+    });
+
+    test("remains a list after a new search", async () => {
+      await searchPage.verifyLayoutSearch("list");
+    });
+  });
+
+  test.describe.serial("Grid view", () => {
+    test.beforeAll(async () => {
+      // set up list view first, since grid is default
       await searchPage.listViewButton.click({ force: true });
     });
 
-    await test.step("list button is highlighted", async () => {
-      await expect(searchPage.listViewButton).toHaveAttribute(
-        "aria-pressed",
-        "true"
-      );
-      await expect(searchPage.gridViewButton).toHaveAttribute(
-        "aria-pressed",
-        "false"
-      );
-    });
-
-    await test.step("results are displayed in a list view", async () => {
-      await searchPage.verifyLayout("list");
-    });
-  });
-
-  test("Toggle search results back to grid view", async () => {
-    await test.step("When clicking the grid view icon", async () => {
+    test("grid button is highlighted", async () => {
       await searchPage.gridViewButton.click({ force: true });
-    });
-
-    await test.step("grid button is highlighted", async () => {
       await expect(searchPage.gridViewButton).toHaveAttribute(
         "aria-pressed",
         "true"
@@ -58,61 +64,20 @@ test.describe.serial("Verify Grid/List Layouts", () => {
       );
     });
 
-    await test.step("results are displayed in a grid", async () => {
+    test("results are displayed in a grid", async () => {
       await searchPage.verifyLayout("grid");
     });
-  });
-});
 
-test.describe.serial("toggle results visualization", () => {
-  test.beforeEach(async ({ page }) => {
-    searchPage = new SearchPage(page);
-    await searchPage.loadPage(SearchPage.largeResultsUrl);
-  });
-
-  test("toggles search results to list view", async () => {
-    await test.step("Click the list view icon", async () => {
-      await searchPage.listViewButton.click({ force: true });
+    test("multi-row results retain their grid view", async () => {
+      await searchPage.verifyMultiRowLayout("grid");
     });
 
-    await test.step("Verify list button shows 'pressed'", async () => {
-      await expect(searchPage.listViewButton).toHaveAttribute(
-        "aria-pressed",
-        "true"
-      );
-      await expect(searchPage.gridViewButton).toHaveAttribute(
-        "aria-pressed",
-        "false"
-      );
+    test("remains a grid after reload", async () => {
+      await searchPage.verifyLayoutReloadState("grid");
     });
 
-    await test.step("Verify results are displayed in a vertical stack", async () => {
-      // Logic defined in POM: checks that box2.y > box1.y
-      await searchPage.verifyLayout("list");
-    });
-  });
-
-  test("toggles search results back to grid view", async () => {
-    // Because of .serial, we are already in List view from the previous test
-
-    await test.step("Click the grid view icon", async () => {
-      await searchPage.gridViewButton.click({ force: true });
-    });
-
-    await test.step("Verify grid button shows 'pressed'", async () => {
-      await expect(searchPage.gridViewButton).toHaveAttribute(
-        "aria-pressed",
-        "true"
-      );
-      await expect(searchPage.listViewButton).toHaveAttribute(
-        "aria-pressed",
-        "false"
-      );
-    });
-
-    await test.step("Verify results are displayed in a grid", async () => {
-      // Logic defined in POM: checks that box1.y is close to box2.y
-      await searchPage.verifyLayout("grid");
+    test("remains a grid after a new search", async () => {
+      await searchPage.verifyLayoutSearch("grid");
     });
   });
 });
