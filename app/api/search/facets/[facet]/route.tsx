f2718b01c1cc5c11ae47d17b3c0f8f38f6efd692
sarangj <sarangjoshi22@gmail.com>
2026-03-25 12:51:15 -0400

    Map filters to API

--- app/api/search/facets/[facet]/route.tsx
+++ app/api/search/facets/[facet]/route.tsx
@@ -1,16 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import { CollectionsApi } from "@/src/utils/apiClients/apiClients";
+import { Filter } from "@/src/types/FilterType";
+
+export interface SearchParamsType {
+  q: string;
+  filters: Filter[];
+}
+
+export type SearchProps = {
+  searchParams: SearchParamsType;
+};
 
 export const GET = async (
   request: NextRequest,
   { params }: { params: { facet: string } }
 ) => {
-  const { facet } = params;
-  const url = new URL(request.url);
-  const queryString = url.searchParams.toString();
-
+  const searchParams = request.nextUrl.searchParams;
+  const q = searchParams.get("q") || "";
+  const filters = searchParams.get("filters") || "";
   try {
-    const response = await CollectionsApi.getFacetOptions(facet, queryString);
+    const response = await CollectionsApi.getFacetOptions(
+      params.facet,
+      q,
+      filters
+    );
     return NextResponse.json(response, { status: 200 });
   } catch (error: any) {
     return NextResponse.json(
