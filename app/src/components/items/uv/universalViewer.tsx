56ce482c8efbbcba8b46e22f1bc3c1d02254e610
sarangj <sarangjoshi22@gmail.com>
2026-04-10 15:12:49 -0400

    Consolidate UV Config
    We pass a config object as a prop to the UV component, as well as to the
    'configure' event. Let's make sure it's the same object.
    
    I also stuck the config object into public/ so that archives portal
    could pass a 'config' param to the iframe src so that we can use the
    same config from archives.

--- app/src/components/items/uv/universalViewer.tsx
+++ app/src/components/items/uv/universalViewer.tsx
@@ -5,6 +5,7 @@ import {
 } from "../../../hooks/useUniversalViewer";
 import React, { useEffect, useMemo, useRef } from "react";
 import { useCanvasContext } from "../../../context/CanvasProvider";
+import uvConfig from "../../../../../public/uvConfig.json";
 
 export type UniversalViewerProps = {
   config?: any;
@@ -102,129 +103,9 @@ const UniversalViewer: React.FC<UniversalViewerProps> = React.memo(
       let mo: MutationObserver | undefined;
 
       if (uv) {
-        // Hide specific default download options by button/anchor text. Right now, just "Whole imiage".
-
         // override config using an inline json object
         uv.on("configure", function ({ config, cb }) {
-          console.log("config on uv.on(configure) is : ", config);
-          console.log("cb is: ", cb);
-          cb(
-            {
-              options: {
-                footerPanelEnabled: true,
-                pagingEnabled: true,
-                pagingHeaderPanel: true,
-                pagingOptionEnabled: true,
-                clickToZoomEnabled: false,
-                zoomToBoundsEnabled: false,
-                // saveUserSettings: false, // uncomment if you want to stop new prefs persisting
-              },
-              modules: {
-                headerPanel: {
-                  options: {
-                    centerOptionsEnabled: true,
-                    localeToggleEnabled: false,
-                    settingsButtonEnabled: true,
-                  },
-                },
-                pagingHeaderPanel: {
-                  options: {
-                    autoCompleteBoxEnabled: true,
-                    autocompleteAllowWords: false,
-                    galleryButtonEnabled: true,
-                    imageSelectionBoxEnabled: false,
-                    pageModeEnabled: false,
-                    pagingToggleEnabled: true,
-                  },
-                  content: {
-                    close: "Close",
-                    emptyValue: "Please enter a value",
-                    first: "First",
-                    firstImage: "First Image",
-                    firstPage: "First Page",
-                    folio: "Folio",
-                    gallery: "Gallery",
-                    go: "Go",
-                    help: "Help",
-                    image: "Image",
-                    last: "Last",
-                    lastImage: "Last Image",
-                    lastPage: "Last Page",
-                    next: "Next",
-                    nextImage: "Next Image",
-                    nextPage: "Next Page",
-                    of: "of {0}",
-                    oneUp: "Single page view",
-                    page: "Page",
-                    pageSearchLabel: "Search by Page Number",
-                    previous: "Previous",
-                    previousImage: "Previous Image",
-                    previousPage: "Previous Page",
-                    settings: "Settings",
-                    twoUp: "Two page view",
-                  },
-                },
-                shareDialogue: {
-                  options: {
-                    embedTemplate:
-                      '<iframe src="{0}" width="{1}" height="{2}" allowfullscreen frameborder="0"></iframe>',
-                    instructionsEnabled: false,
-                    shareFrameEnabled: true,
-                    shareManifestsEnabled: true,
-                  },
-                  content: {
-                    customSize: "custom",
-                    embed: "Embed",
-                    embedInstructions:
-                      "To embed this item in your own website, copy and paste the code below.",
-                    height: "Height",
-                    iiif: "IIIF Manifest",
-                    share: "Share",
-                    shareInstructions:
-                      "To share this item, copy the URL below.",
-                    size: "Size:",
-                    width: "Width",
-                  },
-                },
-                openSeadragonCenterPanel: {
-                  options: {
-                    animationTime: 0.15,
-                    autoHideControls: false,
-                    requiredStatementEnabled: true,
-                    blendTime: 0,
-                    constrainDuringPan: true,
-                    controlsFadeAfterInactive: 10000,
-                    controlsFadeDelay: 20000,
-                    controlsFadeLength: 20000,
-                    defaultZoomLevel: 0,
-                    immediateRender: false,
-                    maxZoomPixelRatio: 1.25,
-                    navigatorPosition: "BOTTOM_RIGHT",
-                    pageGap: 50,
-                    showHomeControl: true,
-                    trimAttributionCount: 150,
-                    visibilityRatio: 0.5,
-                  },
-                  content: {
-                    attribution: "Attribution",
-                    goHome: "Go Home",
-                    imageUnavailable: "Image Unavailable",
-                    next: "Next",
-                    previous: "Previous",
-                    rotateRight: "Rotate Right",
-                    zoomIn: "Zoom In",
-                    zoomOut: "Zoom Out",
-                  },
-                },
-                downloadDialogue: {
-                  options: {
-                    downloadCurrentViewEnabled: false,
-                  },
-                },
-              },
-            },
-            [uv]
-          );
+          cb(uvConfig, [uv]);
         });
       }
     }, [canvasIndex, uv]);
