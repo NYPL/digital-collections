920403cd6aee8531c461aa9b5ffb74c9012c1a5d
Alessandra Vertrees <alessandravertrees@nypl.org>
2026-04-15 16:16:45 -0400

    get link to show up

--- app/src/models/item.ts
+++ app/src/models/item.ts
@@ -16,7 +16,7 @@ import {
 } from "../utils/metadata/generateCitations";
 import { APIItem } from "../types/CollectionsAPI";
 import { PlyrCaption } from "../components/items/plyr/player";
-import allMapsData from "../data/maps/allmaps";
+import allMapsData from "../data/maps/data";
 
 // https://github.com/jptmoore/maniiifest
 // other resources:
@@ -218,9 +218,6 @@ export class ItemModel {
     );
     this.captions = this.captures.flatMap((capture) => capture.captions || []);
 
-    // TO DO: make API Call to get Item UUID from Capture UUID or vise/versa
-    this.isInAllMaps =
-      this.metadata.typeOfResource === "cartographic" &&
-      !!allMapsData.find((obj) => obj.uuid === this.uuid);
+    this.isInAllMaps = !!allMapsData.find((obj) => obj.uuid === this.uuid);
   }
 }
