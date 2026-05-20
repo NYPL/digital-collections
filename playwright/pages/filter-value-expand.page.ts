9434779260301895925d55c71693102dff3b4480
jbdalton <josephdalton@nypl.org>
2026-05-20 15:44:55 -0400

    remove comments

--- playwright/pages/filter-value-expand.page.ts
+++ playwright/pages/filter-value-expand.page.ts
@@ -54,10 +54,6 @@ export class FilterValueExpandedPage extends SearchPage {
       await expect(pageBtn).toHaveAttribute("aria-current", "page");
 
       // 2. Verify name refresh (concurrently with navigation)
-
-      // await expect(radios.last()).not.toHaveAttribute(lastRadioOldValue || "");
-      // lastRadioOldValue = await radios.last().getAttribute("value");
-
       await expect(radios.last()).not.toHaveAttribute(
         "value",
         lastRadioOldValue || ""
