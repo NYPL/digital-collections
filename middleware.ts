c94e14e5b622c8e603009c48abdf265f7d5e2092
Emre Tetik <emretetik@nypl.org>
2026-05-26 15:05:17 -0400

    Use traditional state management instead of url params

--- middleware.ts
+++ middleware.ts
@@ -205,7 +205,7 @@ export function middleware(req: NextRequest) {
   }
 
   if (itemsMatch) {
-    allowedParams = new Set(["type", "canvasIndex", "uuid", "cv", "viewAs"]);
+    allowedParams = new Set(["type", "canvasIndex", "uuid", "cv"]);
   } else {
     allowedParams = new Set(["q", "sort", "page", "filters", "viewMode"]);
   }
