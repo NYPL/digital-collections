6daa1f2752ee25f170f52e2f8a0d7cb325f89a77
Emre Tetik <emretetik@nypl.org>
2026-05-26 15:42:39 -0400

    Reapply "Use traditional state management instead of url params"
    This reverts commit 99ff6a1d9f5e23a6b8c58cedd93acbb224150198.

--- app/src/context/CanvasProvider.tsx
+++ app/src/context/CanvasProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams, usePathname } from "next/navigation";
-import React, { createContext, useContext, useCallback } from "react";
+import React, { createContext, useContext, useCallback, useState } from "react";
 
 interface CanvasContextType {
   currentCanvasIndex: number;
@@ -16,6 +16,8 @@ export const CanvasProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
+  const [isMapView, setIsMapView] = useState(false);
 
   // 1. Read directly from URL
   const currentCanvasIndex = parseInt(
@@ -35,26 +37,9 @@ export const CanvasProvider = ({ children }: { children: React.ReactNode }) => {
     [searchParams, pathname, router]
   );
 
-  const isMapView = searchParams.get("viewAs") === "map";
-
   const handleMapViewToggle = useCallback(() => {
-    const newSearchParams = new URLSearchParams(searchParams.toString());
-    console.log(
-      "toggling map view. current params are: ",
-      searchParams.toString()
-    );
-    if (isMapView) {
-      newSearchParams.delete("viewAs");
-    } else {
-      newSearchParams.set("viewAs", "map");
-      console.log(
-        "enabling map view. new params are: ",
-        newSearchParams.toString()
-      );
-    }
-
-    router.push(`${pathname}?${newSearchParams.toString()}`, { scroll: false });
-  }, [pathname, router, searchParams]);
+    setIsMapView((prev) => !prev);
+  }, []);
 
   return (
     <CanvasContext.Provider
