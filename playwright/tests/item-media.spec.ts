2e210e51ef64fb452bd24ad0ce5e91041830e7c8
jbdalton <josephdalton@nypl.org>
2026-06-03 14:52:10 -0400

    remove fixme comment

--- playwright/tests/item-media.spec.ts
+++ playwright/tests/item-media.spec.ts
@@ -40,9 +40,6 @@ test.describe("Verify Video Viewer Controls", () => {
   });
 });
 
-// FIXME: dl modal is not showing correct links within playwright even though they can be observed
-// manually on localhost.
-
 test.describe("Verify Download Actions", () => {
   test.beforeEach(async ({ page }) => {
     itemMediaPage = new ItemMediaPage(page, "IMAGE");
