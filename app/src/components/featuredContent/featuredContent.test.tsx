f3398dc9d55cbf88200f937f0bead3129d0953dd
Emre Tetik <emretetik@nypl.org>
2026-06-23 16:35:26 -0400

    Modify random number generation logic so that's not hardcoded but is tied to the number of spotlight entries

--- app/src/components/featuredContent/featuredContent.test.tsx
+++ app/src/components/featuredContent/featuredContent.test.tsx
@@ -39,4 +39,28 @@ describe("Featured Content component renders with expected props", () => {
       expect(button).toHaveAttribute("href", "https://www.nyplprint.store/");
     });
   });
+
+  it("renders 250 Years of the United States content", async () => {
+    render(<FeaturedContentComponent randomNumber={2} />);
+    const component = screen.getByTestId("featured-content-2");
+    await waitFor(() => {
+      expect(
+        within(component).getByText(
+          "250 Years of the United States at The New York Public Library"
+        )
+      ).toBeInTheDocument();
+      expect(within(component).getByRole("img")).toHaveAttribute(
+        "src",
+        "/250-years.webp"
+      );
+
+      const button = within(component).getByTestId(
+        "featured-learn-more-250-years"
+      );
+      expect(button).toHaveAttribute(
+        "href",
+        "https://www.nypl.org/spotlight/250-years-united-states"
+      );
+    });
+  });
 });
--- app/src/data/featuredContentData.ts
+++ app/src/data/featuredContentData.ts
@@ -2,19 +2,6 @@ import { FeaturedContentDataType } from "../types/FeaturedContentDataType";
 
 const featuredContentData: FeaturedContentDataType[] = [
   {
-    heading: "250 Years of the United States at The New York Public Library",
-    overline: "Featured",
-    text: "This year, the Library joins our nation in commemorating 250 years since the founding of the United States. As one of the world’s foremost institutions committed to democratic access to knowledge—and as the repository of some of the country’s most important founding documents—the Library is marking this milestone with special exhibitions, free programs and events for all ages, activities, educational offerings, a special anniversary book list, and more!",
-    link: "https://www.nypl.org/spotlight/250-years-united-states",
-    buttonText: "Learn more",
-    buttonId: "featured-learn-more-250-years",
-    ariaLabel:
-      "Learn more about 250 Years of the United States at The New York Public Library",
-    imgSrc: "/250-years.webp",
-    imgAlt:
-      "250 Years of the United States at The New York Public Library banner",
-  },
-  {
     heading: "Spotlight on the public domain",
     overline: "Featured",
     text: "The New York Public Library recently enhanced access to all public domain items in Digital Collections so that everyone has the freedom to enjoy and reuse these materials in almost limitless ways.",
@@ -35,6 +22,19 @@ const featuredContentData: FeaturedContentDataType[] = [
     ariaLabel: "Visit the Digital Collections print store",
     imgSrc: "/service-artehouse.jpg",
     imgAlt: "Service Artehouse banner",
+  },
+  {
+    heading: "250 Years of the United States at The New York Public Library",
+    overline: "Featured",
+    text: "This year, the Library joins our nation in commemorating 250 years since the founding of the United States. As one of the world’s foremost institutions committed to democratic access to knowledge—and as the repository of some of the country’s most important founding documents—the Library is marking this milestone with special exhibitions, free programs and events for all ages, activities, educational offerings, a special anniversary book list, and more!",
+    link: "https://www.nypl.org/spotlight/250-years-united-states",
+    buttonText: "Learn more",
+    buttonId: "featured-learn-more-250-years",
+    ariaLabel:
+      "Learn more about 250 Years of the United States at The New York Public Library",
+    imgSrc: "/250-years.webp",
+    imgAlt:
+      "250 Years of the United States at The New York Public Library banner",
   },
 ];
 
