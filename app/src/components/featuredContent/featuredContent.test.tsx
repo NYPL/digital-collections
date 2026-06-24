54cad134cec176d5f7f7b59ca86649c43e2cc8a0
Emre Tetik <emretetik@nypl.org>
2026-06-24 10:37:22 -0400

    Remove other spotlight items

--- app/src/components/featuredContent/featuredContent.test.tsx
+++ app/src/components/featuredContent/featuredContent.test.tsx
@@ -3,45 +3,8 @@ import React from "react";
 import FeaturedContentComponent from "./featuredContent";
 
 describe("Featured Content component renders with expected props", () => {
-  it("renders public domain content", async () => {
-    render(<FeaturedContentComponent randomNumber={0} />);
-    const component = screen.getByTestId("featured-content-0");
-    await waitFor(() => {
-      expect(
-        within(component).getByText("Spotlight on the public domain")
-      ).toBeInTheDocument();
-      expect(within(component).getByRole("img")).toHaveAttribute(
-        "src",
-        "/pd_banner.png"
-      );
-
-      const button = within(component).getByTestId("featured-learn-more");
-      expect(button).toHaveAttribute(
-        "href",
-        "https://www.nypl.org/research/resources/public-domain-collections"
-      );
-    });
-  });
-
-  it("renders Digital Collections store content", async () => {
-    render(<FeaturedContentComponent randomNumber={1} />);
-    const component = screen.getByTestId("featured-content-1");
-    await waitFor(() => {
-      expect(
-        within(component).getByText("Digital Collections print store")
-      ).toBeInTheDocument();
-      expect(within(component).getByRole("img")).toHaveAttribute(
-        "src",
-        "/service-artehouse.jpg"
-      );
-
-      const button = within(component).getByTestId("featured-visit-store");
-      expect(button).toHaveAttribute("href", "https://www.nyplprint.store/");
-    });
-  });
-
   it("renders 250 Years of the United States content", async () => {
-    render(<FeaturedContentComponent randomNumber={2} />);
+    render(<FeaturedContentComponent randomNumber={0} />);
     const component = screen.getByTestId("featured-content-2");
     await waitFor(() => {
       expect(
--- app/src/data/featuredContentData.ts
+++ app/src/data/featuredContentData.ts
@@ -2,28 +2,6 @@ import { FeaturedContentDataType } from "../types/FeaturedContentDataType";
 
 const featuredContentData: FeaturedContentDataType[] = [
   {
-    heading: "Spotlight on the public domain",
-    overline: "Featured",
-    text: "The New York Public Library recently enhanced access to all public domain items in Digital Collections so that everyone has the freedom to enjoy and reuse these materials in almost limitless ways.",
-    link: "https://www.nypl.org/research/resources/public-domain-collections",
-    buttonText: "Learn more",
-    buttonId: "featured-learn-more",
-    ariaLabel: "Learn more about the public domain",
-    imgSrc: "/pd_banner.png",
-    imgAlt: "Public Domain banner",
-  },
-  {
-    heading: "Digital Collections print store",
-    overline: "Featured",
-    text: "Decorative prints for purchase: choose from archival prints, framed art, stretched canvas, vintage wood, and wall murals.",
-    link: "https://www.nyplprint.store/",
-    buttonText: "Visit Store",
-    buttonId: "featured-visit-store",
-    ariaLabel: "Visit the Digital Collections print store",
-    imgSrc: "/service-artehouse.jpg",
-    imgAlt: "Service Artehouse banner",
-  },
-  {
     heading: "250 Years of the United States at The New York Public Library",
     overline: "Featured",
     text: "This year, the Library joins our nation in commemorating 250 years since the founding of the United States. As one of the world’s foremost institutions committed to democratic access to knowledge—and as the repository of some of the country’s most important founding documents—the Library is marking this milestone with special exhibitions, free programs and events for all ages, activities, educational offerings, a special anniversary book list, and more!",
