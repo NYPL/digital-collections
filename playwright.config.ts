e7e5be08a9ced7574326cfab47c6f8c0aca35078
jbdalton <josephdalton@nypl.org>
2026-07-01 16:47:12 -0400

    add ff and webkit back to config

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,15 +76,15 @@ export default defineConfig({
       },
     },
 
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
 
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
 
     /* Test against mobile viewports. */
     // {
