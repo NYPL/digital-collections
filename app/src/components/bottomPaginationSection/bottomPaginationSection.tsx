b8d12cdc0bf871d756649d5602e7b747a9c32080
Alessandra Vertrees <alessandravertrees@nypl.org>
2026-07-02 14:58:58 -0400

    refactor bottom pagination for the other pages

---
+++ app/src/components/bottomPaginationSection/bottomPaginationSection.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React from "react";
+import { Box, Pagination } from "@nypl/design-system-react-components";
+import { headerBreakpoints } from "@/src/utils/breakpoints";
+import BackToTopLink from "../backToTopLink/backToTopLink";
+
+type BottomPaginationSectionProps = {
+  currentPage: number;
+  initialPage: number;
+  pageCount: number;
+  onPageChange: (newPage: number) => void;
+  paginationId?: string;
+  rightContent?: React.ReactNode;
+};
+
+const BottomPaginationSection = ({
+  currentPage,
+  initialPage,
+  pageCount,
+  onPageChange,
+  paginationId = "pagination-id",
+  rightContent,
+}: BottomPaginationSectionProps) => {
+  return (
+    <Box
+      marginTop="xxl"
+      marginBottom="xxl"
+      sx={{
+        display: "grid",
+        gap: "m",
+        gridTemplateColumns: "1fr",
+        alignItems: "center",
+        [`@media screen and (min-width: ${headerBreakpoints.lgMobile}px)`]: {
+          gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+        },
+        [`@media screen and (min-width: ${headerBreakpoints.lgTablet}px)`]: {
+          gridTemplateColumns: "1fr",
+        },
+        [`@media screen and (min-width: ${headerBreakpoints.desktop}px)`]: {
+          gridTemplateColumns: "1fr auto 1fr",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          justifySelf: "center",
+          order: 2,
+          [`@media screen and (min-width: ${headerBreakpoints.lgMobile}px)`]: {
+            order: 2,
+            gridColumn: "1 / -1",
+            justifySelf: "center",
+          },
+          [`@media screen and (min-width: ${headerBreakpoints.desktop}px)`]: {
+            order: 1,
+            gridColumn: "auto",
+            justifySelf: "start",
+          },
+        }}
+      >
+        <BackToTopLink />
+      </Box>
+
+      <Pagination
+        id={paginationId}
+        initialPage={initialPage}
+        currentPage={currentPage}
+        pageCount={pageCount}
+        onPageChange={onPageChange}
+        sx={{
+          justifySelf: "center",
+          justifyContent: "center",
+          order: 1,
+          [`@media screen and (min-width: ${headerBreakpoints.lgMobile}px)`]: {
+            order: 1,
+            gridColumn: "1 / -1",
+          },
+          [`@media screen and (min-width: ${headerBreakpoints.desktop}px)`]: {
+            order: 2,
+            gridColumn: "auto",
+          },
+        }}
+      />
+
+      {rightContent && (
+        <Box
+          sx={{
+            order: 3,
+            justifySelf: "center",
+            gridColumn: "1 / -1",
+            [`@media screen and (min-width: ${headerBreakpoints.desktop}px)`]: {
+              justifySelf: "end",
+              gridColumn: "auto",
+            },
+          }}
+        >
+          {rightContent}
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default BottomPaginationSection;
