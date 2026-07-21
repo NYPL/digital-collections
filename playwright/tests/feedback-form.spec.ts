9bc1eb5f0bd22e7036304e920d3e597196c5acef
jbdalton <josephdalton@nypl.org>
2026-07-21 14:35:38 -0400

    refactor to add feedback data checks for E2E integration

--- playwright/tests/feedback-form.spec.ts
+++ playwright/tests/feedback-form.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "../base";
 import { AboutPage } from "../pages/about.page";
 import FeedbackModal from "../pages/feedback-form.page";
 
-test.describe("Verify Feedback Controls", () => {
+test.describe("Feedback UX & Integration Tests", () => {
   let feedbackModal: FeedbackModal;
 
   test.beforeEach(async ({ page }) => {
@@ -12,28 +12,58 @@ test.describe("Verify Feedback Controls", () => {
     await feedbackModal.open();
   });
 
-  test("should dynamically update the character counter", async () => {
-    await feedbackModal.verifyCharacterCounterUpdates();
-  });
+  test.describe("Verify UX Feedback Controls", () => {
+    test("should dynamically update the character counter", async () => {
+      await feedbackModal.verifyCharacterCounterUpdates();
+    });
 
-  test("should successfully submit a bug report", async () => {
-    await feedbackModal.setupNetworkMock();
-    await feedbackModal.verifySuccessfulSubmission(
-      feedbackModal.feedbackBugRadioButton
-    );
-  });
+    test("should successfully submit a bug report", async () => {
+      await feedbackModal.setupNetworkMock();
+      await feedbackModal.verifySuccessfulSubmission(
+        feedbackModal.feedbackBugRadioButton
+      );
+    });
 
-  test("should successfully submit a comment", async () => {
-    await feedbackModal.setupNetworkMock();
-    await feedbackModal.verifySuccessfulSubmission(
-      feedbackModal.feedbackCommentRadioButton
-    );
-  });
+    test("should successfully submit a comment", async () => {
+      await feedbackModal.setupNetworkMock();
+      await feedbackModal.verifySuccessfulSubmission(
+        feedbackModal.feedbackCommentRadioButton
+      );
+    });
 
-  test("should successfully submit a correction", async () => {
-    await feedbackModal.setupNetworkMock();
-    await feedbackModal.verifySuccessfulSubmission(
-      feedbackModal.feedbackCorrectionRadioButton
-    );
+    test("should successfully submit a correction", async () => {
+      await feedbackModal.setupNetworkMock();
+      await feedbackModal.verifySuccessfulSubmission(
+        feedbackModal.feedbackCorrectionRadioButton
+      );
+    });
+  });
+  test.describe("Verify Feedback data", () => {
+    // Placeholder runs the 3 types of test: comment, bug, correction
+    // and checks test account to verify all were received correctly.
+
+    // Integration test block (No Mock)
+    test("should successfully submit a comment", async () => {
+      const timestamp = new Date().toISOString();
+      const liveTestComment = `TEST - QA Integration Test - ${timestamp}`;
+
+      // Allow API call to go through
+      await feedbackModal.verifySuccessfulSubmission(
+        feedbackModal.feedbackCommentRadioButton,
+        liveTestComment
+      );
+    });
+
+    test("should successfully submit a bug report", async () => {
+      await feedbackModal.verifySuccessfulSubmission(
+        feedbackModal.feedbackBugRadioButton
+      );
+    });
+
+    test("should successfully submit a correction", async () => {
+      await feedbackModal.verifySuccessfulSubmission(
+        feedbackModal.feedbackCorrectionRadioButton
+      );
+    });
   });
 });
