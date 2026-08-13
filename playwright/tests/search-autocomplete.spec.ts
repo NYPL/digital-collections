e21659158b886f293b016261ede1bc9eaae3046f
jbdalton <josephdalton@nypl.org>
2026-08-13 17:42:21 -0400

    spec scaffolding

---
+++ playwright/tests/search-autocomplete.spec.ts
@@ -0,0 +1,93 @@
+import { test } from "../base";
+import { SearchAutosuggestPage } from "../pages/search-autocomplete.page";
+import { applyRouteFilters } from "../utils/routeFilters";
+
+test.describe("Search autosuggest dropdown", () => {
+  let autosuggestPage: SearchAutosuggestPage;
+
+  test.beforeEach(async ({ page }) => {
+    autosuggestPage = new SearchAutosuggestPage(page);
+    await autosuggestPage.loadPage("/");
+    await autosuggestPage.verifySearchBarVisible();
+  });
+
+  test("does not show dropdown for fewer than 3 characters", async () => {
+    await autosuggestPage.typeQuery(SearchAutosuggestPage.SHORT_QUERY);
+    await autosuggestPage.verifyDropdownNotVisible();
+  });
+
+  test("shows dropdown after typing 3+ characters", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+    await autosuggestPage.verifyFirstOptionVisible();
+  });
+
+  test("clicking a suggestion navigates to search results", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+    const expectedTitle = await autosuggestPage.selectFirstSuggestion();
+    await autosuggestPage.verifyNavigatedToSearchResults(expectedTitle);
+  });
+
+  test("ArrowDown and ArrowUp navigate through suggestions", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+
+    await autosuggestPage.pressArrowDown();
+    await autosuggestPage.verifyOptionSelectedAtIndex(0);
+
+    await autosuggestPage.pressArrowDown();
+    await autosuggestPage.verifyOptionSelectedAtIndex(1);
+
+    await autosuggestPage.pressArrowUp();
+    await autosuggestPage.verifyOptionSelectedAtIndex(0);
+  });
+
+  test("Enter selects the keyboard-highlighted suggestion", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+    const expectedTitle =
+      await autosuggestPage.selectFirstSuggestionViaKeyboard();
+    await autosuggestPage.verifyNavigatedToSearchResults(expectedTitle);
+  });
+
+  test("Escape closes the dropdown", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+    await autosuggestPage.pressEscape();
+    await autosuggestPage.verifyDropdownNotVisible();
+  });
+
+  test("clicking the search button submits the typed query", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+    await autosuggestPage.submitSearch();
+    await autosuggestPage.verifyNavigatedToSearchResults(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+  });
+
+  test("Enter with no suggestion highlighted submits the typed query", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+    await autosuggestPage.pressEnter();
+    await autosuggestPage.verifyNavigatedToSearchResults(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+  });
+
+  test("clicking outside the search component closes the dropdown", async () => {
+    await autosuggestPage.typeAndWaitForSuggestions(
+      SearchAutosuggestPage.SUGGEST_QUERY
+    );
+    await autosuggestPage.dismissByClickingOutside();
+    await autosuggestPage.verifyDropdownNotVisible();
+  });
+});
